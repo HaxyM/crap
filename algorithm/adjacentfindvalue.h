07e32bd3bb7ef3826be3d9510fe511e030b9c0cf
Igor Zarzycki <izarzycki@dmcs.pl>
2019-07-28 01:46:15 +0200

    Add adjacentFind algorithm operating on values.

---
+++ algorithm/adjacentfindvalue.h
@@ -0,0 +1,40 @@
+#ifndef CRAP_ALGORITHM_ADJACENTFIND
+#define CRAP_ALGORITHM_ADJACENTFIND
+
+#include "../utility/valuelist.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class, Type...> struct adjacentFindValue;
+
+ template <class Type, template <Type, Type> class Operator> struct adjacentFindValue<Type, Operator>
+ {
+  constexpr const static std :: size_t value = 0u;
+  constexpr const static std :: size_t npos = 0u;
+ };
+
+ template <class Type, template <Type, Type> class Operator, Type Value> struct adjacentFindValue<Type, Operator, Value>
+ {
+  constexpr const static std :: size_t value = 1u;
+  constexpr const static std :: size_t npos = 1u;
+ };
+
+ template <class Type, template <Type, Type> class Operator, Type ... Values> struct adjacentFindValue
+ {
+  private:
+  using values = valueList<Type, Values...>;
+  constexpr const static std :: size_t half = values :: size / 2u;
+  template <Type ... SubValues> using This = adjacentFindValue<Type, Operator, SubValues...>;
+  using lower = typename values :: template till<half, This>;
+  using upper = typename values :: template since<half,  This>;
+  constexpr const static Type lowerLast = values :: template at<half - 1u>;
+  constexpr const static Type upperFirst = values :: template at<half>;
+  constexpr const static bool foundInLower = ((lower :: value) != (lower :: npos));
+  constexpr const static bool foundOnEdge = Operator <lowerLast, upperFirst> :: value;
+  public:
+  constexpr const static std :: size_t value = (foundInLower ? (lower :: value) : (foundOnEdge ? (half - 1u) : ((lower :: npos) + (upper :: value))));
+  constexpr const static std :: size_t npos = (lower :: npos) + (upper :: npos);
+ };
+}
+#endif
+
