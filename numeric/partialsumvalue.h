b6c622638889c54ace9df48a2d62d2b7b3b6e322
Igor Zarzycki <izarzycki@dmcs.pl>
2019-08-02 09:45:44 +0200

    Fix partialSum algorithm operatirng on values to use passed container.

--- numeric/partialsumvalue.h
+++ numeric/partialsumvalue.h
@@ -1,7 +1,7 @@
 #ifndef CRAP_NUMERIC_PARTIALSUMVALUE
 #define CRAP_NUMERIC_PARTIALSUMVALUE
 
-#include "../utility/valuelist.h"
+#include "../utility/valuelistfortype.h"
 
 namespace crap
 {
@@ -9,12 +9,12 @@ namespace crap
 
  template <class Type, template <Type, Type> class Operator> struct partialSumValue<Type, Operator>
  {
-  using type = valueList<Type>;
+  template <template <Type...> class Container = valueListForType <Type> :: template type> using type = Container<>;
  };
 
  template <class Type, template <Type, Type> class Operator, Type Value> struct partialSumValue<Type, Operator, Value>
  {
-  using type = valueList<Type, Value>;
+  template <template <Type...> class Container = valueListForType <Type> :: template type> using type = Container<Value>;
  };
 
  template <class Type, template <Type, Type> class Operator, Type ... Values> struct partialSumValue
@@ -23,12 +23,20 @@ namespace crap
   using values = valueList<Type, Values...>;
   template <Type ... SubValues> using This = partialSumValue<Type, Operator, SubValues...>;
   constexpr const static std :: size_t half = (values :: size) / 2u;
-  using lower = typename values :: template till <half, This> :: type;
-  using upper = typename values :: template since <half, This> :: type;
+  using lower = typename values :: template till <half, This> :: template type<>;
+  using upper = typename values :: template since <half, This> :: template type<>;
   constexpr const static Type lowerLast = lower :: template at<lower :: size - 1u>;
-  template <Type ... LowerValues, Type ... UpperValues> static valueList<Type, LowerValues..., (lowerLast + UpperValues)...> merge(valueList<Type, LowerValues...>, valueList<Type, UpperValues...>);
+  template <template <Type...> class Container, Type LowerLast> struct merger;
   public:
-  using type = decltype(merge(lower{}, upper{}));
+  template <template <Type...> class Container = valueListForType <Type> :: template type>
+  using type = decltype(merger <Container, lowerLast> :: merge(lower{}, upper{}));
+ };
+
+ template <class Type, template <Type, Type> class Operator, Type ... Values>
+ template <template <Type...> class Container, Type LowerLast> struct partialSumValue <Type, Operator, Values...> :: merger
+ {
+  template <Type ... LowerValues, Type ... UpperValues> static Container<LowerValues..., (LowerLast + UpperValues)...>
+  merge(valueList<Type, LowerValues...>, valueList<Type, UpperValues...>);
  };
 }
 #endif
