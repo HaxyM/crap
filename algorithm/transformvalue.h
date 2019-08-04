d6c34bc31571e3fea9e0f018c26e74e4713f099b
Igor Zarzycki <izarzycki@dmcs.pl>
2019-08-04 14:58:21 +0200

    Fix transform algorithm operating on values to allow type changes

--- algorithm/transformvalue.h
+++ algorithm/transformvalue.h
@@ -3,11 +3,15 @@
 
 #include "../utility/valuelistfortype.h"
 
+#include <type_traits>
+
 namespace crap
 {
  template <class Type, template <Type> class Operator, Type ... Values> struct transformValue
  {
-  template <template <Type...> class Container = valueListForType <Type> :: template type> using type = Container<(Operator <Values> :: value)...>;
+  using resultType = std :: common_type_t<decltype(Operator <Values> :: value)...>;
+  template <template <resultType...> class Container = valueListForType <resultType> :: template type>
+  using type = Container<(static_cast<resultType>(Operator <Values> :: value))...>;
  };
 }
 #endif
