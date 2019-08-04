7dc5e409decaed5726fd5dcbb298e549c4a0b496
Igor Zarzycki <izarzycki@dmcs.pl>
2019-08-04 23:25:09 +0200

    Fix transfor algorithm operating on values not to crash on empty vector.

--- algorithm/transformvalue.h
+++ algorithm/transformvalue.h
@@ -7,6 +7,15 @@
 
 namespace crap
 {
+ template <class Type, template <Type> class, Type...> struct transformValue;
+
+ template <class Type, template <Type> class Operator> struct transformValue<Type, Operator>
+ {
+  using resultType = Type;
+  template <template <resultType...> class Container = valueListForType <resultType> :: template type>
+  using type = Container<>;
+ };
+
  template <class Type, template <Type> class Operator, Type ... Values> struct transformValue
  {
   using resultType = std :: common_type_t<decltype(Operator <Values> :: value)...>;
