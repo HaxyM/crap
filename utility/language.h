59e6e434984ffe6db403067f654fc8fcdf0c1c63
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2019-08-23 00:19:56 +0200

    Add language version recognision.

---
+++ utility/language.h
@@ -0,0 +1,8 @@
+#ifndef CRAP_UTILITY_LANGUAGE
+#define CRAP_UTILITY_LANGUAGE
+
+//In case of msvc requirec "/Zc:__cplusplus" flag to be set.
+#define CPP14 (__cplusplus >= 201402)
+#define CPP17 (__cplusplus >= 201703)
+
+#endif
