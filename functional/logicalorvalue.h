0f00a4e6c30414d4ad53c97290bac202567b28c9
HaxyM <izarzycki@dmcs.pl>
2019-08-23 01:09:44 +0200

    Extend logicalOr operator operating on values to support multiple arguments.

--- functional/logicalorvalue.h
+++ functional/logicalorvalue.h
@@ -1,11 +1,42 @@
 #ifndef CRAP_FUNCTIONAL_LOGICALORVALUE
 #define CRAP_FUNCTIONAL_LOGICALORVALUE
 
+#include "../utility/language.h"
+
+#if CPP17
+#else
+#include "../numeric/reducevalue.h"
+#endif
+
 namespace crap
 {
- template <class Type, Type Value1, Type Value2> struct logicalOrValue
+ template <class Type, Type...> struct logicalOrValue;
+
+ template <class Type> struct logicalOrValue<Type>
+ {
+  constexpr const static auto value = static_cast<Type>(false);
+ };
+
+ template <class Type, Type Value> struct logicalOrValue<Type, Value>
+ {
+  constexpr const static auto value = Value;
+ };
+
+ template <class Type, Type Value1, Type Value2> struct logicalOrValue<Type, Value1, Value2>
  {
   constexpr const static auto value = (Value1 || Value2);
+ };
+
+ template <class Type, Type ... Values> struct logicalOrValue
+ {
+  #if CPP17
+  constexpr const static auto value = (Values || ...);
+  #else
+  private:
+  template <Type ... SubValues> using This = logicalOrValue<Type, SubValues...>;
+  public:
+  constexpr const static auto value = reduceValue <Type, This, Values...> :: value;
+  #endif
  };
 }
 #endif
