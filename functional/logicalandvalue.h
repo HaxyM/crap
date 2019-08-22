7aeb3cabfd60a5369a49ec28bd95d27f2a8a3794
HaxyM <izarzycki@dmcs.pl>
2019-08-22 23:36:39 +0200

    Extend logicalAnd operator operating on values to support multiple arguments.

--- functional/logicalandvalue.h
+++ functional/logicalandvalue.h
@@ -3,9 +3,26 @@
 
 namespace crap
 {
- template <class Type, Type Value1, Type Value2> struct logicalAndValue
+ template <class Type, Type...> struct logicalAndValue;
+
+ template <class Type> struct logicalAndValue<Type>
+ {
+  constexpr const static auto value = static_cast<Type>(true);
+ };
+
+ template <class Type, Type Value> struct logicalAndValue<Type, Value>
+ {
+  constexpr const static auto value = Value;
+ };
+
+ template <class Type, Type Value1, Type Value2> struct logicalAndValue<Type, Value1, Value2>
  {
   constexpr const static auto value = (Value1 && Value2);
+ };
+
+ template <class Type, Type ... Values> struct logicalAndValue
+ {
+  static_assert(sizeof...(Values) <= 2u, "Folding expressions are not supported.");
  };
 }
 #endif
