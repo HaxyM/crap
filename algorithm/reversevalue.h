bc6aad32ca5cc56e0c3375d5d04e1d92acaf1afb
Igor Zarzycki <izarzycki@dmcs.pl>
2019-08-27 00:05:45 +0200

    Add reverse algorithm operating on values.

---
+++ algorithm/reversevalue.h
@@ -0,0 +1,45 @@
+#ifndef CRAP_ALGORITHM_REVERSEVALUE
+#define CRAP_ALGORITHM_REVERSEVALUE
+
+#include "../utility/mergevalue.h"
+#include "../utility/valuelistfortype.h"
+
+namespace crap
+{
+ template <class Type, Type...> struct reverseValue;
+
+ template <class Type> struct reverseValue<Type>
+ {
+  template <template <Type...> class Container = valueListForType <Type> :: template type> using type = Container<>;
+ };
+
+ template <class Type, Type Value> struct reverseValue<Type, Value>
+ {
+  template <template <Type...> class Container = valueListForType <Type> :: template type> using type = Container<Value>;
+ };
+
+ template <class Type, Type Value1, Type Value2> struct reverseValue<Type, Value1, Value2>
+ {
+  template <template <Type...> class Container = valueListForType <Type> :: template type> using type = Container<Value2, Value1>;
+ };
+
+ template <class Type, Type Value1, Type Value2, Type Value3> struct reverseValue<Type, Value1, Value2, Value3>
+ {
+  template <template <Type...> class Container = valueListForType <Type> :: template type> using type = Container<Value3, Value2, Value1>;
+ };
+
+ template <class Type, Type ... Values> struct reverseValue
+ {
+  private:
+  using values = valueList<Type, Values...>;
+  constexpr const static auto half = (values :: size) / 2u;
+  template <Type ... SubTypes> using This = reverseValue<Type, SubTypes...>;
+  template <Type ... SubTypes> using merger = mergeValue<Type, SubTypes...>;
+  using upper = typename values :: template since <half, This> :: template type<merger>;
+  using lower = typename values :: template till <half, This> :: template type<upper :: template with>;
+  public:
+  template <template <Type...> class Container = valueListForType <Type> :: template type> using type = typename lower :: template type<Container>;
+ };
+}
+#endif
+
