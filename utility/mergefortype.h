d0bbcf2b08fa74d25d7086b29152e33f2177163d
Igor Zarzycki <izarzycki@dmcs.pl>
2019-08-29 00:12:36 +0200

    Add mergeForType.

---
+++ utility/mergefortype.h
@@ -0,0 +1,15 @@
+#ifndef CRAP_UTILITY_MERGEFORTYPE
+#define CRAP_UTILITY_MERGEFORTYPE
+
+#include "../utility/mergevalue.h"
+
+namespace crap
+{
+ template <class Type> struct mergeForType
+ {
+  template <Type ... Values> using type = mergeValue<Type, Values...>;
+  template <Type ... Values> using values = mergeValue<Type, Values...>;
+ };
+}
+#endif
+
