03e9e1d12e54d4efc17f020a9856c391efa8dbfa
Igor Zarzycki <izarzycki@dmcs.pl>
2019-08-31 16:48:02 +0200

    Add valueDemultiplexer.

---
+++ utility/valuedemultiplexer.h
@@ -0,0 +1,21 @@
+#ifndef CRAP_UTILITY_VALUEDEMULTIPLEXER
+#define CRAP_UTILITY_VALUEDEMULTIPLEXER
+
+namespace crap
+{
+ template <class Type, bool, template <template <Type...> class> class, template <Type...> class, template <Type...> class> struct valueDemultiplexer;
+
+ template <class Type, template <template <Type...> class> class Source, template <Type...> class ContainerIfTrue, template <Type...> class ContainerIfFalse>
+	 struct valueDemultiplexer<Type, true, Source, ContainerIfTrue, ContainerIfFalse>
+ {
+  using type = Source<ContainerIfTrue>;
+ };
+
+ template <class Type, bool template <template <Type...> class> class Source, template <Type...> class, template <Type...> class ContainerIfFalse>
+	 struct valueDemultiplexer
+ {
+  using type = Source<ContainerIfFalse>;
+ };
+}
+#endif
+
