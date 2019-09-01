f427c3235a88f51f01e64ffdb4d18b7f7e095eba
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-01 15:33:11 +0200

    Add isSortedForType.

---
+++ algorithm/issortedfortype.h
@@ -0,0 +1,17 @@
+#ifndef CRAP_ALGORITHM_ISSORTEDFORTYPE
+#define CRAP_ALGORITHM_ISSORTEDFORTYPE
+
+#include "issortedvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class Comparator = comparatorsForType <Type> :: template Less>
+	 struct isSortedForType
+ {
+  template <Type ... Values> using values = isSortedValue<Type, Comparator, Values...>;
+  template <Type ... Values> using type = isSortedValue<Type, Comparator, Values...>;
+ };
+}
+#endif
+
