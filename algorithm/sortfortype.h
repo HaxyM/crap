83e8654aa06abe5e0fdce8d3f20a5931576c38ce
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-01 17:55:15 +0200

    Add sortForType.

---
+++ algorithm/sortfortype.h
@@ -0,0 +1,17 @@
+#ifndef CRAP_ALGORITHM_SORTFORTYPE
+#define CRAP_ALGORITHM_SORTFORTYPE
+
+#include "sortvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class Comparator = comparatorsForType <Type> :: template Less>
+	 struct sortForType
+ {
+  template <Type ... Values> using values = sortValue<Type, Comparator, Values...>;
+  template <Type ... Values> using type = sortValue<Type, Comparator, Values...>;
+ };
+}
+#endif
+
