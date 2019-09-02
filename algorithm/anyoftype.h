d7816a5c57e5942dd8f0a6ef6347fba91533aa52
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-02 23:27:44 +0200

    Fix anyOf algorithm operating on types to offer same functionality as std :: integral_constant.

--- algorithm/anyoftype.h
+++ algorithm/anyoftype.h
@@ -10,10 +10,18 @@ namespace crap
  template <template <class> class Operator, class ... Types> struct anyOfType
  {
   private:
-  using resultType = std :: common_type_t<decltype(Operator <Types> :: value)...>;
+  using resultType = typename std :: common_type<decltype(Operator <Types> :: value)...> :: type;
   public:
   constexpr const static auto value = logicalOrValue <resultType, static_cast<resultType>(Operator <Types> :: value)...> :: value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
+}
+
+template <template <class> class Operator, class ... Types>
+inline constexpr crap :: anyOfType <Operator, Types...> :: operator typename crap :: anyOfType <Operator, Types...> :: value_type () const noexcept
+{
+ return crap :: anyOfType <Operator, Types...> :: value;
 }
 #endif
 
