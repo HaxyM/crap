42ece0960e1ff026f78ab5f31c8b0fbcd87a6a94
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-02 22:18:16 +0200

    Fix unique algorithm operating on values to support C++11.

--- algorithm/uniquevalue.h
+++ algorithm/uniquevalue.h
@@ -25,8 +25,8 @@ namespace crap
   constexpr const static std :: size_t half = (values :: size) / 2u;
   using lower = typename values :: template till <half, This> :: template type<>;
   using upper = typename values :: template since <half, This> :: template type<>;
-  constexpr const static Type lowerLast = lower :: template at<lower :: size - 1u>;
-  constexpr const static Type upperFirst = upper :: template at<0u>;
+  constexpr const static Type lowerLast = lower :: template At <lower :: size - 1u> :: value;
+  constexpr const static Type upperFirst = upper :: template At <0u> :: value;
   constexpr const static bool shouldSkipUpperFirst = Operator <lowerLast, upperFirst> :: value;
   using upperToMerge = typename upper :: template since<shouldSkipUpperFirst ? 1u : 0u>; //Upper with or without first value
   template <template <Type...> class Container> struct merger;
