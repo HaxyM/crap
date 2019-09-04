a1d6e0fdeb629d1ddfb04cfb1243a80cb9372e62
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-04 09:01:11 +0200

    Fix copyIf algorithm operating on values to use common merger.

--- algorithm/copyifvalue.h
+++ algorithm/copyifvalue.h
@@ -1,6 +1,7 @@
 #ifndef CRAP_ALGORITHM_COPYIFVALUE
 #define CRAP_ALGORITHM_COPYIFVALUE
 
+#include "../utility/mergefortype.h"
 #include "../utility/valuelistfortype.h"
 
 namespace crap
@@ -12,31 +13,27 @@ namespace crap
   template <template <Type...> class Container = valueListForType <Type> :: template type> using type = Container<>;
  };
 
- template <class Type, template <Type...> class Operator, Type Value> struct copyIfValue<Type, Operator, Value>
+ template <class Type, template <Type> class Operator, Type Value> struct copyIfValue<Type, Operator, Value>
  {
   private:
   using values = valueList<Type, Value>;
   constexpr const static std :: size_t offset = (Operator <Value> :: value ? 0u : 1u);
   public:
-  template <template <Type...> class Container = valueListForType <Type> :: template type> using type = typename values :: template since<offset, Container>;
+  template <template <Type...> class Container = valueListForType <Type> :: template type>
+	  using type = typename values :: template since<offset, Container>;
  };
 
- template <class Type, template <Type...> class Operator, Type ... Values> struct copyIfValue
+ template <class Type, template <Type> class Operator, Type ... Values> struct copyIfValue
  {
   private:
   using values = valueList<Type, Values...>;
   constexpr const static std :: size_t half = (values :: size) / 2u;
   template <Type ... SubValues> using This = copyIfValue<Type, Operator, SubValues...>;
-  using lower = typename values :: template till<half, This> :: template type<>;
-  using upper = typename values :: template since<half, This> :: template type<>;
-  template <template <Type...> class Container> struct merger;
+  using lower = typename values :: template till <half, This> :: template type<mergeForType <Type> :: template values>;
+  using upper = typename values :: template since <half, This> :: template type<lower :: template with>;
   public:
-  template <template <Type...> class Container = valueListForType <Type> :: template type> using type = decltype(merger <Container> :: merge(lower{}, upper{}));
- };
-
- template <class Type, template <Type...> class Operator, Type ... Values> template <template <Type...> class Container> struct copyIfValue <Type, Operator, Values...> :: merger
- {
-  template <Type ... LowerValues, Type ... UpperValues> static Container<LowerValues..., UpperValues...> merge(valueList<Type, LowerValues...>, valueList<Type, UpperValues...>);
+  template <template <Type...> class Container = valueListForType <Type> :: template type>
+	  using type = typename upper :: template type<Container>;
  };
 }
 #endif
