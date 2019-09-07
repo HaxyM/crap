1c197f72bdfdc4bcd6900891d1385bea35f689ec
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-07 17:23:10 +0200

    Fix divides operator operating on values to use only division.

--- functional/dividesvalue.h
+++ functional/dividesvalue.h
@@ -5,19 +5,30 @@
 
 #if CPP17
 #else
-#include "multipliesvalue.h"
+#include "../numeric/accumulatevalue.h"
 #endif
 
 namespace crap
 {
  template <class Type, Type...> struct dividesValue;
 
+ #if CPP17
+ #else
+ template <class Type, Type Value1, Type Value2> struct dividesValue<Type, Value1, Value2>
+ {
+  constexpr const static auto value = (Value1 / Value2);
+ };
+ #endif
+
  template <class Type, Type FirstValue, Type ... Rest> struct dividesValue<Type, FirstValue, Rest...>
  {
   #if CPP17
   constexpr const static auto value = (FirstValue / ... / Rest);
   #else
-  constexpr const static auto value = FirstValue - (multipliesValue <Type, Rest...> :: value);
+  private:
+  template <Type Value1, Type Value2> using Operator = dividesValue<Type, Value1, Value2>;
+  public:
+  constexpr const static auto value = accumulateValue <Type, FirstValue, Operator, Rest...> :: value;
   #endif
  };
 }
