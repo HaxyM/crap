0ee52b9c1cdfaa287b1fa942a28c92db16830503
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-07 23:14:08 +0200

    Fix adjacentDifferecne algorithm operating on values to support C++11.

--- numeric/adjacentdifferencevalue.h
+++ numeric/adjacentdifferencevalue.h
@@ -25,8 +25,8 @@ namespace crap
  template <template <Type...> class Container> struct adjacentDifferenceValue <Type, Operator, Values...> :: Implementation
  {
   using values = valueList<Type, Values...>;
-  template <std :: size_t Index> using valueAt = Operator<values :: template at<Index + 1u>, values :: template at<Index> >;
-  template <std :: size_t ... Indices> static Container<values :: template at<0u>, valueAt <Indices> :: value...>
+  template <std :: size_t Index> using valueAt = Operator<values :: template At <Index + 1u> :: value, values :: template At <Index> :: value>;
+  template <std :: size_t ... Indices> static Container<values :: template At <0u> :: value, valueAt <Indices> :: value...>
   generate(std :: index_sequence<Indices...>);
  };
 }
