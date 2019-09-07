197ea90d038181a61ee9a2648470444e0a814a19
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-07 17:24:04 +0200

    Add modulus operator operating on values.

---
+++ functional/modulusvalue.h
@@ -0,0 +1,36 @@
+#ifndef CRAP_FUNCTIONAL_MODULUSVALUE
+#define CRAP_FUNCTIONAL_MODULUSVALUE
+
+#include "../utility/language.h"
+
+#if CPP17
+#else
+#include "../numeric/accumulatevalue.h"
+#endif
+
+namespace crap
+{
+ template <class Type, Type...> struct modulusValue;
+
+ #if CPP17
+ #else
+ template <class Type, Type Value1, Type Value2> struct modulusValue<Type, Value1, Value2>
+ {
+  constexpr const static auto value = (Value1 % Value2);
+ };
+ #endif
+
+ template <class Type, Type FirstValue, Type ... Rest> struct modulusValue<Type, FirstValue, Rest...>
+ {
+  #if CPP17
+  constexpr const static auto value = (FirstValue % ... % Rest);
+  #else
+  private:
+  template <Type Value1, Type Value2> using Operator = modulusValue<Type, Value1, Value2>;
+  public:
+  constexpr const static auto value = accumulateValue <Type, FirstValue, Operator, Rest...> :: value;
+  #endif
+ };
+}
+#endif
+
