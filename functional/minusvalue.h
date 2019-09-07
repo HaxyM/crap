ddf6896b97679624acc358cfde86b1bd814bd2af
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-07 16:53:58 +0200

    Fix minus oparator operatind on values to use onlu subtraction.

--- functional/minusvalue.h
+++ functional/minusvalue.h
@@ -5,19 +5,30 @@
 
 #if CPP17
 #else
-#include "plusvalue.h"
+#include "../numeric/accumulatevalue.h"
 #endif
 
 namespace crap
 {
  template <class Type, Type...> struct minusValue;
 
+ #if CPP17
+ #else
+ template <class Type, Type Value1, Type Value2> struct minusValue<Type, Value1, Value2>
+ {
+  constexpr const static auto value = (Value1 - Value2);
+ };
+ #endif
+
  template <class Type, Type FirstValue, Type ... Rest> struct minusValue<Type, FirstValue, Rest...>
  {
   #if CPP17
   constexpr const static auto value = (FirstValue - ... - Rest);
   #else
-  constexpr const static auto value = FirstValue - (plusValue <Type, Rest...> :: value);
+  private:
+  template <Type Value1, Type Value2> using Operator = minusValue<Type, Value1, Value2>;
+  public:
+  constexpr const static auto value = accumulateValue <Type, FirstValue, Operator, Rest...> :: value;
   #endif
  };
 }
