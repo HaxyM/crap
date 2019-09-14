73215a9d08ba2df8125da667e391453520d6de49
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-14 17:15:47 +0200

    Add searchAllForType.

---
+++ algorithm/searchallfortype.h
@@ -0,0 +1,17 @@
+#ifndef CRAP_ALGORITHM_SEARCHALLFORTYPE
+#define CRAP_ALGORITHM_SEARCHALLFORTYPE
+
+#include "searchallvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class Operator = comparatorsForType <Type> :: template EqualTo>
+	 struct searchAllForType
+ {
+  template <Type ... Pattern> using type = searchAllForType<Type, Operator, Pattern...>;
+  template <Type ... Pattern> using pattern = searchAllForType<Type, Operator, Pattern...>;
+ };
+}
+#endif
+
