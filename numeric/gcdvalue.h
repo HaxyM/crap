5951ca1df969624e7acf1679d9ac950b5e58b9d3
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-28 23:28:05 +0200

    Add gcd operator operating on values.

---
+++ numeric/gcdvalue.h
@@ -0,0 +1,104 @@
+#ifndef CRAP_NUMERIC_GCDVALUE
+#define CRAP_NUMERIC_GCDVALUE
+
+#include "../utility/language.h"
+#include "reducevalue.h"
+
+#if CPP17
+#include <numeric>
+#else
+#include <type_traits>
+#endif
+
+namespace crap
+{
+ template <class Type, Type...> struct gcdValue;
+
+ template <class Type> struct gcdValue<Type>
+ {
+  constexpr const static Type value = static_cast<Type>(0);
+ };
+
+ template <class Type, Type Value> struct gcdValue<Type, Value>
+ {
+  constexpr const static Type value = Value;
+ };
+
+ template <class Type, Type Value> struct gcdValue<Type, Value, Value>
+ {
+  constexpr const static Type value = Value;
+ };
+
+ template <class Type, Type Value1, Type Value2> struct gcdValue<Type, Value1, Value2>
+ {
+  #if CPP17
+  constexpr const static Type value = std :: gcd(Value1, Value2);
+  #else
+  private:
+  template <Type SubValue> constexpr const static Type
+	  getValue(std :: integral_constant<Type, SubValue>, std :: integral_constant<Type, static_cast<Type>(0)>);
+  template <Type SubValue> constexpr const static Type
+	  getValue(std :: integral_constant<Type, static_cast<Type>(0)>, std :: integral_constant<Type, SubValue>);
+  template <Type SubValue> constexpr const static Type
+	  getValue(std :: integral_constant<Type, SubValue>, std :: integral_constant<Type, static_cast<Type>(1)>);
+  template <Type SubValue> constexpr const static Type
+	  getValue(std :: integral_constant<Type, static_cast<Type>(1)>, std :: integral_constant<Type, SubValue>);
+  template <Type SubValue> constexpr const static Type
+	  getValue(std :: integral_constant<Type, SubValue>, std :: integral_constant<Type, SubValue>);
+  template <Type SubValue1, Type SubValue2> constexpr const static Type
+	  getValue(std :: integral_constant<Type, SubValue1>, std :: integral_constant<Type, SubValue2>);
+  public:
+  constexpr const static Type value =
+	  getValue(std :: integral_constant<Type, Value1>{}, std :: integral_constant<Type, Value2>{});
+  #endif
+ };
+
+ template <class Type, Type ... Values> struct gcdValue
+ {
+  private:
+  template <Type ... SubValues> using This = gcdValue<Type, SubValues...>;
+  public:
+  constexpr const static Type value = reduceValue <Type, This, Values...> :: value;
+ };
+}
+
+#if CPP17
+#else
+template <class Type, Type Value1, Type Value2> template <Type SubValue> inline constexpr const Type
+	crap :: gcdValue <Type, Value1, Value2> :: getValue(std :: integral_constant<Type, SubValue>, std :: integral_constant<Type, static_cast<Type>(0)>)
+{
+ return SubValue;
+}
+
+template <class Type, Type Value1, Type Value2> template <Type SubValue> inline constexpr const Type
+	crap :: gcdValue <Type, Value1, Value2> :: getValue(std :: integral_constant<Type, static_cast<Type>(0)>, std :: integral_constant<Type, SubValue>)
+{
+ return SubValue;
+}
+
+template <class Type, Type Value1, Type Value2> template <Type SubValue> inline constexpr const Type
+	crap :: gcdValue <Type, Value1, Value2> :: getValue(std :: integral_constant<Type, SubValue>, std :: integral_constant<Type, static_cast<Type>(1)>)
+{
+ return static_cast<Type>(1);
+}
+
+template <class Type, Type Value1, Type Value2> template <Type SubValue> inline constexpr const Type
+	crap :: gcdValue <Type, Value1, Value2> :: getValue(std :: integral_constant<Type, static_cast<Type>(1)>, std :: integral_constant<Type, SubValue>)
+{
+ return static_cast<Type>(1);
+}
+
+template <class Type, Type Value1, Type Value2> template <Type SubValue> inline constexpr const Type
+	crap :: gcdValue <Type, Value1, Value2> :: getValue(std :: integral_constant<Type, SubValue>, std :: integral_constant<Type, SubValue>)
+{
+ return SubValue;
+}
+
+template <class Type, Type Value1, Type Value2> template <Type SubValue1, Type SubValue2> inline constexpr const Type
+	crap :: gcdValue <Type, Value1, Value2> :: getValue(std :: integral_constant<Type, SubValue1>, std :: integral_constant<Type, SubValue2>)
+{
+ return crap :: gcdValue <Type, Value1, Value2> :: getValue(std :: integral_constant<Type, (SubValue1 < SubValue2) ? SubValue1 : (SubValue1 - SubValue2)>{}, std :: integral_constant<Type, (SubValue2 < SubValue1) ? SubValue2 : (SubValue2 - SubValue1)>{});
+}
+#endif
+#endif
+
