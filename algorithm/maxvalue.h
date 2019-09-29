f2c3914f9050a692dc74f36e94bc1b2a575731aa
Igor Zarzycki <izarzycki@dmcs.pl>
2019-09-29 23:37:32 +0200

    Add max operator acting on values.

---
+++ algorithm/maxvalue.h
@@ -0,0 +1,33 @@
+#ifndef CRAP_ALGORITHM_MAXVALUE
+#define CRAP_ALGORITHM_MAXVALUE
+
+#include "../numeric/reducevalue.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class, Type...> struct maxValue;
+
+ template <class Type, template <Type, Type> class Operator, Type Value>
+	 struct maxValue<Type, Operator, Value>
+ {
+  constexpr const static Type value = Value;
+ };
+
+ template <class Type, template <Type, Type> class Operator, Type Value1, Type Value2>
+	 struct maxValue<Type, Operator, Value1, Value2>
+ {
+  constexpr const static Type value = ((Operator <Value2, Value1> :: value) ? Value1 : Value2);
+ };
+
+ template <class Type, template <Type, Type> class Operator, Type ... Values>
+	 struct maxValue
+ {
+  private:
+  static_assert(sizeof...(Values) > 0u, "Extremum of empty set is not defined.");
+  template <Type ... SubValues> using This = maxValue<Type, Operator, SubValues...>;
+  public:
+  constexpr const static Type value = reduce <Type, Operator, Values...> :: value;
+ };
+}
+#endif
+
