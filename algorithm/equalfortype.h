51aafcc3f103a8665ed04f87063a2695f72a5cac
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2019-10-12 23:56:15 +0200

    Add equalForType

---
+++ algorithm/equalfortype.h
@@ -0,0 +1,15 @@
+#ifndef CRAP_ALGORITHM_EQUALFORTYPE
+#define CRAP_ALGORITHM_EQUALFORTYPE
+
+#include "equalvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class Operator = comparatorsForType <Type> :: template EqualTo> struct equalForType
+ {
+  template <Type ... Values> using type = equalValue<Type, Operator, Values...>;
+  template <Type ... Values> using values = equalValue<Type, Operator, Values...>;
+ };
+}
+#endif
