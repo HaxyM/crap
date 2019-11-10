343303de00b268481d0810e211945c78b837b3e9
Igor Zarzycki <izarzycki@dmcs.pl>
2019-11-10 23:36:58 +0100

    Add mergeForType.

---
+++ algorithm/mergefortype.h
@@ -0,0 +1,17 @@
+#ifndef CRAP_ALGORITHM_MERGEFORTYPE
+#define CRAP_ALGORITHM_MERGEFORTYPE
+
+#include "mergrvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class Operator = comparatorsForType <Type> :: template Less>
+	 struct mergeForType
+ {
+  template <Type ... Values> using type = mergeValue<Type, Operator, Values...>;
+  template <Type ... Values> using values = mergeValue<Type, Operator, Values...>;
+ };
+}
+#endif
+
