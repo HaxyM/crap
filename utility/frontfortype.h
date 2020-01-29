1952de9a44900d80e8e524bee60f681a502152c7
Igor Zarzycki <izarzycki@dmcs.pl>
2020-01-29 20:37:13 +0100

    Add frontForType.

---
+++ utility/frontfortype.h
@@ -0,0 +1,15 @@
+#ifndef CRAP_UTILITY_FRONTFORTYPE
+#define CRAP_UTILITY_FRONTFORTYPE
+
+#include "frontvalue.h"
+
+namespace crap
+{
+ template <class Type> struct frontForType
+ {
+  template <Type ... Values> using type = frontValue<Type, Values...>;
+  template <Type ... Values> using values = frontValues<Type, Values...>;
+ };
+}
+#endif
+
