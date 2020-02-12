757927a28ec895f4184295ee07e5fc94e06a0044
Igor Zarzycki <izarzycki@dmcs.pl>
2020-02-12 20:33:26 +0100

    Fix pushHeap algorithm operationg on vlues to use swapIndices.

--- algorithm/pushheapvalue.h
+++ algorithm/pushheapvalue.h
@@ -2,6 +2,7 @@
 #define CRAP_ALGORITHM_PUSHHEAPVALUE
 
 #include "copyfortype.h"
+#include "../utility/swapindicesvalue.h"
 #include "../utility/valuelist.h"
 #include "../utility/valuelistfortype.h"
 #include "../utility/valuedemultiplexer.h"
@@ -42,79 +43,15 @@ namespace crap
   constexpr const static Type parent = subValues :: template At <ParentIndex> :: value;
   constexpr const static Type child = subValues :: template At <ChildIndex> :: value;
   constexpr const static bool needSwap = Operator <parent, child> :: value;
-  template <std :: size_t parentIndex, Type Parent, Type Child, Type ... NewValues> struct swap;
   template <Type ... NewValues> using NextStep = Implementation<ParentIndex, NewValues...>;
   template <Type ... NewValues>
-	  using goWithSwap = typename swap <ParentIndex, parent, child, NewValues...> :: template type<NextStep>;
+	  using goWithSwap = typename swapIndicesValue <Type, ParentIndex, ChildIndex, NewValues...> :: template type<NextStep>;
   template <template <template <Type...> class> class Source>
 	  using selector = valueDemultiplexer<Type, needSwap, Source, goWithSwap, copyForType <Type> :: template type>;
   template <template <Type...> class Container> using Source = Container<SubValues...>;
   public:
   template <template <Type...> class Container = valueListForType <Type> :: template type>
 	  using type = typename selector <Source> :: type :: template type<Container>;
- };
-
- template <class Type, template <Type, Type> class Operator, Type ... Values>
-	 template <std :: size_t ChildIndex, Type ... SubValues>
-	 template <std :: size_t parentIndex, Type Parent, Type Child, Type ... NewValues>
- struct pushHeapValue <Type, Operator, Values...> :: template
- 	Implementation <ChildIndex, SubValues...> :: swap
- {
-  private:
-  using newValues = valueList<Type, NewValues...>;
-  template <Type ... Ancestors> struct withAncestors;
-  template <Type ... Sibblings>
-	  using ancestors = typename newValues :: template
-	  till<parentIndex, withAncestors> :: template withSibblings<Sibblings...>;
-  template <Type ... Offsprings>
-	  using sibblings = typename newValues :: template
-	  subRange <parentIndex + 1u, ChildIndex, ancestors> :: template
-	  withOffsprings<Offsprings...>;
-  public:
-  template <template <Type...> class Container>
-	  using type = typename newValues :: template
-	  since <ChildIndex + 1u, sibblings> :: template
-	  type<Container>;
- };
-
- template <class Type, template <Type, Type> class Operator, Type ... Values>
-	 template <std :: size_t ChildIndex, Type ... SubValues>
-	 template <std :: size_t parentIndex, Type Parent, Type Child, Type ... NewValues>
-	 template <Type ... Ancestors>
- struct pushHeapValue <Type, Operator, Values...> :: template
- 	Implementation <ChildIndex, SubValues...> :: template
- 	swap <parentIndex, Parent, Child, NewValues...> :: withAncestors
- {
-  template <Type ... Sibblings> struct withSibblings;
- };
-
- template <class Type, template <Type, Type> class Operator, Type ... Values>
-	 template <std :: size_t ChildIndex, Type ... SubValues>
-	 template <std :: size_t parentIndex, Type Parent, Type Child, Type ... NewValues>
-	 template <Type ... Ancestors>
-	 template <Type ... Sibblings>
- struct pushHeapValue <Type, Operator, Values...> :: template
- 	Implementation <ChildIndex, SubValues...> :: template
- 	swap <parentIndex, Parent, Child, NewValues...> :: template
- 	withAncestors <Ancestors...> :: withSibblings
- {
-  template <Type ... Offsprings> struct withOffsprings;
- };
-
- template <class Type, template <Type, Type> class Operator, Type ... Values>
-	 template <std :: size_t ChildIndex, Type ... SubValues>
-	 template <std :: size_t parentIndex, Type Parent, Type Child, Type ... NewValues>
-	 template <Type ... Ancestors>
-	 template <Type ... Sibblings>
-	 template <Type ... Offsprings>
- struct pushHeapValue <Type, Operator, Values...> :: template
- 	Implementation <ChildIndex, SubValues...> :: template
- 	swap <parentIndex, Parent, Child, NewValues...> :: template
- 	withAncestors <Ancestors...> :: template
- 	withSibblings <Sibblings...> :: withOffsprings
- {
-  template <template <Type...> class Container>
-	  using type = Container<Ancestors..., Child, Sibblings..., Parent, Offsprings...>;
  };
 }
 #endif
