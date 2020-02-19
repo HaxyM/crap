5c917e3ed3345f5d2c4a2bf6ec2119e39796af31
Igor Zarzycki <izarzycki@dmcs.pl>
2020-02-19 23:35:40 +0100

    Add setIntersectionForType.

---
+++ algorithm/setintersectionfortype.h
@@ -0,0 +1,17 @@
+#ifndef CRAP_ALGORITHM_SETINTERSECTIONFORTYPE
+#define CRAP_ALGORITHM_SETINTERSECTIONFORTYPE
+
+#include "setintersectionvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class Operator = comparatorsForType <Type> :: template Less>
+	 struct setIntersectionForType
+ {
+  template <Type ... Values> using type = setIntersectionValue<Type, Operator, Values...>;
+  template <Type ... Values> using values = setIntersectionValue<Type, Operator, Values...>;
+ };
+}
+#endif
+
