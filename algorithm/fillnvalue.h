be9928ae56979ca07bfb2dae1f541578be05e7f8
Igor Zarzycki <izarzycki@dmcs.pl>
2020-02-21 20:43:03 +0100

    Improve fillN algorithm operating on values.

--- algorithm/fillnvalue.h
+++ algorithm/fillnvalue.h
@@ -4,23 +4,39 @@
 #include <utility>
 
 #include "../utility/makeindexsequence.h"
+#include "../utility/valuecontaineridentity.h"
 #include "../utility/valuelistfortype.h"
 
 namespace crap
 {
  template <class Type, std :: size_t N, Type Value> struct fillNValue
  {
+  #if defined(__clang__)
   private:
   template <template <Type...> class Container> struct generator;
   public:
   template <template <Type...> class Container = valueListForType <Type> :: template type>
-	  using type = decltype(generator <Container> :: generate(makeIndexSequence<N>{}));
+	  using type = typename generator <Container> :: type;
+  #else
+  private:
+  template <template <Type...> class Container, std :: size_t ... Indices>
+	  static Container<(Indices, Value)...> generate(valueContainerIdentity<Type, Container>, indexSequence<Indices...>);
+  public:
+  template <template <Type...> class Container = valueListForType <Type> :: template type>
+	  using type = decltype(generate(valueContainerIdentity<Type, Container>{}, makeIndexSequence<N>{}));
+  #endif
  };
 
- template <class Type, std :: size_t N, Type Value> template <template <Type...> class Container> struct fillNValue <Type, N, Value> :: generator
+ #if defined(__clang__)
+ template <class Type, std :: size_t N, Type Value>
+	 template <template <Type...> class Container>
+ struct fillNValue <Type, N, Value> :: generator
  {
-  template <std :: size_t ... Indices> static Container<(Indices, Value)...> generate(indexSequence<Indices...>);
+  template <std :: size_t ... Indices> static Container<(Indices,Value)...>
+	  generate(indexSequence<Indices...>);
+  using type = decltype(generate(makeIndexSequence<N>{}));
  };
+ #endif
 }
 #endif
 
