1ad2d5f470cb249150962783218e619514fd9d27
Igor Zarzycki <izarzycki@dmcs.pl>
2020-02-21 20:22:53 +0100

    Add valueContainerIdentity

---
+++ utility/valuecontaineridentity.h
@@ -0,0 +1,9 @@
+#ifndef CRAP_UTILITY_VALUECONTAINERIDENTITY
+#define CRAP_UTILITY_VALUECONTAINERIDENTITY
+
+namespace crap
+{
+ template <class Type, template <Type...> class> struct valueContainerIdentity{};
+}
+#endif
+
