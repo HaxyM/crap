b1fe552bbb423747084bfd23b5a714fdc7e3c68e
Igor Zarzycki <izarzycki@dmcs.pl>
2020-03-09 13:18:07 +0100

    Improve lesEqual operator acting on values.

--- functional/lessequalvalue.h
+++ functional/lessequalvalue.h
@@ -1,12 +1,11 @@
 #ifndef CRAP_FUNCTIONAL_LESSEQUALVALUE
 #define CRAP_FUNCTIONAL_LESSEQUALVALUE
 
+#include <type_traits>
+
 namespace crap
 {
- template <class Type, Type Value1, Type Value2> struct lessEqualValue
- {
-  constexpr const static bool value = (Value1 <= Value2);
- };
+ template <class Type, Type Value1, Type Value2> using lessEqualValue = std :: integral_constant<bool, (Value1 <= Value2)>;
 }
 #endif
 
