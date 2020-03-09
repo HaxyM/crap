5c4213d7e28ec025993450051656aed63f6bd1a9
Igor Zarzycki <izarzycki@dmcs.pl>
2020-03-09 12:51:06 +0100

    Improve equalTo operator acting on values.

--- functional/equaltovalue.h
+++ functional/equaltovalue.h
@@ -1,12 +1,11 @@
 #ifndef CRAP_FUNCTIONAL_EQUALTOVALUE
 #define CRAP_FUNCTIONAL_EQUALTOVALUE
 
+#include <type_traits>
+
 namespace crap
 {
- template <class Type, Type Value1, Type Value2> struct equalToValue
- {
-  constexpr const static bool value = (Value1 == Value2);
- };
+ template <class Type, Type Value1, Type Value2> using equalToValue = std :: integral_constant<bool, (Value1 == Value2)>;
 }
 #endif
 
