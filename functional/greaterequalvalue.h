0a0bd8f017b88f2e5aa3e736d7f81ebb7819ef1f
Igor Zarzycki <izarzycki@dmcs.pl>
2020-03-09 13:38:25 +0100

    Improve greaterEqual operator acting on values.

--- functional/greaterequalvalue.h
+++ functional/greaterequalvalue.h
@@ -1,12 +1,11 @@
 #ifndef CRAP_FUNCTIONAL_GREATEREQUALVALUE
 #define CRAP_FUNCTIONAL_GREATEREQUALVALUE
 
+#include <type_traits>
+
 namespace crap
 {
- template <class Type, Type Value1, Type Value2> struct greaterEqualValue
- {
-  constexpr const static bool value = (Value1 >= Value2);
- };
+ template <class Type, Type Value1, Type Value2> using greaterEqualValue = std :: integral_constant<bool, (Value1 >= Value2)>;
 }
 #endif
 
