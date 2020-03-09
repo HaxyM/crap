d646d77a945a5f5e7e658fd0348ca9ea1821a2ca
Igor Zarzycki <izarzycki@dmcs.pl>
2020-03-09 11:37:05 +0100

    fix concatenate algorithm operating in values to support multiple concatenations.

--- utility/concatenatevalue.h
+++ utility/concatenatevalue.h
@@ -7,14 +7,9 @@ namespace crap
 {
  template <class Type, Type ... Values1> struct concatenateValue
  {
-  template <Type ... Values2> struct with;
- };
-
- template <class Type, Type ... Values1> template <Type ... Values2>
-	 struct concatenateValue <Type, Values1...> :: with
- {
+  template <Type ... Values2> using with = concatenateValue<Type, Values1..., Values2...>;
   template <template <Type...> class Container = valueListForType <Type> :: template type>
-	  using type = Container<Values1..., Values2...>;
+	  using type = Container<Values1...>;
  };
 }
 #endif
