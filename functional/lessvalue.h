1c0c357d5002233dfa177d95000216f154f6b8e8
Igor Zarzycki <izarzycki@dmcs.pl>
2020-03-09 12:18:58 +0100

    Improve less operator acting on values.

--- functional/lessvalue.h
+++ functional/lessvalue.h
@@ -1,12 +1,11 @@
 #ifndef CRAP_FUNCTIONAL_LESSVALUE
 #define CRAP_FUNCTIONAL_LESSVALUE
 
+#include <type_traits>
+
 namespace crap
 {
- template <class Type, Type Value1, Type Value2> struct lessValue
- {
-  constexpr const static bool value = (Value1 < Value2);
- };
+ template <class Type, Type Value1, Type Value2> using lessValue = std :: integral_constant<bool, (Value1 < Value2)>;
 }
 #endif
 
