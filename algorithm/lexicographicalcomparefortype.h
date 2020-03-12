b5331985fbd5875464fa7e7430654a243b60994e
Igor Zarzycki <izarzycki@dmcs.pl>
2020-03-12 16:26:19 +0100

    Add detailedResult_t to lexicographicalCompareForType.

--- algorithm/lexicographicalcomparefortype.h
+++ algorithm/lexicographicalcomparefortype.h
@@ -9,6 +9,7 @@ namespace crap
  template <class Type, template <Type, Type> class Operator = comparatorsForType <Type> :: template Less>
 	 struct lexicographicalCompareForType
  {
+  using detailedResult_t = typename lexicographicalCompareValue <Type, Operator> :: detailedResult_t;
   template <Type ... Values> using type = lexicographicalCompareValue<Type, Operator, Values...>;
   template <Type ... Values> using values = lexicographicalCompareValue<Type, Operator, Values...>;
  };
