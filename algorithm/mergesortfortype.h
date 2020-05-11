8938dbb7061674a7196ff6136aac51cca6d2922f
Igor Zarzycki <Igor.Zarzycki@harman.com>
2020-05-11 20:02:32 +0200

    Fix mergeSortForType typo.

--- algorithm/mergesortfortype.h
+++ algorithm/mergesortfortype.h
@@ -1,15 +1,15 @@
 #ifndef CRAP_ALGORITHM_MERGESORTFORTYPE
 #define CRAP_ALGORITHM_MERGESORTFORTYPE
 
-#include "mergesort.h"
+#include "mergesortvalue.h"
 #include "../functional/comparatorsfortype.h"
 
 namespace crap
 {
  template <class Type, template <Type, Type> class Operator = comparatorsForType <Type> :: template Less> struct mergeSortForType
  {
-  template <Type ... Values> using type = mergeSort<Type, Operator, Values...>;
-  template <Type ... Values> using values = mergeSort<Type, Operator, Values...>;
+  template <Type ... Values> using type = mergeSortValue<Type, Operator, Values...>;
+  template <Type ... Values> using values = mergeSortValue<Type, Operator, Values...>;
  };
 }
 #endif
