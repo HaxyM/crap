02ca3243602a2df92421bdb9fd226cfb6f1f7ae8
Igor Zarzycki <izarzycki@dmcs.pl>
2020-05-13 01:34:05 +0200

    Add string.

---
+++ string/string.h
@@ -0,0 +1,31 @@
+#ifndef CRAP_STRING_STRING
+#define CRAP_STRING_STRING
+
+#include "../utility/valuelist.h"
+
+namespace crap
+{
+ template <class Char, Char ... Letters> struct string : valueList<Char, Letters...>
+ {
+  private:
+  constexpr const static Char letters[] = {Letters..., static_cast<Char>('\0')};
+  public:
+  constexpr const static Char (& c_str() noexcept)[sizeof...(Letters) + 1u];
+ };
+
+ template <class Char, Char ... Letters> constexpr string<Char, Letters...> operator "" _string () noexcept;
+}
+
+template <class Char, Char ... Letters> constexpr const Char crap :: string <Char, Letters...> :: letters[];
+
+template <class Char, Char ... Letters> inline constexpr const Char (& crap :: string <Char, Letters...> :: c_str() noexcept)[sizeof...(Letters) + 1u]
+{
+ return crap :: string <Char, Letters...> :: letters;
+}
+
+template <class Char, Char ... Letters> inline constexpr crap :: string<Char, Letters...> crap :: operator "" _string () noexcept
+{
+ return {};
+}
+#endif
+
