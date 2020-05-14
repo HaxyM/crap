11f41db7a50bd9e767ac95704fb79a683adefa78
Igor Zarzycki <izarzycki@dmcs.pl>
2020-05-14 23:41:05 +0200

    Add lowerBoundForType

---
+++ algorithm/lowerboundfortype.h
@@ -0,0 +1,16 @@
+#ifndef CRAP_ALGORITHM_LOWERBOUNDFORTYPE
+#define CRAP_ALGORITHM_LOWERBOUNDFORTYPE
+
+#include "lowerboundvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, Type Value, template <Type, Type> class Operator = comparatorsForType <Type> :: template Less> struct lowerBoundForType
+ {
+  template <Type ... Values> using type = lowerBoundValue<Type, Value, Operator, Values...>;
+  template <Type ... Values> using values = lowerBoundValue<Type, Value, Operator, Values...>;
+ };
+}
+#endif
+
