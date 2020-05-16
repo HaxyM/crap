36eb733ae6f32e6a7f1550a1964f74d92f7c4ba1
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2020-05-16 16:16:03 +0200

    Improve fill algorithm operating on values.

--- algorithm/fillvalue.h
+++ algorithm/fillvalue.h
@@ -1,15 +1,11 @@
 #ifndef CRAP_ALGORITHM_FILLVALUE
 #define CRAP_ALGORITHM_FILLVALUE
 
-#include "../utility/valuelistfortype.h"
+#include "copyvalue.h"
 
 namespace crap
 {
- template <class Type, Type Value, Type ... Values> struct fillValue
- {
-  template <template <Type...> class Container = valueListForType <Type> :: template type>
-	  using type = Container<(Values,Value)...>;
- };
+ template <class Type, Type Value, Type ... Values> using fillValue = copyValue<Type, (Values, Value)...>;
 }
 #endif
 
