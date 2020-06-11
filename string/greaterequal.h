b5c7bdc95f225e30ed8330c4d2d2e83520fb692b
Igor Zarzycki <izarzycki@dmcs.pl>
2020-06-11 19:40:53 +0200

    Add greaterEqual operator acting on strings.

---
+++ string/greaterequal.h
@@ -0,0 +1,24 @@
+#ifndef CRAP_STRING_GREATEREQUAL
+#define CRAP_STRING_GREATEREQUAL
+
+#include "string.h"
+#include "../algorithm/lexicographicalcomparefortype.h"
+#include "../functional/greaterequaltype.h"
+
+namespace crap
+{
+ template <class Type, Type ... Letters1, Type ... Letters2>
+	 struct greaterEqualType<string<Type, Letters1...>, string<Type, Letters2...> >
+	 : std :: integral_constant<bool, lexicographiclalCompareForType <Type> :: template type <Letters1...> :: template with <Letters2...> :: value !=
+	 lexicographicalCompareForType <Type> :: detailedResult_t :: less> {};
+
+ template <class Type, Type ... Letters1, Type ... Letters2> constexpr bool operator >= (string<Type, Letters1...>, string<Type, Letters2...>) noexcept;
+}
+
+template <class Type, Type ... Letters1, Type ... Letters2>
+inline constexpr bool crap :: operator >= (crap :: string<Type, Letters1...>, crap :: string<Type, Letters2...>) noexcept
+{
+ return crap :: greaterEqualType <crap :: string<Type, Letters1...>, crap :: string<Type, Letters2...> > :: value;
+}
+#endif
+
