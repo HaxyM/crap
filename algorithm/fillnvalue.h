344f58ccb088faa5669cf58c1d9d347b55bf4b0d
Igor Zarzycki <izarzycki@dmcs.pl>
2020-06-24 13:40:42 +0200

    Fix typos in fillN algorithm operating on values.

--- algorithm/fillnvalue.h
+++ algorithm/fillnvalue.h
@@ -2,7 +2,7 @@
 #define CRAP_ALGORITHM_FILLNVALUE
 
 #include "../algorithm/fillfortype.h"
-#include "../utility/concatenatevalue.h"
+#include "../utility/concatenatefortype.h"
 #include "../utility/splitvalue.h"
 #include "../utility/valuelistfortype.h"
 
@@ -11,11 +11,11 @@ namespace crap
  template <std :: size_t N, class Type, Type Value, Type ... Values> struct fillNValue
  {
   private:
-  constexpr const static pivot = ((sizeof...(Values) < N) ? (sizeof...(Values)) : N);
-  using split = splitValue<N, Type, Values...>;
+  constexpr const static std :: size_t pivot = (((sizeof...(Values)) < N) ? sizeof...(Values) : N);
+  using split = splitValue<pivot, Type, Values...>;
   template <template <Type...> class Container>
 	  using lower = typename split :: template
-	  lower <fillForType <Type> :: template type> :: template type<Container>;
+	  lower <fillForType <Type, Value> :: template type> :: template type<Container>;
   public:
   template <template <Type...> class Container = valueListForType <Type> :: template type>
 	  using type = typename split :: template
