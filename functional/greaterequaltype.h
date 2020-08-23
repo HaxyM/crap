acc16012b13ea6a4a2a8f262327c240e437793f4
Igor Zarzycki <izarzycki@dmcs.pl>
2020-08-23 23:12:57 +0200

    Add comparison operators acting on std :: ratio.

--- functional/greaterequaltype.h
+++ functional/greaterequaltype.h
@@ -1,6 +1,7 @@
 #ifndef CRAP_FUNCTIONAL_GREATEREQUALTYPE
 #define CRAP_FUNCTIONAL_GREATEREQUALTYPE
 
+#include <ratio>
 #include <type_traits>
 
 namespace crap
@@ -10,6 +11,10 @@ namespace crap
  template <class Type1, class Type2, Type1 Value1, Type2 Value2>
 	 struct greaterEqualType<std :: integral_constant<Type1, Value1>, std :: integral_constant<Type2, Value2> >
 	 : std :: integral_constant<bool, (Value1 >= Value2)> {};
+ template <intmax_t Numerator1, intmax_t Denominator1, intmax_t Numerator2, intmax_t Denominator2>
+	 struct greaterEqualType<std :: ratio<Numerator1, Denominator1>, std :: ratio<Numerator2, Denominator2> >
+	 : std :: ratio_greater_equal<std :: ratio<Numerator1, Denominator1>, std :: ratio<Numerator2, Denominator2> >
+	 {};
 }
 #endif
 
--- functional/notequaltotype.h
+++ functional/notequaltotype.h
@@ -1,6 +1,7 @@
 #ifndef CRAP_FUNCTIONAL_NOTEQUALTOTYPE
 #define CRAP_FUNCTIONAL_NOTEQUALTOTYPE
 
+#include <ratio>
 #include <type_traits>
 
 namespace crap
@@ -10,6 +11,10 @@ namespace crap
  template <class Type1, class Type2, Type1 Value1, Type2 Value2>
 	 struct notEqualToType<std :: integral_constant<Type1, Value1>, std :: integral_constant<Type2, Value2> >
 	 : std :: integral_constant<bool, (Value1 != Value2)> {};
+ template <intmax_t Numerator1, intmax_t Denominator1, intmax_t Numerator2, intmax_t Denominator2>
+	 struct notEqualToType<std :: ratio<Numerator1, Denominator1>, std :: ratio<Numerator2, Denominator2> >
+	 : std :: ratio_not_equal<std :: ratio<Numerator1, Denominator1>, std :: ratio<Numerator2, Denominator2> >
+	 {};
 }
 #endif
 
