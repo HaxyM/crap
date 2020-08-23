5abf39dfa9f507b83a0ead2a16e8cea2662c4c27
Igor Zarzycki <izarzycki@dmcs.pl>
2020-08-23 23:12:57 +0200

    Add comparison operators acting on std :: ratio.

--- functional/lesstype.h
+++ functional/lesstype.h
@@ -1,6 +1,7 @@
 #ifndef CRAP_FUNCTIONAL_LESSTYPE
 #define CRAP_FUNCTIONAL_LESSTYPE
 
+#include <ratio>
 #include <type_traits>
 
 namespace crap
@@ -10,6 +11,9 @@ namespace crap
  template <class Type1, class Type2, Type1 Value1, Type2 Value2>
 	 struct lessType<std :: integral_constant<Type1, Value1>, std :: integral_constant<Type2, Value2> >
 	 : std :: integral_constant<bool, (Value1 < Value2)> {};
+ template <intmax_t Numerator1, intmax_t Denominator1, intmax_t Numerator2, intmax_t Denominator2>
+	 struct lessType<std :: ratio<Numerator1, Denominator1>, std :: ratio<Numerator2, Denominator2> >
+	 : std :: ratio_less<std :: ratio<Numerator1, Denominator1>, std :: ratio<Numerator2, Denominator2> > {};
 }
 #endif
 
