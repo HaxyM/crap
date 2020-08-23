52fb0bef8e6ce8c22f0f6b654be8adebab15e477
Igor Zarzycki <izarzycki@dmcs.pl>
2020-08-24 00:27:21 +0200

    Add equalTo operator acting on ratio.

---
+++ ratio/equaltotype.h
@@ -0,0 +1,34 @@
+#ifndef CRAP_RATIO_EQUALTOTYPE
+#define CRAP_RATIO_EQUALTOTYPE
+
+#include <type_traits>
+
+#include "contracttype.h"
+#include "valueratio.h"
+#include "../functional/equaltotype.h"
+
+namespace crap
+{
+ template <class Type,
+	  typename std :: make_signed <Type> :: type Numerator1, typename std :: make_unsigned <Type> :: type Denomiantor1,
+	  typename std :: make_signed <Type> :: type Numerator2, typename std :: make_unsigned <Type> :: type Denominator2>
+	 struct equalToType<valueRatio<Type, Numerator1, Denominator1>, valueRatio<Type, Numerator2, Denominator2> >
+	 : std :: integral_constant<bool,
+		(contractType <valueRatio<Type, Numerator1, Denominator1> > :: type :: num == contractType <valueRatio<Type, Numerator2, Denominator2> > :: type :: num)
+		 && (contractType <valueRatio<Type, Numerator1, Denominator1> > :: type :: den == contractType <valueRatio<Type, Numerator2, Denominator2> > :: type :: den)>
+	{};
+
+ template <class Type1, typename std :: make_signed <Type1> :: type Numerator1, typename std :: make_unsigned <Type1> :: type Denominator1,
+	  class Type2, typename std :: make_signed <Type2> :: type Numerator2, typename std :: make_unsigned <Type2> :: type Denominator2>
+	 struct equalToType<valueRatio<Type1, Numerator1, Denominator1>, valueRatio<Type2, Numerator2, Denominator2>>
+	 : equalToType<
+		valueRatio<typename std :: common_type <Type1, Type2> :: type,
+		static_cast<typename std :: make_signed <typename std :: common_type <Type1, Type2> :: type> :: type>(Numerator1),
+		static_cast<typename std :: make_unsigned <typename std :: common_type <Type1, Type2> :: type> :: type>(Denominator1)>,
+		valueRatio<typename std :: common_type <Type1, Type2> :: type,
+		static_cast<typename std :: make_signed <typename std :: common_type<Type1, Type2> :: type> :: type>(Numerator2),
+		static_cast<typename std :: make_unsigned <typename std :: common_type <Type1, Type2> :: type> :: type>(Denominator2)> >
+	{};
+}
+#endif
+
