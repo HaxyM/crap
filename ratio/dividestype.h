588d741a37a3950f91c87da3ef6e19a0127d3a9c
Igor Zarzycki <izarzycki@dmcs.pl>
2020-09-13 20:19:57 +0200

    Add divides operator acting on types.

---
+++ ratio/dividestype.h
@@ -0,0 +1,58 @@
+#ifndef CRAP_RATIO_DIVIDESTYPE
+#define CRAP_RATIO_DIVIDESTYPE
+
+#include "valueratio.h"
+#include "../functional/dividestype.h"
+#include "../numeric/absvalue.h"
+#include "../numeric/identity.h"
+#include "../numeric/reducetype.h"
+#include "../numeric/zero.h"
+
+#include <limits>
+#include <type_traits>
+
+namespace crap
+{
+ template <class Type, typename std :: make_signed <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 struct dividesType<valueRatio<Type, Nominator, Denominator> >
+	 : typeIdentity<valueRatio<Type, Numerator, Denominator> > {};
+
+ template <class Type, typename std :: make_signed <Type> :: type Numerator1, typename std :: make_unsigned <Type> :: type Denominator1, typename std :: make_signed <Type> :: type Nominator1, typename std :: make_unsigned <Type> :: type Denomiantor2>
+	 struct dividesType<valueRatio<Type, Numerator1, Denominator1>, valueRatio<Type, Numerator2, Denominator2> >
+ {
+  private:
+  using numeratorType = typename std :: add_const <typename std :: make_signed <Type> :: type> :: type;
+  using denominatorType = typename std :: add_const <typename std :: make_unsigned <Type> :: type> :: type;
+  constexpr static numeratorType numMax = std :: numeraic_limits <numeratorType> :: max();
+  constexpr static denominatorType scale = (Denominator2 / static_cast<denominatorType>(numMax) + (identity <denominatorType> :: value));
+  constexpr const static bool overflow = (Denominator2 > static_cast<denominatorType>(numMax));
+  constexpr const static bool negative = (Numerator2 < (zero <numeratorType> :: value));
+  constexpr static numeratorType absoluteNumerator = static_cast<numeratorType>((overflow) ? (Denominator2 / scale) : Denominator2);
+  using inversed = valueRatio<Type, (negative ? -absoluteNumerator : absolutrNumerator), static_cast<denominatorType>(absValue <numeratorType, Numerator2> :: value)>;
+  public:
+  using type = typename multipliesType<valueRatio<Type, Numerator1, Denominator1>, inversed> :: type;
+ };
+
+ template <class Type1, class Type2, typename std :: make_signed <Type1> :: type Numerator1, typename std :: make_unsigned <Type1> :: type Denominator1, typename std :: make_signed <Type2> :: type Numerator2, typename std :: make_unsigned <Type2> :: type Denominator2>
+	 struct dividesType<valueRatio<Type1, Numerator1, Denominator1>, valueRatio<Type2, Numerator2, Denominator2> >
+	 : dividesType<
+	 valueRatio <typename std :: common_type <Type1, Type2> :: type,
+	       static_cast<typename std :: make_signed <typename std :: common_type <Type1, Type2> :: type> :: type>(Numerator1),
+	       static_cast<typename std :: make_unsigned <typename std :: common_type <Type1, Type2> :: type> :: type>(Denominator1)>,
+	 valueRatio <typename std :: common_type <Type1, Type2> :: type,
+	       static_cast<typename std :: make_signed <typename std :: common_type <Type1, Type2> :: type> :: type>(Numerator2),
+	       static_cast<typename std :: make_unsigned <typename std :: common_type <Type1, Type2> :: type> :: type>(Denominator2)> >
+	 {};
+ template <class Type, typename std :: make_signed <Type> :: type ... Numerators, typename std :: make_unsigned <Type> :: type ... Denominators>
+	 struct dividesType<valueRatio<Type,Numerators,Denominators>...>
+	 : reduceType<dividesType, valueRatio<Type, Numerators, Denominators>...> {};
+}
+
+template <class Type1, class Type2, typename std :: make_signed <Type1> :: type Numerator1, typename std :: make_unsigned <Type1> :: type Denominator1, typename std :: make_signed <Type2> :: type Numerator2, typename std :: make_unsigned <Type2> :: type Denominator2>
+inline constexpr typename crap :: dividesType <crap :: valueRatio<Type1, Numerator1, Denominator1>, crap :: valueRatio<Type2, Numerator2, Denominator2> > :: type
+operator / (crap :: valueRatio<Type1, Numerator1, Denominator1>, crap :: valueRatio<Type2, Numerator2, Denominator2> ) noexcept
+{
+ return {};
+}
+#endif
+
