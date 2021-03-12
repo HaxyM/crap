12366edf08e6098a50eb90ae67f04b52cf1ac9f5
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2021-03-12 22:02:45 +0100

    Fix typo.

--- string/binaryshiftleft.h
+++ string/binaryshiftleft.h
@@ -8,7 +8,7 @@
 template <class ostreamChar, class Char, Char ... Letters>
 inline std :: basic_ostream<ostreamChar>& operator << (std :: basic_ostream<ostreamChar>& stream, const crap :: string<Char, Letters...>& string)
 {
- return (output << string.c_str());
+ return (stream << string.c_str());
 }
 #endif
 
