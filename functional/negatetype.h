45524576c8a77e8d6b4d9e094a848f51ba7a710f
Igor Zarzycki <izarzycki@dmcs.pl>
2021-03-15 23:40:35 +0100

    Add negate operator acting on types.

---
+++ functional/negatetype.h
@@ -0,0 +1,22 @@
+#ifndef CRAP_FUNCTIONAL_NEGATETYPE
+#define CRAP_FUNCTIONAL_NEGATETYPE
+
+#include "negatevalue.h"
+#include "../utility/typeidentity.h"
+
+#include <ratio>
+#include <type_traits>
+
+namespace crap
+{
+ template <class> struct negateType;
+
+ template <class Type, Type Value> struct negateType<std :: ingegral_constant<Type, Value> >
+	 : typeIdentity<std :: integral_constant<typename negateValue <Type, Value> :: value_type, negateValue <Type, Value> :: value> >
+ {};
+
+ template <intmax_t Numerator, intmax_t Denominator> struct negateType<std :: ratio<Numerator, Denominator> >
+	 : typeIdentity<std :: ratio_subtract<std :: ratio<0>, std :: ratio<Numerator, Denominator> > > {};
+}
+#endif
+
