6b5780607271201fed2af844858b8b21aa7a472f
Igor Zarzycki <izarzycki@dmcs.pl>
2021-03-17 22:56:41 +0100

    Improve multiplies operator acting on ratio.

--- ratio/multipliestype.h
+++ ratio/multipliestype.h
@@ -23,31 +23,33 @@ namespace crap
   using scaleType = const long double;
   using numeratorType = typename std :: add_const <typename std :: make_signed <Type> :: type> :: type;
   using denominatorType = typename std :: add_const <typename std :: make_unsigned <Type> :: type> :: type;
-  using contraction1 = typename contractType <valueRatio<Type, Numerator1, Denominator2> > :: type;
-  using contraction2 = typename contractType <valueRatio<Type, Numerator2, Denominator1> > :: type;
-  constexpr static numeratorType contraction1Num = contraction1 :: num;
-  constexpr static numeratorType contraction2Num = contraction2 :: num;
-  constexpr static denominatorType contraction1Den = contraction1 :: den;
-  constexpr static denominatorType contraction2Den = contraction2 :: den;
-  constexpr static numeratorType numMin = std :: numeric_limits <numeratorType> :: min();
+  constexpr static numeratorType minusOne = -(identity <numeratorType> :: value);
   constexpr static numeratorType numMax = std :: numeric_limits <numeratorType> :: max();
+  constexpr static numeratorType numMin = std :: numeric_limits <numeratorType> :: min();
   constexpr static denominatorType denMax = std :: numeric_limits <denominatorType> :: max();
-  constexpr const static bool numUnderflow = (((contraction1Num != zero <numeratorType> :: value) && (contraction1Num != -(identity <numeratorType> :: value))) ? ((contraction1Num < zero <numeratorType> :: value) ? (contraction2Num > (numMin / contraction1Num)) : (contraction2Num < (numMin / contraction1Num))) : false);
-  constexpr static const bool numOverflow = ((contraction1Num != zero <numeratorType> :: value) ? ((contraction1Num < zero <numeratorType> :: value) ? (contraction2Num < (numMax / contraction1Num)) : (contraction2Num > (numMax / contraction1Num))) : false);
-  constexpr static const bool denOverflow = ((contraction1Den != zero <denominatorType> :: value) ? (contraction2Den > (denMax / contraction1Den)) : false);
-  constexpr static const bool needScale = (numUnderflow || numOverflow || denOverflow);
-  constexpr static scaleType numUnderScale = (numUnderflow ? std :: abs(((static_cast<scaleType>(numMin)) / (static_cast<scaleType>(contraction1Num))) / (static_cast<scaleType>(contraction2Num))) : 1.0l);
-  constexpr static scaleType numOverScale = (numOverflow ? std :: abs(((static_cast<scaleType>(numMax)) / (static_cast<scaleType>(contraction1Num))) / ((const long double)(contraction2Num))) : 1.0l);
-  constexpr static scaleType denScale = (denOverflow ? (((static_cast<scaleType>(denMax)) / (static_cast<scaleType>(contraction1Den))) / (static_cast<scaleType>(contraction2Den))) : 1.0l);
-  constexpr static scaleType scale = ((numUnderScale < numOverScale) ? ((numUnderScale < denScale) ? numUnderScale : denScale) : ((numOverScale < denScale) ? numOverScale : denScale));
-  constexpr static scaleType contr1FloatNum = (static_cast<scaleType>((contraction1Num > contraction2Num) ? contraction1Num : contraction2Num));
-  constexpr static scaleType contr2FloatNum = (static_cast<scaleType>((contraction1Num > contraction2Num) ? contraction2Num : contraction1Num));
-  constexpr static scaleType contr1FloatDen = (static_cast<scaleType>((contraction1Den > contraction2Den) ? contraction1Den : contraction2Den));
-  constexpr static scaleType contr2FloatDen = (static_cast<scaleType>((contraction1Den > contraction2Den) ? contraction2Den : contraction1Den));
-  constexpr static numeratorType finNum = (needScale ? (static_cast<numeratorType>((scale * contr1FloatNum) * contr2FloatNum)) : (contraction1Num * contraction2Num));
-  constexpr static denominatorType finDen = (needScale ? (static_cast<denominatorType>((scale * contr1FloatDen) * contr2FloatDen)) : (contraction1Den * contraction2Den));
+  constexpr static scaleType infinity = std :: numeric_limits <scaleType> :: infinity();
+  constexpr static numeratorType contractedNum1 = contractType <valueRatio<Type, Numerator1, Denominator2> > :: type :: num;
+  constexpr static numeratorType contractedNum2 = contractType <valueRatio<Type, Numerator2, Denominator1> > :: type :: num;
+  constexpr static denominatorType contractedDen1 = contractType <valueRatio<Type, Numerator2, Denominator1> > :: type :: den;
+  constexpr static denominatorType contractedDen2 = contractType <valueRatio<Type, Numerator1, Denominator2> > :: type :: den;
+  constexpr const static bool numOverflow1 = (contractedNum1 > zero <numeratorType> :: value) && (contractedNum2 > zero <numeratorType> :: value) && (contractedNum2 > (numMax / contractedNum1));
+  constexpr const static bool numOverflow2 = (contractedNum1 == minusOne) && (contractedNum2 < zero <numeratorType> :: value) && (zero <numeratorType> :: value > (numMax + contractedNum2));
+  constexpr const static bool numOverflow3 = (contractedNum1 < minusOne) && (contractedNum2 < zero <numeratorType> :: value) && (contractedNum2 < (numMax / contractedNum1));
+  constexpr const static bool numUnderflow1 = (contractedNum1 > zero <numeratorType> :: value) && (contractedNum2 < zero <numeratorType> :: value) && (contractedNum2 < (numMin / contractedNum1));
+  constexpr const static bool numUnderflow2 = (contractedNum1 == minusOne) && (contractedNum2 > zero <numeratorType> :: value) && (zero <numeratorType> :: value < (numMin + contractedNum2));
+  constexpr const static bool numUnderflow3 = (contractedNum1 < minusOne) && (contractedNum2 > zero <numeratorType> :: value) && (contractedNum2 > (numMin / contractedNum1));
+  constexpr const static bool denOverflow = contractedDen2 > (denMax / contractedDen1);
+  constexpr const static bool needScale = numOverflow1 || numOverflow2 || numOverflow3 || numUnderflow1 || numUnderflow2 || numUnderflow3 || denOverflow;
+  constexpr static scaleType numOverflowScale = (numOverflow1 || numOverflow2 || numOverflow3) ? (static_cast<scaleType>(numMax) / static_cast<scaleType>(contractedNum1) / static_cast<scaleType>(contractedNum2)) : infinity;
+  constexpr static scaleType numUnderflowScale = (numUnderflow1 || numUnderflow2 || numUnderflow3) ? (static_cast<scaleType>(numMin) / static_cast<scaleType>(contractedNum1) / static_cast<scaleType>(contractedNum2)) : infinity;
+  constexpr static scaleType denOverflowScale = denOverflow ? (static_cast<scaleType>(denMax) / static_cast<scaleType>(contractedDen1) / static_cast<scaleType>(contractedDen2)) : infinity;
+  constexpr static scaleType scaleCandidate1 = (numOverflowScale < numUnderflowScale) ? numOverflowScale : numUnderflowScale;
+  constexpr static scaleType scale = (scaleCandidate1 < denOverflowScale) ? scaleCandidate1 : denOverflowScale;
+  constexpr static scaleType scaledDenominator = needScale ? (scale * static_cast<scaleType>(contractedDen1) * static_cast<scaleType>(contractedDen2)) : static_cast<scaleType>(contractedDen1 * contractedDen2);
+  constexpr static numeratorType numerator = needScale ? static_cast<numeratorType>(scale * static_cast<scaleType>(contractedNum1) * static_cast<scaleType>(contractedNum2)) : (contractedNum1 * contractedNum2);
+  constexpr static denominatorType denominator = needScale ? static_cast<denominatorType>((scaledDenominator < 1.0) ? 1.0 : scaledDenominator) : (contractedDen1 * contractedDen2);
   public:
-  using type = typename contractType <valueRatio<Type,finNum, finDen> > :: type; //TODO: Consider contracting optional on overflow.
+  using type = typename contractType <valueRatio<Type, numerator, denominator> > :: type; //TODO: Consider contracting optional on overflow.
  };
 
  template <class Type, typename std :: make_signed <Type> :: type ... Numerators, typename std :: make_unsigned <Type> :: type ... Denominators>
