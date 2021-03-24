4a96f40a7169c989d710db32fdb0f4bde6c41ecb
Igor Zarzycki <izarzycki@dmcs.pl>
2021-03-25 00:40:06 +0100

    Add min algorithm operating on types.

---
+++ algorithm/mintype.h
@@ -0,0 +1,33 @@
+#ifndef CRAP_ALGORITHM_MINTYPE
+#define CRAP_ALGORITHM_MINTYPE
+
+#include "../numeric/reducetype.h"
+
+#include <type_traits>
+
+namespace crap
+{
+ template <template <class, class> class, class...> struct minType;
+
+ template <template <class, class> class Operator, class Type> struct minType<Operator, Type>
+ {
+  using type = Type;
+ };
+
+ template <template <class, class> class Operator, class Type1, class Type2>
+	 struct minType<Operator, Type1, Type2>
+ {
+  using type = typename std :: conditional <Operator <Type1, Type2> :: value, Type1, Type2> :: type;
+ };
+
+ template <template <class, class> class Operator, class ... Types> struct minType
+ {
+  private:
+  static_assert(sizeof...(Types) > 0u, "Extremum of empty set is not defined.");
+  template <class ... SubTypes> using This = minType<Operator, SubTypes...>;
+  public:
+  using  type = typename reduceType <This, Types...> :: type;
+ };
+}
+#endif
+
