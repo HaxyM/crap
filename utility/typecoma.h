2297c3e38751dc9e4f3a37dcc3c626ff59105186
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2021-03-28 13:53:33 +0200

    Add typeComa.

---
+++ utility/typecoma.h
@@ -0,0 +1,10 @@
+#ifndef CRAP_UTILITY_TYPECOMA
+#define CRAP_UTILITY_TYPECOMA
+
+#include "typeidentity.h"
+
+namespace crap
+{
+ template <class, class Type> using typeComa = typeIdentity<Type>;
+}
+#endif
