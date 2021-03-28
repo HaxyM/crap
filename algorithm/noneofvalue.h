56adc55fd7cdd92be964aa40bfcc0400e4ffb14e
Igor Zarzycki <izarzycki@dmcs.pl>
2021-03-28 12:35:08 +0200

    Fix noneOf algorithm operating on values.

--- algorithm/noneofvalue.h
+++ algorithm/noneofvalue.h
@@ -1,7 +1,8 @@
 #ifndef CRAP_ALGORITHM_NONEOFVALUE
 #define CRAP_ALGORITHM_NONEOFVALUE
 
-#include "../functional/loricalorvalue.h"
+#include "../functional/logicalnotvalue.h"
+#include "../functional/logicalorvalue.h"
 #include "../utility/commontype.h"
 
 namespace crap
@@ -9,9 +10,10 @@ namespace crap
  template <class Type, template <Type> class Operator, Type ... Values> struct noneOfValue
  {
   private:
-  using resultType = typename commonType <(Operator <Values> :: value)...> :: type;
+  using resultType = typename commonType <decltype(Operator <Values> :: value)...> :: type;
+  constexpr const static auto orValue = logicalOrValue <resultType, static_cast<resultType>(Operator <Values> :: value)...> :: value;
   public:
-  constexpr const static auto value = !(logicalOrValue <resultType, static_cast<resultType>(Operator <Values> :: value)...> :: value);
+  constexpr const static auto value = logicalNotValue <decltype(orValue), orValue> :: value;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
  };
