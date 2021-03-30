d8839612a81931516d227a9eeddb14575dcb86ab
Igor Zarzycki <izarzycki@dmcs.pl>
2021-03-30 23:31:06 +0200

    Add back algorithm operating on types.

---
+++ utility/backtype.h
@@ -0,0 +1,22 @@
+#ifndef CRAP_UTILITY_BACKTYPE
+#define CRAP_UTILITY_BACKTYPE
+
+#include "typelist.h"
+
+namespace crap
+{
+ template <class...> struct backType;
+
+ template <class First, class ... Rest> struct backType<First, Rest...>
+ {
+  using type = typename typeList <First, Rest...> :: template at<sizeof...(Rest)>;
+  template <template <class...> class Container>
+	  using pop = typeList <First, Rest...> :: template till<sizeof...(Rest), Container>;
+ };
+
+ template <class ... Types> struct backType
+ {
+  static_assert(sizeof...(Types) != 0u, "Collection must not be empty.");
+ };
+}
+#endif
