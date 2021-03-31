c2dec56f9914afa2c6975ffcc447746cab0a3a49
Igor Zarzycki <izarzycki@dmcs.pl>
2021-03-31 23:17:05 +0200

    Add setDifferenceValueZipper

---
+++ algorithm/setdifferencevaluezipper.h
@@ -0,0 +1,48 @@
+#ifndef CRAP_ALGORITHM_SETDIFFERENCEVALUEZIPPER
+#define CRAP_ALGORITHM_SETDIFFERENCEVALUEZIPPER
+
+#include "setdifferencefortype.h"
+#include "../utility/valuelistfortype.h"
+#include "../numeric/accumulatevaluesource.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class, template <template <Type...> class> class...>
+	 struct setDifferenceValueZipper;
+
+ template <class Type, template <Type, Type> class Operator> struct setDifferenceValueZipper<Type, Operator>
+ {
+  template <template <Type...> class Container = valueListForType <Type> :: template type>
+	  using type = Container<>;
+ };
+
+ template <class Type, template <Type, Type> class Operator, template <template <Type...> class> class Source>
+	 struct setDifferenceValueZipper<Type, Operator, Source>
+ {
+  template <template <Type...> class Container = valueListForType <Type> :: template type>
+	  using type = Source<Container>;
+ };
+
+ template <class Type, template <Type, Type> class Operator, template <template <Type...> class> class Source1,
+	  template <template <Type...> class> class Source2>
+	 struct setDifferenceValueZipper<Type, Operator, Source1, Source2>
+ {
+  template <template <Type...> class Container = valueListForType <Type> :: template type>
+	  using type = typename Source2 <
+	  Source1 <setDifferenceForType <Type, Operator> :: template type> :: template with
+	  > :: template type<Container>;
+ };
+
+ template <class Type, template <Type, Type> class Operator, template <template <Type...> class> class ... Sources>
+	 struct setDifferenceValueZipper
+ {
+  private:
+  template <template <template <Type...> class> class ... SubSources>
+	  using This = setDifferenceValueZipper<Type, Operator, SubSources...>;
+  public:
+  template <template <Type...> class Container = valueListForType <Type> :: template type>
+	  using type = typename accumulateValueSource <Type, This, Sources...> :: template type<Container>;
+ };
+}
+#endif
+
