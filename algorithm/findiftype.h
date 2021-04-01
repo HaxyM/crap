c860b97d04626a41e8be06b58287a11f55ec96f7
Igor Zarzycki <izarzycki@dmcs.pl>
2021-04-01 15:07:09 +0200

    Unify findIf algorithms operating on both types and values.

--- algorithm/findiftype.h
+++ algorithm/findiftype.h
@@ -12,12 +12,16 @@ namespace crap
  {
   constexpr const static std :: size_t value = 0u;
   constexpr const static std :: size_t npos = 0u;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <template <class> class Operator, class Type> struct findIfType<Operator, Type>
  {
   constexpr const static std :: size_t value = (Operator <Type> :: value ? 0u : 1u);
   constexpr const static std :: size_t npos = 1u;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <template <class> class Operator, class ... Types> struct findIfType
@@ -32,7 +36,30 @@ namespace crap
   public:
   constexpr const static std :: size_t value = (inLower ? (lower :: value) : ((lower :: npos) + (upper :: value)));
   constexpr const static std :: size_t npos = sizeof...(Types);
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
+}
+
+template <template <class> class Operator>
+inline constexpr crap :: findIfType <Operator> :: operator
+typename crap :: findIfType <Operator> :: value_type () const noexcept
+{
+ return crap :: findIfType <Operator> :: value;
+}
+
+template <template <class> class Operator, class Type>
+inline constexpr crap :: findIfType <Operator, Type> :: operator
+typename crap :: findIfType <Operator, Type> :: value_type () const noexcept
+{
+ return crap :: findIfType <Operator, Type> :: value;
+}
+
+template <template <class> class Operator, class ... Types>
+inline constexpr crap :: findIfType <Operator, Types...> :: operator
+typename crap :: findIfType <Operator, Types...> :: value_type () const noexcept
+{
+ return crap :: findIfType <Operator, Types...> :: value;
 }
 #endif
 
--- algorithm/findifvalue.h
+++ algorithm/findifvalue.h
@@ -1,7 +1,8 @@
 #ifndef CRAP_ALGORITHM_FINDIFVALUE
 #define CRAP_ALGORITHM_FINDIFVALUE
 
-#include "../utility/valuelist.h"
+#include "../utility/bisectvalue.h"
+#include "../utility/valuemultiplexer.h"
 
 namespace crap
 {
@@ -11,26 +12,54 @@ namespace crap
  {
   constexpr const static std :: size_t value = 0u;
   constexpr const static std :: size_t npos = 0u;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, template <Type> class Operator, Type Value> struct findIfValue<Type, Operator, Value>
  {
   constexpr const static std :: size_t value = (Operator <Value> :: value ? 0u : 1u);
   constexpr const static std :: size_t npos = 1u;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, template <Type> class Operator, Type ... Values> struct findIfValue
  {
   private:
-  using values = valueList<Type, Values...>;
-  constexpr const static std :: size_t half = values :: size / 2u;
+  using values = bisectValue<Type, Values...>;
   template <Type ... SubValues> using This = findIfValue<Type, Operator, SubValues...>;
-  using lower = typename values :: template till<half, This>;
-  using upper = typename values :: template since<half, This>;
+  template <template <Type...> class Container> using empty = Container<>;
+  using lower = typename values :: template lower<This>;
+  constexpr const static bool inLower = (lower :: value) != (lower :: npos);
+  using upper = typename valueMultiplexer <Type, inLower, This, empty, values :: template upper> :: type;
   public:
-  constexpr const static std :: size_t value = (((lower :: value) != (lower :: npos)) ? (lower :: value) : ((lower :: npos) + (upper :: value)));
-  constexpr const static std :: size_t npos = lower :: npos + upper :: npos;
+  constexpr const static std :: size_t value = (inLower ? (lower :: value) : ((lower :: npos) + (upper :: value)));
+  constexpr const static std :: size_t npos = sizeof...(Values);
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
+}
+
+template <class Type, template <Type> class Operator>
+inline constexpr crap :: findIfValue <Type, Operator> :: operator
+typename crap :: findIfValue <Type, Operator> :: value_type () const noexcept
+{
+ return crap :: findIfValue <Type, Operator> :: value;
+}
+
+template <class Type, template <Type> class Operator, Type Value>
+inline constexpr crap :: findIfValue <Type, Operator, Value> :: operator
+typename crap :: findIfValue <Type, Operator, Value> :: value_type () const noexcept
+{
+ return crap :: findIfValue <Type, Operator, Value> :: value;
+}
+
+template <class Type, template <Type> class Operator, Type ... Values>
+inline constexpr crap :: findIfValue <Type, Operator, Values...> :: operator
+typename crap :: findIfValue <Type, Operator, Values...> :: value_type () const noexcept
+{
+ return crap :: findIfValue <Type, Operator, Values...> :: value;
 }
 #endif
 
