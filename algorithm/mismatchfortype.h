c168ad47e3752d614a5bfff7268726dc10d58da6
Igor Zarzycki <izarzycki@dmcs.pl>
2021-04-15 00:04:20 +0200

    Add mismatchfortype.h

---
+++ algorithm/mismatchfortype.h
@@ -0,0 +1,17 @@
+#ifndef CRAP_ALGORITHM_MISMATCHFORTYPE
+#define CRAP_ALGORITHM_MISMATCHFORTYPE
+
+#include "mismatchvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class Operator = comparatorsForType <Type> :: template EqualTo>
+	 struct mismatchForType
+ {
+  template <Type ... Values> using type = mismatchValue<Type, Operator, Values...>;
+  template <Type ... Values> using values = mismatchValue<Type, Operator, Values...>;
+ };
+}
+#endif
+
