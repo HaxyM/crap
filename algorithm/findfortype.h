ba6b7f15743c6b54a70189374ac87e741322c121
Igor Zarzycki <izarzycki@dmcs.pl>
2021-04-14 23:21:23 +0200

    Add findForType.

---
+++ algorithm/findfortype.h
@@ -0,0 +1,18 @@
+#ifndef CRAP_ALGORITHM_FINDFORTYPE
+#define CRAP_ALGORITHM_FINDFORTYPE
+
+#include "findvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, Type Value, template <Type, Type> class Operator = comparatorsForType <Type> :: template
+	 EqualTo>
+ struct findForType
+ {
+  template <Type ... Values> using type = findValue<Type, Value, Operator, Values...>;
+  template <Type ... Values> using values = findValue<Type, Value, Operator, Values...>;
+ };
+}
+#endif
+
