139ef5212997b34da2e1ba7b05dd3bbf0de2a9f2
Igor Zarzycki <izarzycki@dmcs.pl>
2021-05-04 23:51:50 +0200

    Unify interfaces of lowerBound algorithms operating both on types and values.

--- algorithm/lowerboundtype.h
+++ algorithm/lowerboundtype.h
@@ -11,12 +11,16 @@ namespace crap
  {
   constexpr const static std :: size_t value = 0u;
   constexpr const static std :: size_t npos = 0u;
+  using vlaue_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, template <class, class> class Operator, class Type1> struct lowerBoundType<Type, Operator, Type1>
  {
   constexpr const static std :: size_t value = (Operator <Type1, Type> :: value ? 1u : 0u);
   constexpr const static std :: size_t npos = 1u;
+  using vlaue_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, template <class, class> class Operator, class ... Types> struct lowerBoundType
@@ -29,6 +33,29 @@ namespace crap
   public:
   constexpr const static std :: size_t value = (((lower :: value) != (lower :: npos)) ? (lower :: value) : ((lower :: npos) + (upper :: value)));
   constexpr const static std :: size_t npos = (lower :: npos) + (upper :: npos);
+  using vlaue_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
+}
+
+template <class Type, template <class, class> class Operator>
+        inline constexpr crap :: lowerBoundType <Type, Operator> :: operator
+        typename crap :: lowerBoundType <Type, Operator> :: value_type () const noexcept
+{
+ return crap :: lowerBoundType <Type, Operator> :: value;
+}
+
+template <class Type, template <class, class> class Operator, class Type1>
+        inline constexpr crap :: lowerBoundType <Type, Operator, Type1> :: operator
+        typename crap :: lowerBoundType <Type, Operator, Type1> :: value_type () const noexcept
+{
+ return crap :: lowerBoundType <Type, Operator, Type1> :: value;
+}
+
+template <class Type, template <class, class> class Operator, class ... Types>
+        inline constexpr crap :: lowerBoundType <Type, Operator, Types...> :: operator
+        typename crap :: lowerBoundType <Type, Operator, Types...> :: value_type () const noexcept
+{
+ return crap :: lowerBoundType <Type, Operator, Types...> :: value;
 }
 #endif
--- algorithm/lowerboundvalue.h
+++ algorithm/lowerboundvalue.h
@@ -11,12 +11,16 @@ namespace crap
  {
   constexpr const static std :: size_t value = 0u;
   constexpr const static std :: size_t npos = 0u;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value, template <Type, Type> class Operator, Type Value1> struct lowerBoundValue<Type, Value, Operator, Value1>
  {
   constexpr const static std :: size_t value = (Operator <Value1, Value> :: value ? 1u : 0u);
   constexpr const static std :: size_t npos = 1u;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value, template <Type, Type> class Operator, Type ... Values> struct lowerBoundValue
@@ -30,7 +34,30 @@ namespace crap
   public:
   constexpr const static std :: size_t value = (((lower :: value) != (lower :: npos)) ? (lower :: value) : ((lower :: npos) + (upper :: value)));
   constexpr const static std :: size_t npos = (lower :: npos) + (upper :: npos);
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
+}
+
+template <class Type, Type Value, template <Type, Type> class Operator>
+        inline constexpr crap :: lowerBoundValue <Type, Value, Operator> :: operator
+        typename crap :: lowerBoundValue <Type, Value, Operator> :: value_type () const noexcept
+{
+ return crap :: lowerBoundValue <Type, Value, Operator> :: value;
+}
+
+template <class Type, Type Value, template <Type, Type> class Operator, Type Value1>
+        inline constexpr crap :: lowerBoundValue <Type, Value, Operator, Value1> :: operator
+        typename crap :: lowerBoundValue <Type, Value, Operator, Value1> :: value_type () const noexcept
+{
+ return crap :: lowerBoundValue <Type, Value, Operator, Value1> :: value;
+}
+
+template <class Type, Type Value, template <Type, Type> class Operator, Type ... Values>
+        inline constexpr crap :: lowerBoundValue <Type, Value, Operator, Values...> :: operator
+        typename crap :: lowerBoundValue <Type, Value, Operator, Values...> :: value_type () const noexcept
+{
+ return crap :: lowerBoundValue <Type, Value, Operator, Values...> :: value;
 }
 #endif
 
