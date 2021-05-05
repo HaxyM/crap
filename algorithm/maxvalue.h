6c7eb74c731a0635d45c58b0785de8af57014d5b
Igor Zarzycki <izarzycki@dmcs.pl>
2021-05-05 22:08:42 +0200

    Unify interface of max algorithm operating on values.

--- algorithm/maxvalue.h
+++ algorithm/maxvalue.h
@@ -11,12 +11,16 @@ namespace crap
 	 struct maxValue<Type, Operator, Value>
  {
   constexpr const static Type value = Value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, template <Type, Type> class Operator, Type Value1, Type Value2>
 	 struct maxValue<Type, Operator, Value1, Value2>
  {
   constexpr const static Type value = ((Operator <Value2, Value1> :: value) ? Value1 : Value2);
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, template <Type, Type> class Operator, Type ... Values>
@@ -27,6 +31,29 @@ namespace crap
   template <Type ... SubValues> using This = maxValue<Type, Operator, SubValues...>;
   public:
   constexpr const static Type value = reduceValue <Type, This, Values...> :: value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
+}
+
+template <class Type, template <Type, Type> class Operator, Type Value1>
+        inline constexpr crap :: maxValue <Type, Operator, Value1> :: operator
+        typename crap :: maxValue <Type, Operator, Value1> :: value_type () const noexcept
+{
+ return crap :: maxValue <Type, Operator, Value1> :: value;
+}
+
+template <class Type, template <Type, Type> class Operator, Type Value1, Type Value2>
+        inline constexpr crap :: maxValue <Type, Operator, Value1, Value2> :: operator
+        typename crap :: maxValue <Type, Operator, Value1, Value2> :: value_type () const noexcept
+{
+ return crap :: maxValue <Type, Operator, Value1, Value2> :: value;
+}
+
+template <class Type, template <Type, Type> class Operator, Type ... Values>
+        inline constexpr crap :: maxValue <Type, Operator, Values...> :: operator
+        typename crap :: maxValue <Type, Operator, Values...> :: value_type () const noexcept
+{
+ return crap :: maxValue <Type, Operator, Values...> :: value;
 }
 #endif
