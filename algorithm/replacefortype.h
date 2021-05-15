dd927332c36e78420ec4a4b66f460a6f7b6fc602
Igor Zarzycki <izarzycki@dmcs.pl>
2021-05-15 23:52:42 +0200

    Add replaceForType.

---
+++ algorithm/replacefortype.h
@@ -0,0 +1,17 @@
+#ifndef CRAP_ALGORITHM_REPLACEFORTYPE
+#define CRAP_ALGORITHM_REPLACEFORTYPE
+
+#include "replacevalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, Type Old, Type New, template <Type, Type> class Operator = comparatorsForType <Type> :: template EqualTo>
+	 struct replaceForType
+ {
+  template <Type ... Values> using type = replaceValue<Type, Old, New, Operator, Values...>;
+  template <Type ... Values> using values = replaceValue<Type, Old, New, Operator, Values...>;
+ };
+}
+#endif
+
