d749f493e4445f0bd6752c43b9fe83afb21fd037
Igor Zarzycki <izarzycki@dmcs.pl>
2021-05-26 23:37:24 +0200

    Add anyOfForType.

---
+++ algorithm/anyoffortype.h
@@ -0,0 +1,15 @@
+#ifndef CRAP_ALGORITHM_ANYOFFORTYPE
+#define CRAP_ALGORITHM_ANYOFFORTYPE
+
+#include "anyofvalue.h"
+
+namespace crap
+{
+ template <class Type, template <Type> class Operator> struct anyOfForType
+ {
+  template <Type ... Values> using type = anyOfValue<Type, Operator, Values...>;
+  template <Type ... Values> using values = anyOfForType<Type, Operator, Values...>;
+ };
+}
+#endif
+
