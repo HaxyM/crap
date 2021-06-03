e16118816f7403831d3131d281b3b3d8f85c068c
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2021-06-03 12:43:16 +0200

    Fix typo.

--- algorithm/anyoffortype.h
+++ algorithm/anyoffortype.h
@@ -8,7 +8,7 @@ namespace crap
  template <class Type, template <Type> class Operator> struct anyOfForType
  {
   template <Type ... Values> using type = anyOfValue<Type, Operator, Values...>;
-  template <Type ... Values> using values = anyOfForType<Type, Operator, Values...>;
+  template <Type ... Values> using values = anyOfValue<Type, Operator, Values...>;
  };
 }
 #endif
