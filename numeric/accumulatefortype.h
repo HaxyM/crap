b7824168b5c07f465d0042141c2248ec5b5d85a3
Igor Zarzycki <izarzycki@dmcs.pl>
2021-06-11 22:14:18 +0200

    Add accumulateForType.

---
+++ numeric/accumulatefortype.h
@@ -0,0 +1,17 @@
+#ifndef CRAP_NUMERIC_ACCUMULATEFORTYPE
+#define CRAP_NUMERIC_ACCUMULATEFORTYPE
+
+#include "accumulatevalue.h"
+#include "../functional/arithmeticoperatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, template <Type...> class Operator = arithmeticOperatorsForType <Type> :: template Plus>
+	 struct accumulateForType
+ {
+  template <Type ... Values> using type = accumulateValue<Type, Operator, Values...>;
+  template <Type ... Values> using values = accumulateValue<Type, Operator, Values...>;
+ };
+}
+#endif
+
