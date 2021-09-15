8ec0b4415b904ea8b29b42264984d38425e92921
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2021-09-15 18:18:13 +0200

    Adapt contractType for new ratio concept.

--- ratio/contracttype.h
+++ ratio/contracttype.h
@@ -4,25 +4,19 @@
 #include <type_traits>
 
 #include "valueratio.h"
-#include "../numeric/absvalue.h"
 #include "../numeric/contracttype.h"
 #include "../numeric/gcdvalue.h"
-#include "../numeric/zero.h"
 
 namespace crap
 {
- template <typename Type, typename std :: make_signed <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
-	 struct contractType<valueRatio<Type, Numerator, Denominator> >
+ template <typename Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 struct contractType<valueRatio<Type, Sign, Numerator, Denominator> >
  {
   private:
-  using numeratorType = typename std :: add_const <typename std :: make_signed <Type> :: type> :: type;
-  using denominatorType = typename std :: add_const <typename std :: make_unsigned <Type> :: type> :: type;
-  constexpr static denominatorType absolute = static_cast<denominatorType>(absValue <Type, Numerator> :: value);
-  constexpr static numeratorType contractedNumerator = static_cast<numeratorType>(absolute / gcdValue <denominatorType, absolute, Denominator> :: value);
-  constexpr static numeratorType newNumerator = ((Numerator < zero <numeratorType> :: value) ? (-contractedNumerator) : contractedNumerator);
-  constexpr static denominatorType newDenominator = Denominator / gcdValue <denominatorType, absolute, Denominator> :: value;
+  using valueType = typename std :: add_const <typename std :: make_unsigned <Type> :: type> :: type;
+  constexpr static valueType gcd = gcdValue <valueType, Numerator, Denominator> :: value;
   public:
-  using type = valueRatio<Type, newNumerator, newDenominator>;
+  using type = valueRatio<Type, Sign, Numerator / gcd, Denominator / gcd>;
  };
 }
 #endif
