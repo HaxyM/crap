50fd6187867a2c9d625717d3c575054b780b9268
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2021-10-09 20:28:11 +0200

    Adapt cbrtType for new ratio concept.

--- ratio/cbrttype.h
+++ ratio/cbrttype.h
@@ -17,16 +17,16 @@
 
 namespace crap
 {
- template <class Type, typename std :: make_signed <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
-	 struct cbrtType<valueRatio<Type, Numerator, Denominator>, 0u>
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 struct cbrtType<valueRatio<Type, Sign, Numerator, Denominator>, 0u>
  {
-  using type = valueRatio<Type, Numerator, Denominator>;
+  using type = valueRatio<Type, Sign, Numerator, Denominator>;
  };
- template <class Type, typename std :: make_signed <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator, std :: size_t Steps>
-	 struct cbrtType<valueRatio<Type, Numerator, Denominator>, Steps>
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator, std :: size_t Steps>
+	 struct cbrtType<valueRatio<Type, Sign, Numerator, Denominator>, Steps>
  {
   private:
-  using z = valueRatio<Type, Numerator, Denominator>;
+  using z = valueRatio<Type, Sign, Numerator, Denominator>;
   using x = typename cbrtType <z, Steps - 1u> :: type;
   using x2 = typename multipliesType <x, x> :: type;
   using partNum = typename minusType <typename multipliesType <x, x, x> :: type, z> :: type;
@@ -35,16 +35,15 @@ namespace crap
   using type = typename minusType <x, part> :: type;
  };
 
- template <class Type, typename std :: make_signed <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
-	 constexpr typename cbrtType <valueRatio<Type, Numerator, Denominator> > :: type
-	 cbrt(valueRatio<Type, Numerator, Denominator>) noexcept;
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 constexpr typename cbrtType <valueRatio<Type, Sign, Numerator, Denominator> > :: type
+	 cbrt(valueRatio<Type, Sign, Numerator, Denominator>) noexcept;
 }
 
-template <class Type, typename std :: make_signed <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
-inline constexpr typename crap :: cbrtType <crap :: valueRatio<Type, Numerator, Denominator> > :: type
-crap :: cbrt(crap :: valueRatio<Type, Numerator, Denominator>) noexcept
+template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+inline constexpr typename crap :: cbrtType <crap :: valueRatio<Type, Sign, Numerator, Denominator> > :: type
+crap :: cbrt(crap :: valueRatio<Type, Sign, Numerator, Denominator>) noexcept
 {
  return {};
 }
 #endif
-
