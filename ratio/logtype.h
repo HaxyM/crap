6481c6282fe8d464e6e011ef2e2e8d9d61cd4fab
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2021-10-09 20:35:33 +0200

    Adapt logType for new ratio concept.

--- ratio/logtype.h
+++ ratio/logtype.h
@@ -22,37 +22,36 @@
 
 namespace crap
 {
- template <class Type, typename std :: make_signed <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator, std :: size_t Steps>
-	 struct logType<valueRatio<Type, Numerator, Denominator>, Steps>
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator, std :: size_t Steps>
+	 struct logType<valueRatio<Type, Sign, Numerator, Denominator>, Steps>
  {
   private:
-  using numeratorType = typename std :: make_signed <Type> :: type;
-  using denominatorType = typename std :: make_unsigned <Type> :: type;
-  using z = valueRatio<Type, Numerator, Denominator>;
+  static_assert(Sign == '+', "Value must be positive.");
+  using valueType = typename std :: make_unsigned <Type> :: type;
+  using z = valueRatio<Type, Sign, Numerator, Denominator>;
   using argument = typename dividesType <typename minusType <z, typename identity <z> :: type> :: type,
 	typename plusType <z, typename identity <z> :: type> :: type> :: type;
-  template <denominatorType N> using constant = valueRatio<Type, identity <numeratorType> :: value, N>;
+  template <valueType N> using constant = valueRatio<Type, '+', identity <valueType> :: value, N>;
   template <std :: size_t N>
 	  using power = typename reproduceType<N, argument> :: template type<multipliesType> :: type;
-  template <denominatorType ... N> using constants = innerProductType<plusType, multipliesType, constant<N>...>;
+  template <valueType ... N> using constants = innerProductType<plusType, multipliesType, constant<N>...>;
   template <std :: size_t ... N>
-	  using odds = typename constants <static_cast<denominatorType>(2u * N + 1u)...> :: template
+	  using odds = typename constants <static_cast<valueType>(2u * N + 1u)...> :: template
 	  with <power<2u * N + 1u>...> :: type;
   using half = plusType<argument, typename iotaValue <Steps, std :: size_t, 1u> :: template type<odds> >;
   public:
   using type = typename plusType <typename half :: type, typename half :: type> :: type;
  };
 
- template <class Type, typename std :: make_signed <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
-	 constexpr typename logType <valueRatio<Type, Numerator, Denominator> > :: type
-	 log(valueRatio<Type, Numerator, Denominator>) noexcept;
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 constexpr typename logType <valueRatio<Type, Sign, Numerator, Denominator> > :: type
+	 log(valueRatio<Type, Sign, Numerator, Denominator>) noexcept;
 }
 
-template <class Type, typename std :: make_signed <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
-inline constexpr typename crap :: logType <crap :: valueRatio<Type, Numerator, Denominator> > :: type
-crap :: log(crap :: valueRatio<Type, Numerator, Denominator>) noexcept
+template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+inline constexpr typename crap :: logType <crap :: valueRatio<Type, Sign, Numerator, Denominator> > :: type
+crap :: log(crap :: valueRatio<Type, Sign, Numerator, Denominator>) noexcept
 {
  return {};
 }
 #endif
-
