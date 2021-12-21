bdfaa76e1b24b64256fef3e7f95f8c57339e30df
Igor Zarzycki <izarzycki@dmcs.pl>
2021-12-21 17:05:23 +0100

    Add inplaceMergeForType.

---
+++ algorithm/inplacemergefortype.h
@@ -0,0 +1,19 @@
+#ifndef CRAP_ALGORITHM_INPLACEMERGEFORTYPE
+#define CRAP_ALGORITHM_INPLACEMERGEFORTYPE
+
+#include <cstddef>
+
+#include "inplacemergevalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, std :: size_t Middle, template <Type, Type> class Operator = comparatorsForType <Type> :: template Less>
+	 struct inplaceMergeForType
+ {
+  template <Type ... Values> using type = inplaceMergeValue<Type, Middle, Operator, Values...>;
+  template <Type ... Values> using values = inplaceMergeValue<Type, Middle, Operator, Values...>;
+ };
+}
+#endif
+
