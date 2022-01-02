f1b97341e14e7469673b3e7531b2b75f01cd7837
Igor Zarzycki <izarzycki@dmcs.pl>
2022-01-02 01:04:28 +0100

    Fix minElement algorithm operating on values to behave  same as std :: min_element in case of equal values.

--- algorithm/minelementvalue.h
+++ algorithm/minelementvalue.h
@@ -35,9 +35,9 @@ namespace crap
  {
   template <class NewType, template <NewType, NewType> class, NewType...> friend struct minElementValue;
   private:
-  constexpr const static Type minValue = ((Operator <Value1, Value2> :: value) ? Value1 : Value2);
+  constexpr const static Type minValue = ((Operator <Value2, Value1> :: value) ? Value2 : Value1);
   public:
-  constexpr const static std :: size_t value = ((Operator <Value1, Value2> :: value) ? 0u : 1u);
+  constexpr const static std :: size_t value = ((Operator <Value2, Value1> :: value) ? 1u : 0u);
   constexpr const static std :: size_t npos = 2u;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
@@ -51,10 +51,10 @@ namespace crap
   using lower = typename bisectValue <Type, Values...> :: template lower<This>;
   using upper = typename bisectValue <Type, Values...> :: template upper<This>;
   constexpr const static Type minValue =
-	  ((Operator <lower :: minValue, upper :: minValue> :: value) ? (lower :: minValue) : (upper :: minValue));
+	  ((Operator <upper :: minValue, lower :: minValue> :: value) ? (upper :: minValue) : (lower :: minValue));
   public:
   constexpr const static std :: size_t value =
-	  ((Operator <lower :: minValue, upper :: minValue> :: value) ? (lower :: value) : (lower :: npos + upper :: value));
+	  ((Operator <upper :: minValue, lower :: minValue> :: value) ? (lower :: npos + upper :: value) : (lower :: value));
   constexpr const static std :: size_t npos = sizeof...(Values);
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
