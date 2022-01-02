2c4ba9262397a3bea7f583f27d8efea31a6839d7
Igor Zarzycki <izarzycki@dmcs.pl>
2022-01-02 03:29:23 +0100

    Add maxElementForType.

---
+++ algorithm/maxelementfortype.h
@@ -0,0 +1,17 @@
+#ifndef CRAP_ALGORITHM_MAXELEMENTFORTYPE
+#define CRAP_ALGORITHM_MAXELEMENTFORTYPE
+
+#include "maxelementvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class Operator = comparatorsForType <Type> :: template Less>
+	 struct maxElementForType
+ {
+  template <Type ... Values> using type = maxElementValue<Type, Operator, Values...>;
+  template <Type ... Values> using values = maxElementValue<Type, Operator, Values...>;
+ };
+}
+#endif
+
