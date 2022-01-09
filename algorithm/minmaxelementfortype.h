bc997141e92f60ee0e892ae3bd7e3e28d02210ce
Igor Zarzycki <izarzycki@dmcs.pl>
2022-01-09 01:01:33 +0100

    Add minMaxElmentForType.

---
+++ algorithm/minmaxelementfortype.h
@@ -0,0 +1,17 @@
+#ifndef CRAP_ALGORITHM_MINMAXELEMENTFORTYPE
+#define CRAP_ALGORITHM_MINMAXELEMENTFORTYPE
+
+#include "minmaxelementvalue.h"
+#include "../functional/comparatorsfortype.h"
+
+namespace crap
+{
+ template <class Type, template <Type, Type> class Operator = comparatorsForType <Type> :: template Less>
+	 struct minMaxElementForType
+ {
+  template <Type ... Values> using type = minMaxElementValue<Type, Operator, Values...>;
+  template <Type ... Values> using values = minMaxElementValue<Type, Operator, Values...>;
+ };
+}
+#endif
+
