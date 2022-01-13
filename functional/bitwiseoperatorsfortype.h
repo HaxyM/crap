a358ce15e47a56c45f26e69d7ceb2aa8c63d563b
Igor Zarzycki <izarzycki@dmcs.pl>
2022-01-13 20:26:18 +0100

    Unify bitwiseOperatorsForType.

--- functional/bitwiseoperatorsfortype.h
+++ functional/bitwiseoperatorsfortype.h
@@ -11,13 +11,24 @@ namespace crap
  template <class Type> struct bitwiseOperatorsForType
  {
   template <Type Value> using BitwiseNot = bitwiseNotValue<Type, Value>;
-  template <Type Value1, Type Value2> using BitwiseOr = bitwiseOrValue<Type, Value1, Value2>;
-  template <Type Value1, Type Value2> using BitwiseAnd = bitwiseAndValue<Type, Value1, Value2>;
-  template <Type Value1, Type Value2> using BitwiseXor = bitwiseXorValue<Type, Value1, Value2>;
+  template <Type ... Values> using BitwiseOr = bitwiseOrValue<Type, Values...>;
+  template <Type ... Values> using BitwiseAnd = bitwiseAndValue<Type, Values...>;
+  template <Type ... Values> using BitwiseXor = bitwiseXorValue<Type, Values...>;
+  template <Type Value1, Type Value2> using BitwiseOr2 = bitwiseOrValue<Type, Value1, Value2>;
+  template <Type Value1, Type Value2> using BitwiseAnd2 = bitwiseAndValue<Type, Value1, Value2>;
+  template <Type Value1, Type Value2> using BitwiseXor2 = bitwiseXorValue<Type, Value1, Value2>;
+  #if CPP14
   template <Type Value> constexpr const static auto bitwiseNot = bitwiseNotValue <Type, Value> :: value;
-  template <Type Value1, Type Value2> constexpr const static auto bitwiseOr = bitwiseOrValue <Type, Value1, Value2> :: value;
-  template <Type Value1, Type Value2> constexpr const static auto bitwiseAnd = bitwiseAndValue <Type, Value1, Value2> :: value;
-  template <Type Value1, Type Value2> constexpr const static auto bitwiseXor = bitwiseXorValue <Type, Value1, Value2> :: value;
+  template <Type ... Values> constexpr const static auto bitwiseOr = bitwiseOrValue <Type, Values...> :: value;
+  template <Type ... Values> constexpr const static auto bitwiseAnd = bitwiseAndValue <Type, Values...> :: value;
+  template <Type ... Values> constexpr const static auto bitwiseXor = bitwiseXorValue <Type, Values...> :: value;
+  template <Type Value1, Type Value2>
+	  constexpr const static auto bitwiseOr2 = bitwiseOrValue <Type, Value1, Value2> :: value;
+  template <Type Value1, Type Value2>
+	  constexpr const static auto bitwiseAnd2 = bitwiseAndValue <Type, Value1, Value2> :: value;
+  template <Type Value1, Type Value2>
+	  constexpr const static auto bitwiseXor2 = bitwiseXorValue <Type, Value1, Value2> :: value;
+  #endif
  };
 }
 #endif
