e096cc44730bc5b1d1f69fd811d8712b6a4e88ce
Igor Zarzycki <izarzycki@dmcs.pl>
2022-01-13 20:58:47 +0100

    Unify interface of multiplies operator acting on values.

--- functional/multipliesvalue.h
+++ functional/multipliesvalue.h
@@ -1,6 +1,7 @@
 #ifndef CRAP_FUNCTIONAL_MULTIPLIESVALUE
 #define CRAP_FUNCTIONAL_MULTIPLIESVALUE
 
+#include "../numeric/identity.h"
 #include "../utility/language.h"
 
 #if CPP17
@@ -14,17 +15,23 @@ namespace crap
 
  template <class Type> struct multipliesValue<Type>
  {
-  constexpr const static auto value = static_cast<Type>(1);
+  constexpr const static auto value = identity <Type> :: value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value> struct multipliesValue<Type, Value>
  {
   constexpr const static auto value = Value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value1, Type Value2> struct multipliesValue<Type, Value1, Value2>
  {
   constexpr const static auto value = (Value1 * Value2);
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type ... Values> struct multipliesValue
@@ -37,7 +44,37 @@ namespace crap
   public:
   constexpr const static auto value = reduceValue <Type, This, Values...> :: value;
   #endif
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
+}
+
+template <class Type>
+inline constexpr crap :: multipliesValue <Type> :: operator
+typename crap :: multipliesValue <Type> :: value_type () const noexcept
+{
+ return crap :: multipliesValue <Type> :: value;
+}
+
+template <class Type, Type Value>
+inline constexpr crap :: multipliesValue <Type, Value> :: operator
+typename crap :: multipliesValue <Type, Value> :: value_type () const noexcept
+{
+ return crap :: multipliesValue <Type, Value> :: value;
+}
+
+template <class Type, Type Value1, Type Value2>
+inline constexpr crap :: multipliesValue <Type, Value1, Value2> :: operator
+typename crap :: multipliesValue <Type, Value1, Value2> :: value_type () const noexcept
+{
+ return crap :: multipliesValue <Type, Value1, Value2> :: value;
+}
+
+template <class Type, Type ... Values>
+inline constexpr crap :: multipliesValue <Type, Values...> :: operator
+typename crap :: multipliesValue <Type, Values...> :: value_type () const noexcept
+{
+ return crap :: multipliesValue <Type, Values...> :: value;
 }
 #endif
 
