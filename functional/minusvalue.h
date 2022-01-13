cab45be6fe4e72a5ab0b3406327dbfb74c07557c
Igor Zarzycki <izarzycki@dmcs.pl>
2022-01-13 21:23:16 +0100

    Unify interface of minus operator acting on values.

--- functional/minusvalue.h
+++ functional/minusvalue.h
@@ -17,11 +17,15 @@ namespace crap
  template <class Type, Type Value> struct minusValue<Type, Value>
  {
   constexpr const static auto value = Value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value1, Type Value2> struct minusValue<Type, Value1, Value2>
  {
   constexpr const static auto value = (Value1 - Value2);
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
  #endif
 
@@ -35,7 +39,33 @@ namespace crap
   public:
   constexpr const static auto value = accumulateValue <Type, This, FirstValue, Rest...> :: value;
   #endif
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
+}
+
+#if CPP17
+#else
+template <class Type, Type Value>
+inline constexpr crap :: minusValue <Type, Value> :: operator
+typename crap :: minusValue <Type, Value> :: value_type () const noexcept
+{
+ return crap :: minusValue <Type, Value> :: value;
+}
+
+template <class Type, Type Value1, Type Value2>
+inline constexpr crap :: minusValue <Type, Value1, Value2> :: operator
+typename crap :: minusValue <Type, Value1, Value2> :: value_type () const noexcept
+{
+ return crap :: minusValue <Type, Value1, Value2> :: value;
+}
+#endif
+
+template <class Type, Type FirstValue, Type ... Rest>
+inline constexpr crap :: minusValue <Type, FirstValue, Rest...> :: operator
+typename crap :: minusValue <Type, FirstValue, Rest...> :: value_type () const noexcept
+{
+ return crap :: minusValue <Type, FirstValue, Rest...> :: value;
 }
 #endif
 
