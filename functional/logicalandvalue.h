d2b2e3b7d4d98d1eed2228423da1ec876995e5ee
Igor Zarzycki <izarzycki@dmcs.pl>
2022-01-15 01:12:49 +0100

    Unify logicalAnd operator acting on values.

--- functional/logicalandvalue.h
+++ functional/logicalandvalue.h
@@ -1,6 +1,7 @@
 #ifndef CRAP_FUNCTIONAL_LOGICALANDVALUE
 #define CRAP_FUNCTIONAL_LOGICALANDVALUE
 
+#include "../numeric/identity.h"
 #include "../utility/language.h"
 
 #if CPP17
@@ -14,17 +15,23 @@ namespace crap
 
  template <class Type> struct logicalAndValue<Type>
  {
-  constexpr const static auto value = static_cast<Type>(true);
+  constexpr const static auto value = identity <Type> :: value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value> struct logicalAndValue<Type, Value>
  {
   constexpr const static auto value = Value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value1, Type Value2> struct logicalAndValue<Type, Value1, Value2>
  {
   constexpr const static auto value = (Value1 && Value2);
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type ... Values> struct logicalAndValue
@@ -37,7 +44,37 @@ namespace crap
   public:
   constexpr const static auto value = reduceValue <Type, This, Values...> :: value;
   #endif
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
+}
+
+template <class Type>
+inline constexpr crap :: logicalAndValue <Type> :: operator
+typename crap :: logicalAndValue <Type> :: value_type () const noexcept
+{
+ return crap :: logicalAndValue <Type> :: value;
+}
+
+template <class Type, Type Value>
+inline constexpr crap :: logicalAndValue <Type, Value> :: operator
+typename crap :: logicalAndValue <Type, Value> :: value_type () const noexcept
+{
+ return crap :: logicalAndValue <Type, Value> :: value;
+}
+
+template <class Type, Type Value1, Type Value2>
+inline constexpr crap :: logicalAndValue <Type, Value1, Value2> :: operator
+typename crap :: logicalAndValue <Type, Value1, Value2> :: value_type () const noexcept
+{
+ return crap :: logicalAndValue <Type, Value1, Value2> :: value;
+}
+
+template <class Type, Type ... Values>
+inline constexpr crap :: logicalAndValue <Type, Values...> :: operator
+typename crap :: logicalAndValue <Type, Values...> :: value_type () const noexcept
+{
+ return crap :: logicalAndValue <Type, Values...> :: value;
 }
 #endif
 
