26e4ad232ac27752056b6301295f8a97d51ee4e0
Igor Zarzycki <izarzycki@dmcs.pl>
2022-01-15 01:12:36 +0100

    Unify logicalOr operator acting on values.

--- functional/logicalorvalue.h
+++ functional/logicalorvalue.h
@@ -1,6 +1,7 @@
 #ifndef CRAP_FUNCTIONAL_LOGICALORVALUE
 #define CRAP_FUNCTIONAL_LOGICALORVALUE
 
+#include "../numeric/zero.h"
 #include "../utility/language.h"
 
 #if CPP17
@@ -14,17 +15,23 @@ namespace crap
 
  template <class Type> struct logicalOrValue<Type>
  {
-  constexpr const static auto value = static_cast<Type>(false);
+  constexpr const static auto value = zero <Type> :: value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value> struct logicalOrValue<Type, Value>
  {
   constexpr const static auto value = Value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value1, Type Value2> struct logicalOrValue<Type, Value1, Value2>
  {
   constexpr const static auto value = (Value1 || Value2);
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type ... Values> struct logicalOrValue
@@ -37,7 +44,37 @@ namespace crap
   public:
   constexpr const static auto value = reduceValue <Type, This, Values...> :: value;
   #endif
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
+}
+
+template <class Type>
+inline constexpr crap :: logicalOrValue <Type> :: operator
+typename crap :: logicalOrValue <Type> :: value_type () const noexcept
+{
+ return crap :: logicalOrValue <Type> :: value;
+}
+
+template <class Type, Type Value>
+inline constexpr crap :: logicalOrValue <Type, Value> :: operator
+typename crap :: logicalOrValue <Type, Value> :: value_type () const noexcept
+{
+ return crap :: logicalOrValue <Type, Value> :: value;
+}
+
+template <class Type, Type Value1, Type Value2>
+inline constexpr crap :: logicalOrValue <Type, Value1, Value2> :: operator
+typename crap :: logicalOrValue <Type, Value1, Value2> :: value_type () const noexcept
+{
+ return crap :: logicalOrValue <Type, Value1, Value2> :: value;
+}
+
+template <class Type, Type ... Values>
+inline constexpr crap :: logicalOrValue <Type, Values...> :: operator
+typename crap :: logicalOrValue <Type, Values...> :: value_type () const noexcept
+{
+ return crap :: logicalOrValue <Type, Values...> :: value;
 }
 #endif
 
