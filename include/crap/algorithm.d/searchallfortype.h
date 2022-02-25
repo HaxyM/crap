0076f01e4dbe969f6d753f52502fc66a5a79e3d2
Igor Zarzycki <izarzycki@dmcs.pl>
2022-02-25 09:56:07 +0100

    Fix searchAllForType.

--- include/crap/algorithm.d/searchallfortype.h
+++ include/crap/algorithm.d/searchallfortype.h
@@ -9,8 +9,8 @@ namespace crap
  template <class Type, template <Type, Type> class Operator = comparatorsForType <Type> :: template EqualTo>
 	 struct searchAllForType
  {
-  template <Type ... Pattern> using type = searchAllForType<Type, Operator, Pattern...>;
-  template <Type ... Pattern> using pattern = searchAllForType<Type, Operator, Pattern...>;
+  template <Type ... Pattern> using type = searchAllValue<Type, Operator, Pattern...>;
+  template <Type ... Pattern> using pattern = searchAllValue<Type, Operator, Pattern...>;
  };
 }
 #endif
