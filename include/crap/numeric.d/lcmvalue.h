4417bf1f7756dbce6c519c0b4351e9f889c54657
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2022-06-05 19:39:21 +0200

    Unify lcm operator acting on values.

--- include/crap/numeric.d/lcmvalue.h
+++ include/crap/numeric.d/lcmvalue.h
@@ -3,6 +3,7 @@
 
 #include "../utility.d/language.h"
 #include "reducevalue.h"
+#include "identity.h"
 
 #if CPP17
 #include <numeric>
@@ -17,12 +18,16 @@ namespace crap
 
  template <class Type> struct lcmValue<Type>
  {
-  constexpr const static Type value = static_cast<Type>(1);
+  constexpr const static Type value = identity <Type> :: value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value> struct lcmValue<Type, Value>
  {
   constexpr const static Type value = Value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type Value1, Type Value2> struct lcmValue<Type, Value1, Value2>
@@ -35,6 +40,8 @@ namespace crap
   public:
   constexpr const static Type value = (Value1 / Gcd) * Value2;
   #endif
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type ... Values> struct lcmValue
@@ -43,7 +50,37 @@ namespace crap
   template <Type ... SubValues> using This = lcmValue<Type, SubValues...>;
   public:
   constexpr const static Type value = reduceValue <Type, This, Values...> :: value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 }
+
+template <class Type>
+	inline constexpr crap :: lcmValue <Type> :: operator
+	typename crap :: lcmValue <Type> :: value_type () const noexcept
+{
+ return crap :: lcmValue <Type> :: value;
+};
+
+template <class Type, Type Value>
+	inline constexpr crap :: lcmValue <Type, Value> :: operator
+	typename crap :: lcmValue <Type, Value> :: value_type () const noexcept
+{
+ return crap :: lcmValue <Type, Value> :: value;
+};
+
+template <class Type, Type Value1, Type Value2>
+	inline constexpr crap :: lcmValue <Type, Value1, Value2> :: operator
+	typename crap :: lcmValue <Type, Value1, Value2> :: value_type () const noexcept
+{
+ return crap :: lcmValue <Type, Value1, Value2> :: value;
+};
+
+template <class Type, Type ... Values>
+	inline constexpr crap :: lcmValue <Type, Values...> :: operator
+	typename crap :: lcmValue <Type, Values...> :: value_type () const noexcept
+{
+ return crap :: lcmValue <Type, Values...> :: value;
+};
 #endif
 
