ba655e07d3682f4b066f246bc68509ed37f6e031
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2022-06-07 00:41:26 +0200

    Unify front operator acting on values.

--- include/crap/utility.d/frontvalue.h
+++ include/crap/utility.d/frontvalue.h
@@ -8,6 +8,8 @@ namespace crap
  template <class Type, Type First, Type ... Rest> struct frontValue<Type, First, Rest...>
  {
   constexpr const static Type value = First;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 
  template <class Type, Type ... Values> struct frontValue
@@ -15,5 +17,11 @@ namespace crap
   static_assert(sizeof...(Values) != 0u, "Collection must not be empty.");
  };
 }
-#endif
 
+template <class Type, Type Front, Type ... Rest>
+	inline constexpr crap :: frontValue <Type, Front, Rest...> :: operator
+	typename crap :: frontValue <Type, Front, Rest...> :: value_type () const noexcept
+{
+ return crap :: frontValue <Type, Front, Rest...> :: value;
+};
+#endif
