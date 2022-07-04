111a75055802a3abe02a71985014532420594e7b
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2022-07-05 01:12:39 +0200

    Unify contract operator acting on types.

--- include/crap/numeric.d/contracttype.h
+++ include/crap/numeric.d/contracttype.h
@@ -11,6 +11,8 @@ namespace crap
 
  template <intmax_t Numerator, intmax_t Denominator> struct contractType<std :: ratio<Numerator, Denominator> >
 	 : typeIdentity<typename std :: ratio <Numerator, Denominator> :: type> {};
+
+ template <class Type> using contractType_t = typename contractType <Type> :: type;
 }
 #endif
 
