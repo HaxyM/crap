9cf75424b9e43b3e8d88bb5dce67a0edb0981022
Igor Zarzycki <izarzycki@dmcs.pl>
2022-08-07 15:26:37 +0200

    Initial version of sqrt2 in numbers header.

---
+++ include/crap/numbers.d/sqrt2.h
@@ -0,0 +1,24 @@
+#ifndef CRAP_NUMBERS_SQRT2TYPE
+#define CRAP_NUMBERS_SQRT2TYPE
+
+namespace crap
+{
+ template <class> struct sqrt2;
+
+ template <> struct sqrt2<float>
+ {
+  constexpr const static float value = 1.4142135623730950488f;
+ };
+
+ template <> struct sqrt2<double>
+ {
+  constexpr const static double value = 1.4142135623730950488;
+ };
+
+ template <> struct sqrt2<double>
+ {
+  constexpr const static double value = 1.4142135623730950488l;
+ };
+}
+#endif
+
