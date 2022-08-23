34a3061d0bcc64a7a68dcb70fb26fff902a73eac
Igor Zarzycki <53280107+HaxyM@users.noreply.github.com>
2022-08-23 12:42:13 +0200

    Unify popcount function acting on values.

--- include/crap/bit.d/popcountvalue.h
+++ include/crap/bit.d/popcountvalue.h
@@ -24,16 +24,28 @@ namespace crap
  template <class Type, Type Value> struct popcountValue
  {
   private:
-  constexpr const static std :: size_t any = zero <std :: size_t> :: value;
-  constexpr const static std :: size_t zero = any ^ any;
-  constexpr const static std :: size_t one = ~((~zero) << 1u);
+  constexpr const static Type any = zero <Type> :: value;
+  constexpr const static Type zeros = any ^ any;
+  constexpr const static Type ones = ~zeros;
+  constexpr const static Type one = (~(ones << 1u)) & ones;
   constexpr const static std :: size_t bits = CHAR_BIT * sizeof(Type);
   template <std :: size_t ... Indices>
-	  using sum = plusValue<std :: size_t, ((((one << Indices) & Value) == zero) ? 0u : 1u)...>;
+	  using sum = plusValue<std :: size_t, ((((one << Indices) & Value) == zeros) ? 0u : 1u)...>;
   public:
   constexpr const static std :: size_t value = iotaValue <bits, std :: size_t> :: template type <sum> :: value;
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
  };
 #endif
 }
-#endif
 
+#if CPP20
+#else
+template <class Type, Type Value>
+inline constexpr crap :: popcountValue <Type, Value> :: operator
+typename crap :: popcountValue <Type, Value> :: value_type () const noexcept
+{
+ return crap :: popcountValue <Type, Value> :: value;
+}
+#endif
+#endif
