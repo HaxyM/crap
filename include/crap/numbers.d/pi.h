28230b3d22002e33e282051afd8a6de4f5ec7f81
Igor Zarzycki <izarzycki@dmcs.pl>
2022-12-05 23:08:56 +0100

    Add pi operator.

---
+++ include/crap/numbers.d/pi.h
@@ -0,0 +1,63 @@
+#ifndef CRAP_NUMBERS_PI
+#define CRAP_NUMBERS_PI
+
+namespace crap
+{
+ template <class> struct pi;
+
+ template <> struct pi<float>
+ {
+  constexpr const static float value = 3.14159265358979323846f;
+ };
+
+ template <> struct pi<double>
+ {
+  constexpr const static double value = 3.14159265358979323846;
+ };
+
+ template <> struct pi<long double>
+ {
+  constexpr const static double value = 3.14159265358979323846l;
+ };
+}
+
+#include <limits>
+#include <ratio>
+#include <type_traits>
+
+#include "identity.h"
+#include "../numeric.d/contracttype.h"
+#include "../ratio.d/identity.h"
+#include "../ratio.d/pi.h"
+
+namespace crap
+{
+ template <std :: intmax_t Numerator, std :: intmax_t Denominator>
+	 struct pi<std :: ratio<Numerator, Denominator> >
+ {
+  private:
+  using scaleType = const long double;
+  using valueType = typename std :: add_const<typename std :: make_unsigned <std :: intmax_t> :: type> :: type;
+  constexpr const static valueType int_1 = identity <valueType> :: value;
+  using ratio_1 = typename identity <valueRatio<std :: intmax_t, '+', int_1, int_1> > :: type;
+  using value = typename pi <ratio_1> :: type;
+  constexpr static scaleType infinity = std :: numeric_limits <scaleType> :: infinity();
+  constexpr static scaleType max = static_cast<scaleType>(std :: numeric_limits<std :: intmax_t> :: max());
+  constexpr static scaleType numFloat = static_cast<scaleType>(value :: num);
+  constexpr static scaleType denFloat = static_cast<scaleType>(value :: den);
+  constexpr static const bool numOverflow = (max <= numFloat);
+  constexpr static const bool denOverflow = (max <= denFloat);
+  constexpr static const bool needScale = numOverflow || denOverflow;
+  constexpr static scaleType numScale = numOverflow ? (max / numFloat) : infinity;
+  constexpr static scaleType denScale = denOverflow ? (max / denFloat) : infinity;
+  constexpr static scaleType scale = (numScale < denScale) ? numScale : denScale;
+  constexpr static std :: intmax_t num =
+	  needScale ? static_cast<std :: intmax_t>(scale * numFloat) : static_cast<std :: intmax_t>(value :: num);
+  constexpr static std :: intmax_t den =
+	  needScale ? static_cast<std :: intmax_t>(scale * denFloat) : static_cast<std :: intmax_t>(value :: den);
+  public:
+  using type = typename contractType <std :: ratio<num, den> > :: type;
+ };
+}
+#endif
+
---
+++ include/crap/ratio.d/pi.h
@@ -0,0 +1,137 @@
+#include <cstdint>
+#include <type_traits>
+
+#include "dividestype.h"
+#include "identity.h"
+#include "minustype.h"
+#include "multipliestype.h"
+#include "plustype.h"
+#include "sqrt2.h"
+#include "sqrttype.h"
+#include "valueratio.h"
+#include "zero.h"
+#include "../numbers.d/pi.h"
+#include "../functional.d/equaltovalue.h"
+
+#ifndef CRAP_RATIO_PI
+#define CRAP_RATIO_PI
+
+namespace crap
+{
+#ifdef UINT8_C
+ template <char Sign, typename std :: make_unsigned <std :: int8_t> :: type Numerator, typename std :: make_unsigned <std :: int8_t> :: type Denominator>
+	 struct pi<valueRatio<std :: int8_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: int8_t, '+', UINT8_C(85), UINT8_C(27)>;
+ };
+
+ template <char Sign, typename std :: make_unsigned <std :: uint8_t> :: type Numerator, typename std :: make_unsigned <std :: uint8_t> :: type Denominator>
+	 struct pi<valueRatio<std :: uint8_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: uint8_t, '+', UINT8_C(85), UINT8_C(27)>;
+ };
+#endif
+
+#ifdef UINT16_C
+ template <char Sign, typename std :: make_unsigned <std :: int16_t> :: type Numerator, typename std :: make_unsigned <std :: int16_t> :: type Denominator>
+	 struct pi<valueRatio<std :: int16_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: int16_t, '+', UINT16_C(23513), UINT16_C(7487)>;
+ };
+
+ template <char Sign, typename std :: make_unsigned <std :: uint16_t> :: type Numerator, typename std :: make_unsigned <std :: uint16_t> :: type Denominator>
+	 struct pi<valueRatio<std :: uint16_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: uint16_t, '+', UINT16_C(23513), UINT16_C(7487)>;
+ };
+#endif
+
+#ifdef UINT32_C
+ template <char Sign, typename std :: make_unsigned <std :: int32_t> :: type Numerator, typename std :: make_unsigned <std :: int32_t> :: type Denominator>
+	 struct pi<valueRatio<std :: int32_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: int32_t, '+', UINT32_C(3082798721), UINT32_C(981285313)>;
+ };
+
+ template <char Sign, typename std :: make_unsigned <std :: uint32_t> :: type Numerator, typename std :: make_unsigned <std :: uint32_t> :: type Denominator>
+	 struct pi<valueRatio<std :: uint32_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: uint32_t, '+', UINT32_C(3082798721), UINT32_C(981285313)>;
+ };
+#endif
+
+#ifdef UINT64_C
+ template <char Sign, typename std :: make_unsigned <std :: int64_t> :: type Numerator, typename std :: make_unsigned <std :: int64_t> :: type Denominator>
+	 struct pi<valueRatio<std :: int64_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: int64_t, '+', UINT64_C(18446744073709551615), UINT64_C(5871781006564002461)>;
+ };
+
+ template <char Sign, typename std :: make_unsigned <std :: uint64_t> :: type Numerator, typename std :: make_unsigned <std :: uint64_t> :: type Denominator>
+	 struct pi<valueRatio<std :: uint64_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: uint64_t, '+', UINT64_C(18446744073709551615), UINT64_C(5871781006564002461)>;
+ };
+#endif
+
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 struct pi<valueRatio<Type, Sign, Numerator, Denominator> >
+ {
+  private:
+  using passed = valueRatio<Type, Sign, Numerator, Denominator>;
+  using const1 = typename identity <passed> :: type;
+  using const4 = typename plusType <const1, const1, const1, const1> :: type;
+  using initialA = typename identity <const1> :: type;
+  using initialB = typename dividesType <const1, typename sqrt2 <const1> :: type> :: type;
+  using initialT = typename dividesType <const1, const4> :: type;
+  using initialP = typename identity <const1> :: type;
+  using numSum = typename plusType <initialA, initialB> :: type;
+  using num = typename multipliesType <numSum, numSum> :: type;
+  using initialResult = typename dividesType <num, const4, initialT> :: type;
+  template <class A, class B, class T, class P, class prevResult, bool isFinal> struct step;
+  template <class A, class B, class T, class P, class prevResult> struct step<A, B, T, P, prevResult, true>;
+  public:
+  using type = typename step <initialA, initialB, initialT, initialP, initialResult, false> :: type;
+ };
+
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 template <class A, class B, class T, class P, class prevResult, bool isFinal>
+	 struct pi<valueRatio<Type, Sign, Numerator, Denominator> > :: step
+ {
+  private:
+  using passed = valueRatio<Type, Sign, Numerator, Denominator>;
+  using const1 = typename identity <passed> :: type;
+  using const2 = typename plusType <const1, const1> :: type;
+  using const4 = typename plusType <const1, const1, const1, const1> :: type;
+  using nextA = typename dividesType <typename plusType <A, B> :: type, const2> :: type;
+  using nextB = typename sqrtType <typename multipliesType <A, B> :: type> :: type;
+  using aDiff = typename minusType <A, nextA> :: type;
+  using nextT = typename minusType <T, typename multipliesType <P, aDiff, aDiff> :: type> :: type;
+  using nextP = typename plusType <P, P> :: type;
+  using numSum = typename plusType <nextA, nextB> :: type;
+  using num = typename multipliesType <numSum, numSum> :: type;
+  using nextResult = typename dividesType <num, const4, nextT> :: type;
+  using cond = typename minusType <prevResult, nextResult> :: type;
+  constexpr const static bool nextFinal = //FIXME: Add check if is zero.
+	  equalToValue <decltype(cond :: num), cond :: num, zero <decltype(cond :: num)> :: value> :: value;
+  public:
+  using type = typename step <nextA, nextB, nextT, nextP, nextResult, nextFinal> :: type;
+ };
+
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 template <class A, class B, class T, class P, class prevResult>
+ struct pi<valueRatio<Type, Sign, Numerator, Denominator> > :: template
+	 step<A, B, T, P, prevResult, true>
+ {
+  private:
+  using passed = valueRatio<Type, Sign, Numerator, Denominator>;
+  using const1 = typename identity <passed> :: type;
+  using const4 = typename plusType <const1, const1, const1, const1> :: type;
+  using numSum = typename plusType <A, B> :: type;
+  using num = typename multipliesType <numSum, numSum> :: type;
+  public:
+  using type = typename dividesType <num, const4, T> :: type;
+ };
+}
+#endif
+
