d4bc4192d1ac3b5e3d36c86a8660b9de5aa0258e
Igor Zarzycki <izarzycki@dmcs.pl>
2022-12-08 01:12:22 +0100

    Add cbrt2 operator.

---
+++ include/crap/numbers.d/cbrt2.h
@@ -0,0 +1,63 @@
+#ifndef CRAP_NUMBERS_CBRT2
+#define CRAP_NUMBERS_CBRT2
+
+namespace crap
+{
+ template <class> struct cbrt2;
+
+ template <> struct cbrt2<float>
+ {
+  constexpr const static float value = 1.25992104989487316477f;
+ };
+
+ template <> struct cbrt2<double>
+ {
+  constexpr const static double value = 1.25992104989487316477;
+ };
+
+ template <> struct cbrt2<long double>
+ {
+  constexpr const static double value = 1.25992104989487316477l;
+ };
+}
+
+#include <limits>
+#include <ratio>
+#include <type_traits>
+
+#include "identity.h"
+#include "../numeric.d/contracttype.h"
+#include "../ratio.d/identity.h"
+#include "../ratio.d/cbrt2.h"
+
+namespace crap
+{
+ template <std :: intmax_t Numerator, std :: intmax_t Denominator>
+	 struct cbrt2<std :: ratio<Numerator, Denominator> >
+ {
+  private:
+  using scaleType = const long double;
+  using valueType = typename std :: add_const<typename std :: make_unsigned <std :: intmax_t> :: type> :: type;
+  constexpr const static valueType int_1 = identity <valueType> :: value;
+  using ratio_1 = typename identity <valueRatio<std :: intmax_t, '+', int_1, int_1> > :: type;
+  using value = typename cbrt2 <ratio_1> :: type;
+  constexpr static scaleType infinity = std :: numeric_limits <scaleType> :: infinity();
+  constexpr static scaleType max = static_cast<scaleType>(std :: numeric_limits<std :: intmax_t> :: max());
+  constexpr static scaleType numFloat = static_cast<scaleType>(value :: num);
+  constexpr static scaleType denFloat = static_cast<scaleType>(value :: den);
+  constexpr static const bool numOverflow = (max <= numFloat);
+  constexpr static const bool denOverflow = (max <= denFloat);
+  constexpr static const bool needScale = numOverflow || denOverflow;
+  constexpr static scaleType numScale = numOverflow ? (max / numFloat) : infinity;
+  constexpr static scaleType denScale = denOverflow ? (max / denFloat) : infinity;
+  constexpr static scaleType scale = (numScale < denScale) ? numScale : denScale;
+  constexpr static std :: intmax_t num =
+	  needScale ? static_cast<std :: intmax_t>(scale * numFloat) : static_cast<std :: intmax_t>(value :: num);
+  constexpr static std :: intmax_t den =
+	  needScale ? static_cast<std :: intmax_t>(scale * denFloat) : static_cast<std :: intmax_t>(value :: den);
+  public:
+  using type = typename contractType <std :: ratio<num, den> > :: type;
+ };
+}
+#endif
+
---
+++ include/crap/ratio.d/cbrt2.h
@@ -0,0 +1,117 @@
+#include <cstdint>
+#include <type_traits>
+
+#include "dividestype.h"
+#include "identity.h"
+#include "minustype.h"
+#include "multipliestype.h"
+#include "plustype.h"
+#include "valueratio.h"
+#include "zero.h"
+#include "../numbers.d/cbrt2.h"
+#include "../functional.d/equaltovalue.h"
+
+#ifndef CRAP_RATIO_CBRT2
+#define CRAP_RATIO_CBRT2
+
+namespace crap
+{
+#ifdef UINT8_C
+ template <char Sign, typename std :: make_unsigned <std :: int8_t> :: type Numerator, typename std :: make_unsigned <std :: int8_t> :: type Denominator>
+	 struct cbrt2<valueRatio<std :: int8_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: int8_t, '+', UINT8_C(85), UINT8_C(67)>;
+ };
+
+ template <char Sign, typename std :: make_unsigned <std :: uint8_t> :: type Numerator, typename std :: make_unsigned <std :: uint8_t> :: type Denominator>
+	 struct cbrt2<valueRatio<std :: uint8_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: uint8_t, '+', UINT8_C(85), UINT8_C(67)>;
+ };
+#endif
+
+#ifdef UINT16_C
+ template <char Sign, typename std :: make_unsigned <std :: int16_t> :: type Numerator, typename std :: make_unsigned <std :: int16_t> :: type Denominator>
+	 struct cbrt2<valueRatio<std :: int16_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: int16_t, '+', UINT16_C(65534), UINT16_C(52013)>;
+ };
+
+ template <char Sign, typename std :: make_unsigned <std :: uint16_t> :: type Numerator, typename std :: make_unsigned <std :: uint16_t> :: type Denominator>
+	 struct cbrt2<valueRatio<std :: uint16_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: uint16_t, '+', UINT16_C(65534), UINT16_C(52013)>;
+ };
+#endif
+
+#ifdef UINT32_C
+ template <char Sign, typename std :: make_unsigned <std :: int32_t> :: type Numerator, typename std :: make_unsigned <std :: int32_t> :: type Denominator>
+	 struct cbrt2<valueRatio<std :: int32_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: int32_t, '+', UINT32_C(858993459), UINT32_C(681783560)>;
+ };
+
+ template <char Sign, typename std :: make_unsigned <std :: uint32_t> :: type Numerator, typename std :: make_unsigned <std :: uint32_t> :: type Denominator>
+	 struct cbrt2<valueRatio<std :: uint32_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: uint32_t, '+', UINT32_C(858993459), UINT32_C(681783560)>;
+ };
+#endif
+
+#ifdef UINT64_C
+ template <char Sign, typename std :: make_unsigned <std :: int64_t> :: type Numerator, typename std :: make_unsigned <std :: int64_t> :: type Denominator>
+	 struct cbrt2<valueRatio<std :: int64_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: int64_t, '+', UINT64_C(18446744073709551615), UINT64_C(14641190473997345813)>;
+ };
+
+ template <char Sign, typename std :: make_unsigned <std :: uint64_t> :: type Numerator, typename std :: make_unsigned <std :: uint64_t> :: type Denominator>
+	 struct cbrt2<valueRatio<std :: uint64_t, Sign, Numerator, Denominator> >
+ {
+  using type = valueRatio<std :: uint64_t, '+', UINT64_C(18446744073709551615), UINT64_C(14641190473997345813)>;
+ };
+#endif
+
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 struct cbrt2<valueRatio<Type, Sign, Numerator, Denominator> >
+ {
+  private:
+  using passed = valueRatio<Type, Sign, Numerator, Denominator>;
+  using const1 = typename identity <passed> :: type;
+  using initialA = typename dividesType <const1, typename plusType <const1, const1> :: type> :: type;
+  using initialC = typename minusType <typename multipliesType <initialA, initialA> :: type, const1> :: type;
+  template <class A, class C, bool isFinal> struct step;
+  template <class A, class C> struct step<A, C, true>;
+  public:
+  using type = typename step <initialA, initialC, false> :: type;
+ };
+
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 template <class A, class C, bool isFinal>
+	 struct cbrt2<valueRatio<Type, Sign, Numerator, Denominator> > :: step
+ {
+  private:
+  using const1 = typename identity <A> :: type;
+  using const3 = typename plusType <const1, const1, const1> :: type;
+  using thirdC = typename dividesType <C, const3> :: type;
+  using C1 = typename plusType <const1, C> :: type;
+  using C2 = typename minusType <const1, thirdC> :: type;
+  using C3 = typename multipliesType <C1, C2, C2, C2> :: type;
+  using nextC = typename minusType <C3, const1> :: type;
+  using nextA = typename multipliesType <A, C2> :: type;
+  using cond = typename minusType <A, nextA> :: type;
+  constexpr const static bool nextFinal = //FIXME: Add check if is zero.
+	  equalToValue <decltype(cond :: num), cond :: num, zero <decltype(cond :: num)> :: value> :: value;
+  public:
+  using type = typename step <nextA, nextC, nextFinal> :: type;
+ };
+
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 template <class A, class C>
+	 struct cbrt2<valueRatio<Type, Sign, Numerator, Denominator> > :: template step<A, C, true>
+ {
+  using type = valueRatio<Type, A :: sign, A :: den, A :: num>;
+ };
+}
+#endif
+
