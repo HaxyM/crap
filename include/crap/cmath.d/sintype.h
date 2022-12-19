f0af2444766756a3abd15f7207f7666cf70164b1
Igor Zarzycki <izarzycki@dmcs.pl>
2022-12-19 23:14:52 +0100

    Unify sin function acting on types.

--- include/crap/cmath.d/sintype.h
+++ include/crap/cmath.d/sintype.h
@@ -1,35 +1,99 @@
 #ifndef CRAP_CMATH_SINTYPE
 #define CRAP_CMATH_SINTYPE
 
+#include "../functional.d/dividestype.h"
+#include "../functional.d/minustype.h"
 #include "../functional.d/multipliestype.h"
-#include "../functional.d/negatetype.h"
 #include "../functional.d/plustype.h"
+#include "../numbers.d/identity.h"
+#include "../numeric.d/innerproducttype.h"
 #include "../numeric.d/iotavalue.h"
+#include "../utility.d/reproducetype.h"
 
 #include <cstddef>
 #include <ratio>
+#include <type_traits>
 
 namespace crap
 {
- template <class, std :: size_t Steps = 10u> struct sinType;
+ template <class> struct sinType;
+}
 
- template <intmax_t Numerator, intmax_t Denominator, std :: size_t Steps>
-	 struct sinType<std :: ratio<Numerator, Denominator>, Steps>
+#include <limits>
+#include <ratio>
+#include <type_traits>
+
+#include "../functional.d/lesstype.h"
+#include "../numbers.d/identity.h"
+#include "../numbers.d/zero.h"
+#include "../numeric.d/contracttype.h"
+#include "../ratio.d/identity.h"
+#include "../ratio.d/sintype.h"
+
+namespace crap
+{
+ template <intmax_t Numerator, intmax_t Denominator>
+	 struct sinType<std :: ratio<Numerator, Denominator> >
  {
   private:
-  using x = std :: ratio<Numerator, Denominator>;
-  template <std :: intmax_t N> using constant1 = std :: ratio<1, 2 * N + 1>;
-  template <std :: intmax_t N> using constant2 = std :: ratio<1, 2 * N + 2>;
-  template <std :: intmax_t N> using prodTerm1 = typename multipliesType <x, constant1<N> > :: type;
-  template <std :: intmax_t N> using prodTerm2 = typename multipliesType <x, constant2<N> > :: type;
-  template <std :: intmax_t N>
-	  using prodTerm = typename negateType <typename multipliesType <prodTerm1<N>, prodTerm2<N> > :: type> :: type;
-  template <std :: intmax_t ... N>
-	  using product = typename multipliesType <prodTerm<N>..., prodTerm1<sizeof...(N)> > :: type;
-  template <std :: size_t N> using sumTerm = typename iotaValue <N, std :: intmax_t, 0> :: template type<product>;
-  template <std :: size_t ... N> using sum = typename plusType <x, sumTerm<N>...> :: type;
+  using scaleType = const long double;
+  using valueType = typename std :: add_const<typename std :: make_unsigned <std :: intmax_t> :: type> :: type;
+  using orig = typename contractType <std :: ratio<Numerator, Denominator> > :: type;
+  constexpr static const bool origNegative = lessType <orig, typename zero <orig> :: type> :: value;
+  constexpr static scaleType infinity = std :: numeric_limits <scaleType> :: infinity();
+  constexpr static scaleType max = static_cast<scaleType>(std :: numeric_limits<std :: intmax_t> :: max());
+  constexpr static const bool origNumOverflow =
+	  (origNegative && (orig :: num < zero <std :: intmax_t> :: value)) ? (zero <std :: intmax_t> :: value > (max + orig :: num)) : false;
+  constexpr static const bool origDenOverflow =
+	  (origNegative && (orig :: den < zero <std :: intmax_t> :: value)) ? (zero <std :: intmax_t> :: value > (max + orig :: den)) : false;
+  constexpr static scaleType origNumScale = origNumOverflow ? (static_cast<scaleType>(max) / static_cast<scaleType>(orig :: num)) : infinity;
+  constexpr static scaleType origDenScale = origDenOverflow ? (static_cast<scaleType>(max) / static_cast<scaleType>(orig :: den)) : infinity;
+  constexpr static const bool origNeedScale = origNumOverflow || origDenOverflow;
+  constexpr static scaleType origScale = (origNumScale < origDenScale) ? origNumScale : origDenScale;
+  constexpr static scaleType sign = origNegative ? -1.0l : 1.0l;
+  constexpr static scaleType origNumFloat =
+	  origNeedScale ? (origScale * sign * static_cast<scaleType>(orig :: num)) : static_cast<scaleType>(origNegative ? (-orig :: num) : (orig :: num));
+  constexpr static scaleType origDenFloat =
+	  origNeedScale ? (origScale * sign * static_cast<scaleType>(orig :: den)) : static_cast<scaleType>(origNegative ? (-orig :: den) : (orig :: den));
+  constexpr static valueType origScaledNumInRange =
+	  (origNumFloat < 0.0l) ? zero <valueType> :: value : (origNumFloat > static_cast<scaleType>(max) ? max : static_cast<valueType>(origNumFloat));
+  constexpr static valueType origScaledDenInRange =
+	  (origDenFloat < 1.0l) ? identity <valueType> :: value : (origDenFloat > static_cast<scaleType>(max) ? max : static_cast<valueType>(origDenFloat));
+  constexpr const static valueType origNum =
+	  origNeedScale ? origScaledNumInRange : static_cast<valueType>((orig :: num < zero <std :: intmax_t> :: value) ? (- orig :: num) : (orig :: num));
+  constexpr const static valueType origDen =
+	  origNeedScale ? origScaledDenInRange : static_cast<valueType>((orig :: den < zero <std :: intmax_t> :: value) ? (- orig :: den) : (orig :: den));
+  using value = typename sinType <valueRatio<std :: intmax_t, (origNegative ? '-' : '+'), origNum, origDen> > :: type;
+  constexpr static const bool valueNegative = (value :: sign == '-');
+  constexpr static const std :: intmax_t maxVal = std :: numeric_limits<std :: intmax_t> :: max();
+  constexpr static const std :: intmax_t minVal = std :: numeric_limits<std :: intmax_t> :: min();
+  constexpr static scaleType signFloat = valueNegative ? -1.0l : 1.0l;
+  constexpr static scaleType numFloat = signFloat * static_cast<scaleType>(value :: num);
+  constexpr static scaleType denFloat = static_cast<scaleType>(value :: den);
+  constexpr static const bool numOverflow = (max <= numFloat);
+  constexpr static const bool denOverflow = (max <= denFloat);
+  constexpr static const bool numUnderflow = (static_cast<scaleType>(minVal) >= numFloat);
+  constexpr static const bool denUnderflow = (static_cast<scaleType>(minVal) >= denFloat);
+  constexpr static const bool needScale = numOverflow || denOverflow || numUnderflow || denUnderflow;
+  constexpr static scaleType numOverScale = numOverflow ? (max / numFloat) : infinity;
+  constexpr static scaleType denOverScale = denOverflow ? (max / denFloat) : infinity;
+  constexpr static scaleType numUnderScale = numUnderflow ? (static_cast<scaleType>(minVal) / numFloat) : infinity;
+  constexpr static scaleType denUnderScale = denUnderflow ? (static_cast<scaleType>(minVal) / denFloat) : infinity;
+  constexpr static scaleType overScale = (numOverScale < denOverScale) ? numOverScale : denOverScale;
+  constexpr static scaleType underScale = (numUnderScale < denUnderScale) ? numUnderScale : denUnderScale;
+  constexpr static scaleType scale = (overScale < underScale) ? overScale : underScale;
+  constexpr static scaleType scaledNum = needScale ? (scale * numFloat) : numFloat;
+  constexpr static scaleType scaledDen = needScale ? (scale * denFloat) : denFloat;
+  constexpr static std :: intmax_t scaledNumInRange =
+	  (scaledNum < static_cast<scaleType>(minVal)) ? minVal : ((scaledNum > max) ? maxVal : static_cast<std :: intmax_t>(scaledNum));
+  constexpr static std :: intmax_t scaledDenInRange =
+	  (scaledDen < 1.0l) ? identity <std :: intmax_t> :: value : ((scaledDen > max) ? maxVal : static_cast<std :: intmax_t>(scaledDen));
+  constexpr static std :: intmax_t num =
+	  needScale ? static_cast<std :: intmax_t>(scaledNumInRange) : (valueNegative ? -static_cast<std :: intmax_t>(value :: num) : static_cast<std :: intmax_t>(value :: num));
+  constexpr static std :: intmax_t den =
+	  needScale ? static_cast<std :: intmax_t>(scaledDenInRange) : static_cast<std :: intmax_t>(value :: den);
   public:
-  using type = typename iotaValue <Steps, std :: size_t, 1> :: template type<sum>;
+  using type = typename contractType <std :: ratio<num, den> > :: type;
  };
 
  template <intmax_t Numerator, intmax_t Denominator>
--- include/crap/ratio.d/sintype.h
+++ include/crap/ratio.d/sintype.h
@@ -1,39 +1,79 @@
+#include <cstddef>
+#include <cstdint>
+#include <type_traits>
+
+#include "abstype.h"
+#include "dividestype.h"
+#include "floortype.h"
+#include "identity.h"
+#include "lesstype.h"
+#include "minustype.h"
+#include "multipliestype.h"
+#include "negatetype.h"
+#include "pi.h"
+#include "plustype.h"
+#include "valueratio.h"
+#include "zero.h"
+#include "../cmath.d/sintype.h"
+#include "../functional.d/equaltovalue.h"
+
 #ifndef CRAP_RATIO_SINTYPE
 #define CRAP_RATIO_SINTYPE
 
-#include "identity.h"
-#include "negatetype.h"
-#include "multipliestype.h"
-#include "plustype.h"
-#include "valueratio.h"
-#include "../cmath.d/sintype.h"
-#include "../numbers.d/identity.h"
-#include "../numeric.d/iotavalue.h"
-
-#include <cstddef>
-#include <ratio>
-#include <type_traits>
-
 namespace crap
 {
- template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator, std :: size_t Steps>
-	 struct sinType<valueRatio<Type, Sign, Numerator, Denominator>, Steps>
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 struct sinType<valueRatio<Type, Sign, Numerator, Denominator> >
  {
   private:
-  using valueType = typename std :: make_unsigned <Type> :: type;
-  using x = valueRatio<Type, Sign, Numerator, Denominator>;
-  template <valueType N> using constant1 = valueRatio<Type, '+', identity <valueType> :: value, 2u * N + 1u>;
-  template <valueType N> using constant2 = valueRatio<Type, '+', identity <valueType> :: value, 2u * N + 2u>;
-  template <valueType N> using prodTerm1 = typename multipliesType <x, constant1<N> > :: type;
-  template <valueType N> using prodTerm2 = typename multipliesType <x, constant2<N> > :: type;
-  template <valueType N>
-	  using prodTerm = typename negateType <typename multipliesType <prodTerm1<N>, prodTerm2<N> > :: type> :: type;
-  template <valueType ... N>
-	  using product = typename multipliesType <prodTerm<N>..., prodTerm1<sizeof...(N)> > :: type;
-  template <std :: size_t N> using sumTerm = typename iotaValue <N, valueType, 0> :: template type<product>;
-  template <std :: size_t ... N> using sum = typename plusType <x, sumTerm<N>...> :: type;
+  using passed = typename absType <valueRatio<Type, Sign, Numerator, Denominator> > :: type;
+  using const1 = typename identity <passed> :: type;
+  using constPi = typename pi <const1> :: type;
+  using constTau = typename plusType <constPi, constPi> :: type;
+  using periods = typename dividesType <passed, constTau> :: type;
+  using wholePeriods = typename floorType <periods> :: type;
+  using shift = typename multipliesType <constTau, wholePeriods> :: type;
+  using x = typename minusType <passed, shift> :: type;
+  using initialSum = x;
+  using initialElement = x;
+  using initialArgument = typename negateType <typename multipliesType <x, x> :: type> :: type;
+  using initialStep = typename identity <passed> :: type;
+  template <class Sum, class Element, class Argument, class Step, bool isFinal> struct step;
+  template <class Sum, class Element, class Argument, class Step> struct step<Sum, Element, Argument, Step, true>;
   public:
-  using type = typename iotaValue <Steps, std :: size_t, 1u> :: template type<sum>;
+  using type = typename step <initialSum, initialElement, initialArgument, initialStep, false> :: type;
+ };
+
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 template <class Sum, class Element, class Argument, class Step, bool isFinal>
+	 struct sinType<valueRatio<Type, Sign, Numerator, Denominator> > :: step
+ {
+  private:
+  using passed = valueRatio<Type, Sign, Numerator, Denominator>;
+  using newNumerator = typename multipliesType <Element, Argument> :: type;
+  using const1 = typename identity <passed> :: type;
+  using nextStep1 = typename plusType <Step, const1> :: type;
+  using nextElement1 = typename dividesType <newNumerator, nextStep1> :: type;
+  using nextStep = typename plusType <nextStep1, const1> :: type;
+  using nextElement = typename dividesType <nextElement1, nextStep> :: type;
+  using sum = typename plusType <Sum, nextElement> :: type;
+  using cond = typename minusType <sum, Sum> :: type;
+  constexpr const static bool nextFinal = //FIXME: Add check if is zero.
+	  equalToValue <decltype(cond :: num), cond :: num, zero <decltype(cond :: num)> :: value> :: value;
+  public:
+  using type = typename step <sum, nextElement, Argument, nextStep, nextFinal> :: type;
+ };
+
+ template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+	 template <class Sum, class Element, class Argument, class Step>
+ struct sinType<valueRatio<Type, Sign, Numerator, Denominator> > :: template
+	 step<Sum, Element, Argument, Step, true>
+ {
+  private:
+  constexpr const static char newSign =
+	  (Sign == '-') ? ((Sum :: sign == '-') ? '+' : '-') : (Sum :: sign);
+  public:
+  using type = valueRatio<Type, newSign, Sum :: num, Sum :: den>;
  };
 
  template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
