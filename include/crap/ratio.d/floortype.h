c116cd51a3c3af734f66413309a8bc686252aecd
Igor Zarzycki <izarzycki@dmcs.pl>
2023-05-08 23:03:59 +0200

    Adapt rounding functions to new value ratio.

--- include/crap/ratio.d/floortype.h
+++ include/crap/ratio.d/floortype.h
@@ -10,24 +10,24 @@
 
 namespace crap
 {
- template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+ template <class Type, char Sign, Type Numerator, Type Denominator>
 	 struct floorType<valueRatio<Type, Sign, Numerator, Denominator> >
  {
   private:
   using value = typename contractType <valueRatio<Type, Sign, Numerator, Denominator> > :: type;
   constexpr const static bool whole = ((value :: num % value :: den) == 0u);
   constexpr const static bool positive = (Sign == '+');
-  constexpr const static typename value :: numeratorType newNum = value :: num / value :: den;
+  constexpr const static typename value :: valueType newNum = value :: num / value :: den;
   public:
-  using type = valueRatio<Type, Sign, ((whole || positive) ? newNum : (newNum + 1u)), identity <typename value :: denominatorType> :: value>;
+  using type = valueRatio<Type, Sign, ((whole || positive) ? newNum : (newNum + 1u)), identity <typename value :: valueType> :: value>;
  };
 
- template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+ template <class Type, char Sign, Type Numerator, Type Denominator>
 	 constexpr typename floorType <valueRatio<Type, Sign, Numerator, Denominator> > :: type
 	 floor(valueRatio<Type, Sign, Numerator, Denominator>) noexcept;
 }
 
-template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+template <class Type, char Sign, Type Numerator, Type Denominator>
 inline constexpr typename crap :: floorType <crap :: valueRatio<Type, Sign, Numerator, Denominator> > :: type
 crap :: floor(crap :: valueRatio<Type, Sign, Numerator, Denominator>) noexcept
 {
--- include/crap/ratio.d/trunctype.h
+++ include/crap/ratio.d/trunctype.h
@@ -10,21 +10,21 @@
 
 namespace crap
 {
- template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+ template <class Type, char Sign, Type Numerator, Type Denominator>
 	 struct truncType<valueRatio<Type, Sign, Numerator, Denominator> >
  {
   private:
   using value = typename contractType <valueRatio<Type, Sign, Numerator, Denominator> > :: type;
   public:
-  using type = valueRatio<Type, Sign, (value :: num / value :: den), identity <typename value :: denominatorType> :: value>;
+  using type = valueRatio<Type, Sign, (value :: num / value :: den), identity <typename value :: valueType> :: value>;
  };
 
- template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+ template <class Type, char Sign, Type Numerator, Type Denominator>
 	 constexpr typename truncType <valueRatio<Type, Sign, Numerator, Denominator> > :: type
 	 trunc(valueRatio<Type, Sign, Numerator, Denominator>) noexcept;
 }
 
-template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+template <class Type, char Sign, Type Numerator, Type Denominator>
 inline constexpr typename crap :: truncType <crap :: valueRatio<Type, Sign, Numerator, Denominator> > :: type
 crap :: trunc(crap :: valueRatio<Type, Sign, Numerator, Denominator>) noexcept
 {
