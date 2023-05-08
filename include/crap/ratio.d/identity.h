f769a9ca167ffdee116d4a91a3c2dc222cbebfc4
Igor Zarzycki <izarzycki@dmcs.pl>
2023-05-08 20:38:36 +0200

    Adapt identity to new value ratio.

--- include/crap/ratio.d/identity.h
+++ include/crap/ratio.d/identity.h
@@ -9,9 +9,9 @@
 
 namespace crap
 {
- template <class Type, char Sign, typename std :: make_unsigned <Type> :: type Numerator, typename std :: make_unsigned <Type> :: type Denominator>
+ template <class Type, char Sign, Type Numerator, Type Denominator>
 	 struct identity<valueRatio<Type, Sign, Numerator, Denominator> >
-	 : typeIdentity<valueRatio<Type, '+', identity <typename std :: make_unsigned <Type> :: type> :: value, identity <typename std :: make_unsigned <Type> :: type> :: value> >
+	 : typeIdentity<valueRatio<Type, '+', identity <Type> :: value, identity <Type> :: value> >
  {};
 }
 #endif
