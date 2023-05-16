71f54fb815743213b012d3e435b359657b2719a9
Igor Zarzycki <izarzycki@dmcs.pl>
2023-05-16 19:47:10 +0200

    Adapt sign functions to new value ratio.

--- test/signbittypetest.cpp
+++ test/signbittypetest.cpp
@@ -16,12 +16,12 @@ int main()
  static_assert(!crap :: signbitType <std :: ratio<1, 1> >(), "Signbit of 1 should be false.");
  static_assert(crap :: signbitType <std :: ratio<-1, 1> >(), "Signbit of -1 should be true.");
 
- static_assert(crap :: signbitType <crap :: valueRatio<int, '-', 1u, 2u> >(), "Signbit of -1/2 should be true.");
- static_assert(!crap :: signbitType <crap :: valueRatio<int, '+', 1u, 2u> >(), "Signbit of 1/2 should be false.");
- static_assert(!crap :: signbitType <crap :: valueRatio<int, '+', 0u, 1u> >(), "Signbit of 0 should be false.");
- static_assert(crap :: signbitType <crap :: valueRatio<int, '-', 0u, 1u> >(), "Signbit of -0 should be true.");
- static_assert(!crap :: signbitType <crap :: valueRatio<int, '+', 1u, 1u> >(), "Signbit of 1 should be false.");
- static_assert(crap :: signbitType <crap :: valueRatio<int, '-', 1u, 1u> >(), "Signbit of -1 should be true.");
+ static_assert( crap :: signbitType <crap :: valueRatio<unsigned int, '-', 1u, 2u> >(), "Signbit of -1/2 should be true.");
+ static_assert(!crap :: signbitType <crap :: valueRatio<unsigned int, '+', 1u, 2u> >(), "Signbit of 1/2 should be false.");
+ static_assert(!crap :: signbitType <crap :: valueRatio<unsigned int, '+', 0u, 1u> >(), "Signbit of 0 should be false.");
+ static_assert( crap :: signbitType <crap :: valueRatio<unsigned int, '-', 0u, 1u> >(), "Signbit of -0 should be true.");
+ static_assert(!crap :: signbitType <crap :: valueRatio<unsigned int, '+', 1u, 1u> >(), "Signbit of 1 should be false.");
+ static_assert( crap :: signbitType <crap :: valueRatio<unsigned int, '-', 1u, 1u> >(), "Signbit of -1 should be true.");
 
  //FIXME: Looks like std :: ratio having problems itself on edge cases :(
  static_assert(crap :: signbitType <std :: ratio<(std :: numeric_limits <std :: intmax_t> :: min() + 1), 1> >(), "Should not fail on close to end of range values.");
@@ -34,12 +34,12 @@ int main()
  static_assert(!crap :: signbitType <std :: ratio<std :: numeric_limits <std :: intmax_t> :: max(), std :: numeric_limits <std :: intmax_t> :: max()> >(), "Should not fail on close to end of range values.");
  static_assert(!crap :: signbitType <std :: ratio<1, std :: numeric_limits <std :: intmax_t> :: max()> >(), "Should not fail on close to end of range values.");
 
- static_assert(!crap :: signbitType <crap :: valueRatio<int, '+', 1u, std :: numeric_limits <unsigned int> :: max()> >(), "Should not fail on close to end of range values.");
- static_assert(crap :: signbitType <crap :: valueRatio<int, '-', 1u, std :: numeric_limits <unsigned int> :: max()> >(), "Should not fail on close to end of range values.");
- static_assert(!crap :: signbitType <crap :: valueRatio<int, '+', std :: numeric_limits <unsigned int> :: max(), std :: numeric_limits <unsigned int> :: max()> >(), "Should not fail on close to end of range values.");
- static_assert(crap :: signbitType <crap :: valueRatio<int, '-', std :: numeric_limits <unsigned int> :: max(), std :: numeric_limits <unsigned int> :: max()> >(), "Should not fail on close to end of range values.");
- static_assert(!crap :: signbitType <crap :: valueRatio<int, '+', std :: numeric_limits <unsigned int> :: max(), 1u> >(), "Should not fail on close to end of range values.");
- static_assert(crap :: signbitType <crap :: valueRatio<int, '-', std :: numeric_limits <unsigned int> :: max(), 1u> >(), "Should not fail on close to end of range values.");
+ static_assert(!crap :: signbitType <crap :: valueRatio<unsigned int, '+', 1u, std :: numeric_limits <unsigned int> :: max()> >(), "Should not fail on close to end of range values.");
+ static_assert( crap :: signbitType <crap :: valueRatio<unsigned int, '-', 1u, std :: numeric_limits <unsigned int> :: max()> >(), "Should not fail on close to end of range values.");
+ static_assert(!crap :: signbitType <crap :: valueRatio<unsigned int, '+', std :: numeric_limits <unsigned int> :: max(), std :: numeric_limits <unsigned int> :: max()> >(), "Should not fail on close to end of range values.");
+ static_assert( crap :: signbitType <crap :: valueRatio<unsigned int, '-', std :: numeric_limits <unsigned int> :: max(), std :: numeric_limits <unsigned int> :: max()> >(), "Should not fail on close to end of range values.");
+ static_assert(!crap :: signbitType <crap :: valueRatio<unsigned int, '+', std :: numeric_limits <unsigned int> :: max(), 1u> >(), "Should not fail on close to end of range values.");
+ static_assert( crap :: signbitType <crap :: valueRatio<unsigned int, '-', std :: numeric_limits <unsigned int> :: max(), 1u> >(), "Should not fail on close to end of range values.");
 
  return EXIT_SUCCESS;
 }
