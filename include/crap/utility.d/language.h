507eb8ca707bc1b1734ee1b321b23d039595aa5e
Igor Zarzycki <izarzycki@dmcs.pl>
2024-07-29 01:47:03 +0200

    Remove old feature testing macros.

--- include/crap/utility.d/language.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRAP_UTILITY_LANGUAGE
-#define CRAP_UTILITY_LANGUAGE
-
-//In case of msvc requirec "/Zc:__cplusplus" flag to be set.
-#define CPP14 (__cplusplus >= 201402)
-#define CPP17 (__cplusplus >= 201703)
-#define CPP20 (__cplusplus >= 202002)
-
-#endif
