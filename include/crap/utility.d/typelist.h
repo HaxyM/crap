c8c69c7908d93e854fc099c7c0954b5b98e35ee1
Igor Zarzycki <izarzycki@dmcs.pl>
2024-07-29 14:13:20 +0200

    Add pack indexing support in typeList.

--- include/crap/utility.d/typelist.h
+++ include/crap/utility.d/typelist.h
@@ -5,6 +5,13 @@
 
 #include "makeindexsequence.h"
 
+#include "../version.d/packindexing.h"
+
+#if (crap_pack_indexing >= 202311L)
+#include "typeidentity.h"
+#else
+#endif
+
 namespace crap
 {
  template <class ... Types> struct typeList
@@ -13,13 +20,21 @@ namespace crap
   struct ignore;
   template <class ... SubTypes> using This = typeList<SubTypes...>;
   template <class Type> struct identity;
+#if (crap_pack_indexing >= 202311L)
+  template <std :: size_t N> using At = typeIdentity<Types...[N]>;
+#else
   template <std :: size_t N> struct At;
+#endif
   template <std :: size_t N, template <class...> class Container> struct Since;
   template <std :: size_t N, template <class...> class Container> struct Till;
   template <std :: size_t Begin, std :: size_t End, template <class...> class Container> struct SubRange;
   public:
   constexpr const static std :: size_t size = sizeof...(Types);
+#if (crap_pack_indexing >= 202311L)
+  template <std :: size_t N> using at = Types...[N];
+#else
   template <std :: size_t N> using at = typename At <N> :: type;
+#endif
   template <std :: size_t N, template <class...> class Container = This> using since = typename Since <N, Container> :: type;
   template <std :: size_t N, template <class...> class Container = This> using till = typename Till <N, Container> :: type;
   template <std :: size_t Begin, std :: size_t End, template <class...> class Container = This> using subRange = typename SubRange <Begin, End, Container> :: type;
@@ -32,6 +47,8 @@ namespace crap
 
  template <class ... Types> template <class Type> struct typeList <Types...> :: identity{};
 
+#if (crap_pack_indexing >= 202311L)
+#else
  template <class ... Types> template <std :: size_t N> struct typeList <Types...> :: At
  {
   private:
@@ -43,10 +60,11 @@ namespace crap
   public:
   using type = decltype(implementation :: generate(identity<Types>{}...));
  };
+#endif
 
  template <class ... Types> template <std :: size_t N, template <class ...> class Container>
  struct typeList <Types...> :: Since
- {
+ {//TODO: Check if using pack indexing can be better.
   private:
   static_assert(N <= (typeList <Types...> :: size), "Index out of range.");
   template <std :: size_t ... Indices> struct Implementation;
@@ -59,7 +77,7 @@ namespace crap
 
  template <class ... Types> template <std :: size_t N, template <class...> class Container>
  struct typeList <Types...> :: Till
- {//TODO: Find some better solution.
+ {
   private:
   static_assert(N <= (typeList <Types...> :: size), "Index out of range.");
   template <std :: size_t Index> using at = typename typeList <Types...> :: template at<Index>;
@@ -70,13 +88,15 @@ namespace crap
  
  template <class ... Types> template <std :: size_t Begin, std :: size_t End, template <class...> class Container>
  struct typeList <Types...> :: SubRange
- {//TODO: Find some better solution.
+ {//TODO: Check if using pack indexing can be better.
   private:
   template <class ... SubTypes> using Since = typename typeList <SubTypes...> :: template since <Begin, Container>;
   public:
   using type = typename typeList <Types...> :: template till<End, Since>;
  };
 
+#if (crap_pack_indexing >= 202311L)
+#else
  template <class ... Types> template <std :: size_t N> template <std :: size_t ... Indices>
  struct typeList <Types...> :: template At <N> :: Implementation
  {
@@ -84,6 +104,7 @@ namespace crap
   template <class NthType> static NthType
   generate(decltype(typename typeList <Types...> :: ignore(Indices))..., identity<NthType>, ...);
  };
+#endif
 
  template <class ... Types> template <std :: size_t N, template <class...> class Container>
  template <std :: size_t ... Indices>
