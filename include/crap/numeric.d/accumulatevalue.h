92eb5223b7eabd5cb5535f87434a7b6b05e260c4
Igor Zarzycki <izarzycki@dmcs.pl>
2024-08-17 20:07:43 +0200

    Make accumulate function acting on values uniformly callable.

--- include/crap/numeric.d/accumulatevalue.h
+++ include/crap/numeric.d/accumulatevalue.h
@@ -4,6 +4,7 @@
 #include <type_traits>
 
 #include "../utility.d/bisectvalue.h"
+#include "../version.d/libintegralconstantcallable.h"
 
 namespace crap
 {
@@ -12,13 +13,16 @@ namespace crap
  template <class Type, template <Type...> class Operator> struct accumulateValue<Type, Operator>
 	 : std :: integral_constant<decltype(Operator <> :: value), Operator <> :: value> {};
 
- template <class Type, template <Type...> class Operator, Type Value> struct accumulateValue<Type, Operator, Value>
+ template <class Type, template <Type...> class Operator, Type Value>
+	 struct accumulateValue<Type, Operator, Value>
 	 : std :: integral_constant<decltype(Operator <Value> :: value), Operator <Value> :: value> {};
 
- template <class Type, template <Type...> class Operator, Type Value1, Type Value2> struct accumulateValue<Type, Operator, Value1, Value2>
+ template <class Type, template <Type...> class Operator, Type Value1, Type Value2>
+	 struct accumulateValue<Type, Operator, Value1, Value2>
 	 : std :: integral_constant<decltype(Operator <Value1, Value2> :: value), Operator <Value1, Value2> :: value> {};
 
- template <class Type, template <Type...> class Operator, Type Value1, Type Value2, Type Value3> struct accumulateValue<Type, Operator, Value1, Value2, Value3>
+ template <class Type, template <Type...> class Operator, Type Value1, Type Value2, Type Value3>
+	 struct accumulateValue<Type, Operator, Value1, Value2, Value3>
  {
   private:
   constexpr const static Type subResult = Operator <Value1, Value2> :: value;
@@ -26,6 +30,9 @@ namespace crap
   constexpr const static Type value = Operator <subResult, Value3> :: value;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
 
  template <class Type, template <Type...> class Operator, Type ... Values> struct accumulateValue
@@ -39,6 +46,9 @@ namespace crap
   constexpr const static Type value = values :: template upper <secondHalf> :: value;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
 }
 
@@ -47,12 +57,31 @@ template <class Type, template <Type...> class Operator, Type Value1, Type Value
 	typename crap :: accumulateValue <Type, Operator, Value1, Value2, Value3> :: value_type () const noexcept
 {
  return crap :: accumulateValue <Type, Operator, Value1, Value2, Value3> :: value;
-};
+}
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <class Type, template <Type...> class Operator, Type Value1, Type Value2, Type Value3>
+inline constexpr typename crap :: accumulateValue <Type, Operator, Value1, Value2, Value3> :: value_type
+crap :: accumulateValue <Type, Operator, Value1, Value2, Value3> :: operator () () const noexcept
+{
+ return crap :: accumulateValue <Type, Operator, Value1, Value2, Value3> :: value;
+}
+#endif
 
 template <class Type, template <Type...> class Operator, Type ... Values>
 	inline constexpr crap :: accumulateValue <Type, Operator, Values...> :: operator
 	typename crap :: accumulateValue <Type, Operator, Values...> :: value_type () const noexcept
 {
  return crap :: accumulateValue <Type, Operator, Values...> :: value;
-};
+}
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <class Type, template <Type...> class Operator, Type ... Values>
+inline constexpr typename crap :: accumulateValue <Type, Operator, Values...> :: value_type
+crap :: accumulateValue <Type, Operator, Values...> :: operator () () const noexcept
+{
+ return crap :: accumulateValue <Type, Operator, Values...> :: value;
+}
 #endif
+#endif
+
