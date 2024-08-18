61902e71c7eb5201bf2fe70a023e76d44fe20884
Igor Zarzycki <izarzycki@dmcs.pl>
2024-08-18 03:03:54 +0200

    Make minus function acting on values uniformly callable.

--- include/crap/functional.d/minusvalue.h
+++ include/crap/functional.d/minusvalue.h
@@ -1,65 +1,46 @@
 #ifndef CRAP_FUNCTIONAL_MINUSVALUE
 #define CRAP_FUNCTIONAL_MINUSVALUE
 
+#include <type_traits>
+
 #include "../version.d/foldexpressions.h"
 
 #if (crap_fold_expressions >= 201603L)
 #else
 #include "../numeric.d/accumulatevalue.h"
+#include "../version.d/libintegralconstantcallable.h"
 #endif
 
 namespace crap
 {
  template <class Type, Type...> struct minusValue;
 
-#if (crap_fold_expressions >= 201603L)
-#else
  template <class Type, Type Value> struct minusValue<Type, Value>
- {
-  constexpr const static auto value = Value;
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<Type, Value> {};
 
  template <class Type, Type Value1, Type Value2> struct minusValue<Type, Value1, Value2>
- {
-  constexpr const static auto value = (Value1 - Value2);
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
-#endif
+	 : std :: integral_constant<Type, (Value1 - Value2)> {};
 
+#if (crap_fold_expressions >= 201603L)
+ template <class Type, Type FirstValue, Type ... Rest> struct minusValue<Type, FirstValue, Rest...>
+	 : std :: integral_constant<Type, (FirstValue - ... - Rest)> {};
+#else
  template <class Type, Type FirstValue, Type ... Rest> struct minusValue<Type, FirstValue, Rest...>
  {
-#if (crap_fold_expressions >= 201603L)
-  constexpr const static auto value = (FirstValue - ... - Rest);
-#else
   private:
   template <Type ... SubValues> using This = minusValue<Type, SubValues...>;
   public:
   constexpr const static auto value = accumulateValue <Type, This, FirstValue, Rest...> :: value;
-#endif
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
+#endif
 }
-
 #if (crap_fold_expressions >= 201603L)
 #else
-template <class Type, Type Value>
-inline constexpr crap :: minusValue <Type, Value> :: operator
-typename crap :: minusValue <Type, Value> :: value_type () const noexcept
-{
- return crap :: minusValue <Type, Value> :: value;
-}
-
-template <class Type, Type Value1, Type Value2>
-inline constexpr crap :: minusValue <Type, Value1, Value2> :: operator
-typename crap :: minusValue <Type, Value1, Value2> :: value_type () const noexcept
-{
- return crap :: minusValue <Type, Value1, Value2> :: value;
-}
-#endif
 
 template <class Type, Type FirstValue, Type ... Rest>
 inline constexpr crap :: minusValue <Type, FirstValue, Rest...> :: operator
@@ -67,5 +48,15 @@ typename crap :: minusValue <Type, FirstValue, Rest...> :: value_type () const n
 {
  return crap :: minusValue <Type, FirstValue, Rest...> :: value;
 }
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <class Type, Type FirstValue, Type ... Rest>
+inline constexpr typename crap :: minusValue <Type, FirstValue, Rest...> :: value_type
+crap :: minusValue <Type, FirstValue, Rest...> :: operator () () const noexcept
+{
+ return crap :: minusValue <Type, FirstValue, Rest...> :: value;
+}
+#endif
+#endif
 #endif
 
