6f6081e05fd781569038e1112458acaffd7d2b46
Igor Zarzycki <izarzycki@dmcs.pl>
2024-08-18 02:08:38 +0200

    Make reduce function acting on values uniformly callable.

--- include/crap/numeric.d/reducevalue.h
+++ include/crap/numeric.d/reducevalue.h
@@ -1,33 +1,26 @@
 #ifndef CRAP_NUMERIC_REDUCEVALUE
 #define CRAP_NUMERIC_REDUCEVALUE
 
+#include <type_traits>
+
+#include "../version.d/libintegralconstantcallable.h"
 #include "../utility.d/bisectvalue.h"
 
 namespace crap
 {
  template <class Type, template <Type...> class, Type...> struct reduceValue;
 
- template <class Type, template <Type...> class Operator> struct reduceValue<Type, Operator>
- {
-  constexpr const static auto value = Operator <> :: value;
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+ template <class Type, template <Type...> class Operator>
+	 struct reduceValue<Type, Operator>
+	 : std :: integral_constant<decltype(Operator <> :: value), Operator <> :: value> {};
 
- template <class Type, template <Type...> class Operator, Type Value> struct reduceValue<Type, Operator, Value>
- {
-  constexpr const static auto value = Operator <Value> :: value;
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+ template <class Type, template <Type...> class Operator, Type Value>
+	 struct reduceValue<Type, Operator, Value>
+	 : std :: integral_constant<decltype(Operator <Value> :: value), Operator <Value> :: value> {};
 
  template <class Type, template <Type...> class Operator, Type Value1, Type Value2>
 	 struct reduceValue<Type, Operator, Value1, Value2>
- {
-  constexpr const static auto value = Operator <Value1, Value2> :: value;
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<decltype(Operator <Value1, Value2> :: value), Operator <Value1, Value2> :: value> {};
 
  template <class Type, template <Type...> class Operator, Type ... Values> struct reduceValue
  {
@@ -40,29 +33,11 @@ namespace crap
   constexpr const static auto value = Operator <lower, upper> :: value;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
 }
-
-template <class Type, template <Type...> class Operator>
-	inline constexpr crap :: reduceValue <Type, Operator> :: operator
-	typename crap :: reduceValue <Type, Operator> :: value_type () const noexcept
-{
- return crap :: reduceValue <Type, Operator> :: value;
-};
-
-template <class Type, template <Type...> class Operator, Type Value>
-	inline constexpr crap :: reduceValue <Type, Operator, Value> :: operator
-	typename crap :: reduceValue <Type, Operator, Value> :: value_type () const noexcept
-{
- return crap :: reduceValue <Type, Operator, Value> :: value;
-};
-
-template <class Type, template <Type...> class Operator, Type Value1, Type Value2>
-	inline constexpr crap :: reduceValue <Type, Operator, Value1, Value2> :: operator
-	typename crap :: reduceValue <Type, Operator, Value1, Value2> :: value_type () const noexcept
-{
- return crap :: reduceValue <Type, Operator, Value1, Value2> :: value;
-};
 
 template <class Type, template <Type...> class Operator, Type ... Values>
 	inline constexpr crap :: reduceValue <Type, Operator, Values...> :: operator
@@ -70,4 +45,14 @@ template <class Type, template <Type...> class Operator, Type ... Values>
 {
  return crap :: reduceValue <Type, Operator, Values...> :: value;
 };
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <class Type, template <Type...> class Operator, Type ... Values>
+inline constexpr typename crap :: reduceValue <Type, Operator, Values...> :: value_type
+crap :: reduceValue <Type, Operator, Values...> :: operator () () const noexcept
+{
+ return crap :: reduceValue <Type, Operator, Values...> :: value;
+}
 #endif
+#endif
+
