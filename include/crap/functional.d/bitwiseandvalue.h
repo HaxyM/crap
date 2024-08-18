6c2a6965d4fa103fe05b380cc6c9522d5f51c93e
Igor Zarzycki <izarzycki@dmcs.pl>
2024-08-18 02:30:34 +0200

    Make bitwiseAnd function acting on values uniformly callable.

--- include/crap/functional.d/bitwiseandvalue.h
+++ include/crap/functional.d/bitwiseandvalue.h
@@ -1,12 +1,15 @@
 #ifndef CRAP_FUNCTIONAL_BITWISEANDVALUE
 #define CRAP_FUNCTIONAL_BITWISEANDVALUE
 
+#include <type_traits>
+
 #include "../numbers.d/zero.h"
 #include "../version.d/foldexpressions.h"
 
 #if (crap_fold_expressions >= 201603L)
 #else
 #include "../numeric.d/reducevalue.h"
+#include "../version.d/libintegralconstantcallable.h"
 #endif
 
 namespace crap
@@ -14,67 +17,50 @@ namespace crap
  template <class Type, Type...> struct bitwiseAndValue;
 
  template <class Type> struct bitwiseAndValue<Type>
- {
-  constexpr const static auto value = ~(zero <Type> :: value ^ zero <Type> :: value);
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<Type, ~(zero <Type> :: value ^ zero <Type> :: value)> {};
 
  template <class Type, Type Value> struct bitwiseAndValue<Type, Value>
- {
-  constexpr const static auto value = Value;
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<Type, Value> {};
 
  template <class Type, Type Value1, Type Value2> struct bitwiseAndValue<Type, Value1, Value2>
- {
-  constexpr const static auto value = (Value1 & Value2);
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<Type, (Value1 & Value2)> {};
 
+#if (crap_fold_expressions >= 201603L)
+ template <class Type, Type ... Values> struct bitwiseAndValue
+	 : std :: integral_constant<Type, (Values & ...)> {};
+#else
  template <class Type, Type ... Values> struct bitwiseAndValue
  {
-#if (crap_fold_expressions >= 201603L)
-  constexpr const static auto value = (Values & ...);
-#else
   private:
   template <Type ... SubValues> using This = bitwiseAndValue<Type, SubValues...>;
   public:
   constexpr const static auto value = reduceValue <Type, This, Values...> :: value;
-#endif
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
+#endif
 }
 
-template <class Type>
-inline constexpr crap :: bitwiseAndValue <Type> :: operator
-typename crap :: bitwiseAndValue <Type> :: value_type () const noexcept
-{
- return crap :: bitwiseAndValue <Type> :: value;
-}
-
-template <class Type, Type Value>
-inline constexpr crap :: bitwiseAndValue <Type, Value> :: operator
-typename crap :: bitwiseAndValue <Type, Value> :: value_type () const noexcept
-{
- return crap :: bitwiseAndValue <Type, Value> :: value;
-}
-
-template <class Type, Type Value1, Type Value2>
-inline constexpr crap :: bitwiseAndValue <Type, Value1, Value2> :: operator
-typename crap :: bitwiseAndValue <Type, Value1, Value2> :: value_type () const noexcept
-{
- return crap :: bitwiseAndValue <Type, Value1, Value2> :: value;
-}
-
+#if (crap_fold_expressions >= 201603L)
+#else
 template <class Type, Type ... Values>
 inline constexpr crap :: bitwiseAndValue <Type, Values...> :: operator
 typename crap :: bitwiseAndValue <Type, Values...> :: value_type () const noexcept
 {
  return crap :: bitwiseAndValue <Type, Values...> :: value;
 }
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <class Type, Type ... Values>
+inline constexpr typename crap :: bitwiseAndValue <Type, Values...> :: value_type
+crap :: bitwiseAndValue <Type, Values...> :: operator () () const noexcept
+{
+ return crap :: bitwiseAndValue <Type, Values...> :: value;
+}
+#endif
+#endif
 #endif
 
