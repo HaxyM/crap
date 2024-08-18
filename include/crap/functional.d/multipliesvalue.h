25f13b5c21b900cd65c0a471a03c1fb41d5ea094
Igor Zarzycki <izarzycki@dmcs.pl>
2024-08-18 03:21:37 +0200

    Make multiplies function acting on values uniformly callable.

--- include/crap/functional.d/multipliesvalue.h
+++ include/crap/functional.d/multipliesvalue.h
@@ -1,12 +1,15 @@
 #ifndef CRAP_FUNCTIONAL_MULTIPLIESVALUE
 #define CRAP_FUNCTIONAL_MULTIPLIESVALUE
 
+#include <type_traits>
+
 #include "../numbers.d/identity.h"
 #include "../version.d/foldexpressions.h"
 
 #if (crap_fold_expressions >= 201603L)
 #else
 #include "../numeric.d/reducevalue.h"
+#include "../version.d/libintegralconstantcallable.h"
 #endif
 
 namespace crap
@@ -14,61 +17,34 @@ namespace crap
  template <class Type, Type...> struct multipliesValue;
 
  template <class Type> struct multipliesValue<Type>
- {
-  constexpr const static auto value = identity <Type> :: value;
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<Type, identity <Type> :: value> {};
 
  template <class Type, Type Value> struct multipliesValue<Type, Value>
- {
-  constexpr const static auto value = Value;
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<Type, Value> {};
 
  template <class Type, Type Value1, Type Value2> struct multipliesValue<Type, Value1, Value2>
- {
-  constexpr const static auto value = (Value1 * Value2);
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<Type, (Value1 * Value2)> {};
 
+#if (crap_fold_expressions >= 201603L)
+ template <class Type, Type ... Values> struct multipliesValue
+	 : std :: integral_constant<Type, (Values * ...)> {};
+#else
  template <class Type, Type ... Values> struct multipliesValue
  {
-#if (crap_fold_expressions >= 201603L)
-  constexpr const static auto value = (Values * ...);
-#else
   private:
   template <Type ... SubValues> using This = multipliesValue<Type, SubValues...>;
   public:
   constexpr const static auto value = reduceValue <Type, This, Values...> :: value;
-#endif
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
+#endif
 }
-
-template <class Type>
-inline constexpr crap :: multipliesValue <Type> :: operator
-typename crap :: multipliesValue <Type> :: value_type () const noexcept
-{
- return crap :: multipliesValue <Type> :: value;
-}
-
-template <class Type, Type Value>
-inline constexpr crap :: multipliesValue <Type, Value> :: operator
-typename crap :: multipliesValue <Type, Value> :: value_type () const noexcept
-{
- return crap :: multipliesValue <Type, Value> :: value;
-}
-
-template <class Type, Type Value1, Type Value2>
-inline constexpr crap :: multipliesValue <Type, Value1, Value2> :: operator
-typename crap :: multipliesValue <Type, Value1, Value2> :: value_type () const noexcept
-{
- return crap :: multipliesValue <Type, Value1, Value2> :: value;
-}
+#if (crap_fold_expressions >= 201603L)
+#else
 
 template <class Type, Type ... Values>
 inline constexpr crap :: multipliesValue <Type, Values...> :: operator
@@ -76,5 +52,15 @@ typename crap :: multipliesValue <Type, Values...> :: value_type () const noexce
 {
  return crap :: multipliesValue <Type, Values...> :: value;
 }
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <class Type, Type ... Values>
+inline constexpr typename crap :: multipliesValue <Type, Values...> :: value_type
+crap :: multipliesValue <Type, Values...> :: operator () () const noexcept
+{
+ return crap :: multipliesValue <Type, Values...> :: value;
+}
+#endif
+#endif
 #endif
 
