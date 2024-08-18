2f04d3ce97bd88eaa8a5b0e08944514d2f4f7aeb
Igor Zarzycki <izarzycki@dmcs.pl>
2024-08-18 02:47:53 +0200

    Make divides function acting on values uniformly callable.

--- include/crap/functional.d/dividesvalue.h
+++ include/crap/functional.d/dividesvalue.h
@@ -1,71 +1,62 @@
 #ifndef CRAP_FUNCTIONAL_DIVIDESVALUE
 #define CRAP_FUNCTIONAL_DIVIDESVALUE
 
+#include <type_traits>
+
 #include "../version.d/foldexpressions.h"
 
 #if (crap_fold_expressions >= 201603L)
 #else
 #include "../numeric.d/accumulatevalue.h"
+#include "../version.d/libintegralconstantcallable.h"
 #endif
 
 namespace crap
 {
  template <class Type, Type...> struct dividesValue;
 
-#if (crap_fold_expressions >= 201603L)
-#else
  template <class Type, Type Value> struct dividesValue<Type, Value>
- {
-  constexpr const static auto value = Value;
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<Type, Value> {};
 
  template <class Type, Type Value1, Type Value2> struct dividesValue<Type, Value1, Value2>
- {
-  constexpr const static auto value = (Value1 / Value2);
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
-#endif
+	 : std :: integral_constant<Type, (Value1 / Value2)> {};
 
+#if (crap_fold_expressions >= 201603L)
+ template <class Type, Type FirstValue, Type ... Rest> struct dividesValue<Type, FirstValue, Rest...>
+	 : std :: integral_constant<Type, (FirstValue / ... / Rest)> {};
+#else
  template <class Type, Type FirstValue, Type ... Rest> struct dividesValue<Type, FirstValue, Rest...>
  {
-#if (crap_fold_expressions >= 201603L)
-  constexpr const static auto value = (FirstValue / ... / Rest);
-#else
   private:
   template <Type ... SubValues> using This = dividesValue<Type, SubValues...>;
   public:
   constexpr const static auto value = accumulateValue <Type, This, FirstValue, Rest...> :: value;
-#endif
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
+#endif
 }
 
 #if (crap_fold_expressions >= 201603L)
 #else
-template <class Type, Type Value>
-inline constexpr crap :: dividesValue <Type, Value> :: operator
-typename crap :: dividesValue <Type, Value> :: value_type () const noexcept
-{
- return crap :: dividesValue <Type, Value> :: value;
-}
-
-template <class Type, Type Value1, Type Value2>
-inline constexpr crap :: dividesValue <Type, Value1, Value2> :: operator
-typename crap :: dividesValue <Type, Value1, Value2> :: value_type () const noexcept
-{
- return crap :: dividesValue <Type, Value1, Value2> :: value;
-}
-#endif
-
 template <class Type, Type FirstValue, Type ... Rest>
 inline constexpr crap :: dividesValue <Type, FirstValue, Rest...> :: operator
 typename crap :: dividesValue <Type, FirstValue, Rest...> :: value_type () const noexcept
 {
  return crap :: dividesValue <Type, FirstValue, Rest...> :: value;
 }
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <class Type, Type FirstValue, Type ... Rest>
+inline constexpr typename crap :: dividesValue <Type, FirstValue, Rest...> :: value_type
+crap :: dividesValue <Type, FirstValue, Rest...> :: operator () () const noexcept
+{
+ return crap :: dividesValue <Type, FirstValue, Rest...> :: value;
+}
+#endif
+#endif
 #endif
 
