330adf4d7def08a81f5fa65e31b52af5ace0c9b8
Igor Zarzycki <izarzycki@mailfence.com>
2024-08-20 14:30:46 +0200

    Make anyOf function acting on values uniformly callable.

--- include/crap/algorithm.d/anyofvalue.h
+++ include/crap/algorithm.d/anyofvalue.h
@@ -1,21 +1,18 @@
 #ifndef CRAP_ALGORITHM_ANYOFVALUE
 #define CRAP_ALGORITHM_ANYOFVALUE
 
+#include <type_traits>
+
 #include "../functional.d/logicalorvalue.h"
 #include "../utility.d/commontype.h"
-
-#include <type_traits>
+#include "../version.d/libintegralconstantcallable.h"
 
 namespace crap
 {
  template <class Type, template <Type> class, Type...> struct anyOfValue;
 
  template <class Type, template <Type> class Operator> struct anyOfValue<Type, Operator>
- {
-  constexpr const static bool value = false;
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<bool, false> {};
 
  template <class Type, template <Type> class Operator, Type ... Values> struct anyOfValue
  {
@@ -26,15 +23,11 @@ namespace crap
 	  logicalOrValue <resultType, static_cast<resultType>(Operator <Values> :: value)...> :: value;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
 }
-
-template <class Type, template <Type> class Operator>
-	inline constexpr crap :: anyOfValue <Type, Operator> :: operator
-	typename crap :: anyOfValue <Type, Operator> :: value_type () const noexcept
-{
- return crap :: anyOfValue <Type, Operator> :: value;
-};
 
 template <class Type, template <Type> class Operator, Type ... Values>
 	inline constexpr crap :: anyOfValue <Type, Operator, Values...> :: operator
@@ -42,5 +35,14 @@ template <class Type, template <Type> class Operator, Type ... Values>
 {
  return crap :: anyOfValue <Type, Operator, Values...> :: value;
 };
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <class Type, template <Type> class Operator, Type ... Values>
+	inline constexpr typename crap :: anyOfValue <Type, Operator, Values...> :: value_type
+	crap :: anyOfValue <Type, Operator, Values...> :: operator () () const noexcept
+{
+ return crap :: anyOfValue <Type, Operator, Values...> :: value;
+}
+#endif
 #endif
 
