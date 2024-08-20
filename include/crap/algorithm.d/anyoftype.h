b6d31eda47bb2fd139dcded90a004d431e3a133a
Igor Zarzycki <izarzycki@mailfence.com>
2024-08-20 15:18:09 +0200

    Make anyOf function acting on types uniformly callable.

--- include/crap/algorithm.d/anyoftype.h
+++ include/crap/algorithm.d/anyoftype.h
@@ -1,19 +1,18 @@
 #ifndef CRAP_ALGORITHM_ANYOFTYPE
 #define CRAP_ALGORITHM_ANYOFTYPE
 
+#include <type_traits>
+
 #include "../functional.d/logicalorvalue.h"
 #include "../utility.d/commontype.h"
+#include "../version.d/libintegralconstantcallable.h"
 
 namespace crap
 {
  template <template <class> class, class...> struct anyOfType;
 
  template <template <class> class Operator> struct anyOfType<Operator>
- {
-  constexpr const static bool value = false;
-  using value_type = decltype(value);
-  constexpr operator value_type () const noexcept;
- };
+	 : std :: integral_constant<bool, false> {};
 
  template <template <class> class Operator, class ... Types> struct anyOfType
  {
@@ -23,14 +22,10 @@ namespace crap
   constexpr const static auto value = logicalOrValue <resultType, static_cast<resultType>(Operator <Types> :: value)...> :: value;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
-}
-
-template <template <class> class Operator>
-        inline constexpr crap :: anyOfType <Operator> :: operator
-	typename crap :: anyOfType <Operator> :: value_type () const noexcept
-{
- return crap :: anyOfType <Operator> :: value;
 }
 
 template <template <class> class Operator, class ... Types>
@@ -39,5 +34,14 @@ template <template <class> class Operator, class ... Types>
 {
  return crap :: anyOfType <Operator, Types...> :: value;
 }
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <template <class> class Operator, class ... Types>
+	inline constexpr typename crap :: anyOfType <Operator, Types...> :: value_type
+	crap :: anyOfType <Operator, Types...> :: operator () () const noexcept
+{
+ return crap :: anyOfType <Operator, Types...> :: value;
+}
+#endif
 #endif
 
