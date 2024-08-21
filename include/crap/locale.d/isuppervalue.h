7abc2a9300bff4028873ae03b679e482b8aca000
Igor Zarzycki <izarzycki@mailfence.com>
2024-08-21 14:08:54 +0200

    Improve character comparisons.

--- include/crap/locale.d/isuppervalue.h
+++ include/crap/locale.d/isuppervalue.h
@@ -1,16 +1,20 @@
 #ifndef CRAP_LOCALE_ISUPPERVALUE
 #define CRAP_LOCALE_ISUPPERVALUE
 
-#include "locale.h"
-
+#include <cstdint>
 #include <type_traits>
+
+#include "locale.h"
+#include "../utility.d/cmpgreaterequalvalue.h"
+#include "../utility.d/cmplessequalvalue.h"
 
 namespace crap
 {
  template <class CharType, CharType, locale = locale :: C> struct isupperValue;
 
  template <class CharType, CharType Char> struct isupperValue<CharType, Char, locale :: C>
-	 : std :: integral_constant<bool, (Char >= static_cast<CharType>(65)) && (Char <= static_cast<CharType>(90))>
+	 : std :: integral_constant<bool, (cmpGreaterEqualValue <CharType, Char, std :: uint32_t, UINT32_C(65)> :: value)
+	 && (cmpLessEqualValue <CharType, Char, std :: uint32_t, UINT32_C(90)> :: value)>
 	 {};
 }
 #endif
