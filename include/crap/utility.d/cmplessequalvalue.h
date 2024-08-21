75a69b3f54b278829c2e9febb098920c5b6b38a2
Igor Zarzycki <izarzycki@mailfence.com>
2024-08-22 00:27:46 +0200

    Fix typo.

--- include/crap/utility.d/cmplessequalvalue.h
+++ include/crap/utility.d/cmplessequalvalue.h
@@ -20,7 +20,7 @@ namespace crap
 	 using cmpLessEqualValue = std :: integral_constant<bool, std :: cmp_less_equal(Value1, Value2)>;
 #else
  template <class Type1, Type1 Value1, class Type2, Type2 Value2>
-	 using cmpLessEqialValue = std :: integral_constant<bool,
+	 using cmpLessEqualValue = std :: integral_constant<bool,
 	       !(cmpLessValue <Type2, Value2, Type1, Value1> :: value)>;
 #endif
 }
