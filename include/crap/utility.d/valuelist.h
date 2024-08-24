37ada0e314c1734fc0a623b60ea01c0ab4ac9009
Igor Zarzycki <izarzycki@dmcs.pl>
2024-08-24 13:47:48 +0200

    Add empty valueLict support.

--- include/crap/utility.d/valuelist.h
+++ include/crap/utility.d/valuelist.h
@@ -1,12 +1,41 @@
 #ifndef CRAP_UTILITY_VALUELIST
 #define CRAP_UTILITY_VALUELIST
 
+#include <cstddef>
 #include <utility>
 
 #include "../version.d/variabletemplates.h"
 
 namespace crap
 {
+ template <class Type, Type...> struct valueList;
+
+ template <class Type> struct valueList<Type>
+ {
+  private:
+  template <Type ... SubValues> using This = valueList<Type, SubValues...>;
+  public:
+  constexpr const static std :: size_t size = 0u;
+  constexpr const static std :: size_t begin = 0u;
+  constexpr const static std :: size_t end = 0u;
+  template <std :: size_t N> struct At;
+  template <std :: size_t N> struct Till;
+  template <std :: size_t N> struct Since;
+  template <std :: size_t Begin, std :: size_t End> struct SubRange;
+
+#if (crap_variable_templates >= 201304L)
+  template <std :: size_t N> constexpr const static Type at = At <N> :: value;
+#endif
+  constexpr static std :: nullptr_t data() noexcept;
+  template <template <Type...> class Container = This> using copy = Container<>;
+  template <std :: size_t N, template <Type...> class Container = This>
+	  using till = typename Till <N> :: template type<Container>;
+  template <std :: size_t N, template <Type...> class Container = This>
+	  using since = typename Since <N> :: template type<Container>;
+  template <std :: size_t Begin, std :: size_t End, template <Type...> class Container = This>
+	  using subRange = typename SubRange <Begin, End> :: template type<Container>;
+ };
+
  template <class Type, Type ... Values> struct valueList
  {
   private:
@@ -41,15 +70,30 @@ namespace crap
   template <std :: size_t Begin, std :: size_t End, template <Type...> class Container = This>
 	  using subRange = typename SubRange <Begin, End> :: template type<Container>;
  };
-}
 
-template <class Type, Type ... Values>
-constexpr const Type crap :: valueList <Type, Values...> :: values[sizeof...(Values)];
+ template <class Type> template <std :: size_t N> struct valueList <Type> :: At
+ {
+  static_assert(N < 0u, "Index out of range.");
+ };
 
-template <class Type, Type ... Values>
-inline constexpr const Type (& crap :: valueList <Type, Values...> :: data() noexcept)[sizeof...(Values)]
-{
- return crap :: valueList <Type, Values...> :: values;
+ template <class Type> template <std :: size_t N> struct valueList <Type> :: Till
+ {
+  static_assert(N <= 0u, "Index out of range.");
+  template <template <Type... > class Container = This> using type = Container<>;
+ };
+
+ template <class Type> template <std :: size_t N> struct valueList <Type> :: Since
+ {
+  static_assert(N <= 0u, "Index out of range.");
+  template <template <Type... > class Container = This> using type = Container<>;
+ };
+
+ template <class Type> template <std :: size_t Begin, std :: size_t End> struct valueList <Type> :: SubRange
+ {
+  static_assert(End <= 0u, "Index out of range.");
+  static_assert(Begin <= End, "Negative range.");
+  template <template <Type... > class Container = This> using type = Container<>;
+ };
 }
 
 #include "makeindexsequence.h"
@@ -109,6 +153,21 @@ namespace crap
   public:
   template <template <Type...> class Container = This> using type = typename generator :: template type<Container>;
  };
+}
+
+template <class Type>
+inline constexpr std :: nullptr_t crap :: valueList <Type> :: data() noexcept
+{
+ return nullptr;
+}
+
+template <class Type, Type ... Values>
+constexpr const Type crap :: valueList <Type, Values...> :: values[sizeof...(Values)];
+
+template <class Type, Type ... Values>
+inline constexpr const Type (& crap :: valueList <Type, Values...> :: data() noexcept)[sizeof...(Values)]
+{
+ return crap :: valueList <Type, Values...> :: values;
 }
 #endif
 
