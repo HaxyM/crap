55b484cea7f3eac6d4aeb646424fe18f3d016e45
Igor Zarzycki <izarzycki@mailfence.com>
2024-09-03 14:33:47 +0200

    Make find function acting on types uniformly callable.

--- include/crap/algorithm.d/findtype.h
+++ include/crap/algorithm.d/findtype.h
@@ -1,9 +1,10 @@
 #ifndef CRAP_ALGORITHM_FINDTYPE
 #define CRAP_ALGORITHM_FINDTYPE
 
-#include "findiftype.h"
-
 #include <cstddef>
+
+#include "findiftype.h"
+#include "../version.d/libintegralconstantcallable.h"
 
 namespace crap
 {
@@ -17,6 +18,9 @@ namespace crap
   constexpr const static std :: size_t npos = findIfType <condition, Types...> :: npos;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
 }
 
@@ -26,5 +30,14 @@ template <class Type, template <class, class> class Operator, class ... Types>
 {
  return crap :: findType <Type, Operator, Types...> :: value;
 }
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <class Type, template <class, class> class Operator, class ... Types>
+        inline constexpr typename crap :: findType <Type, Operator, Types...> :: value_type
+        crap :: findType <Type, Operator, Types...> :: operator () () const noexcept
+{
+ return crap :: findType <Type, Operator, Types...> :: value;
+}
+#endif
 #endif
 
