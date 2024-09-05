745cd708abc67adba1a653ec4c04b84a271c8226
Igor Zarzycki <izarzycki@mailfence.com>
2024-09-05 15:50:05 +0200

    Add countlZero function acting on values type control.

--- include/crap/bit.d/countlzerovalue.h
+++ include/crap/bit.d/countlzerovalue.h
@@ -9,8 +9,11 @@
 #if (crap_lib_bitops >= 201907L)
 #include <bit>
 #else
-#include "bitwidthvalue.h"
 #include <climits>
+
+#include "bitwidthvalue.h"
+#include "../version.d/char8t.h"
+#include "../version.d/unicodecharacters.h"
 #endif
 
 namespace crap
@@ -21,6 +24,18 @@ namespace crap
 #else
  template <class Type, Type Value> struct countlZeroValue
  {
+  static_assert((std :: is_integral <Type> :: value) && (std :: is_unsigned <Type> :: value) &&
+		  !(std :: is_same <typename std :: remove_cv <Type> :: type, bool> :: value) &&
+		  !(std :: is_same <typename std :: remove_cv <Type> :: type, char> :: value) &&
+#if (crap_char8_t >= 201811L)
+		  !(std :: is_same <typename std :: remove_cv <Type> :: type, char8_t> :: value) &&
+#endif
+#if (crap_unicode_characters >= 200704L)
+		  !(std :: is_same <typename std :: remove_cv <Type> :: type, char16_t> :: value) &&
+		  !(std :: is_same <typename std :: remove_cv <Type> :: type, char32_t> :: value) &&
+#endif
+		  !(std :: is_same <typename std :: remove_cv <Type> :: type, wchar_t> :: value),
+		  "Unsupported type.");
   private:
   constexpr const static std :: size_t bits = CHAR_BIT * sizeof(Type);
   constexpr const static std :: size_t width = bitWidthValue <Type, Value> :: value;
