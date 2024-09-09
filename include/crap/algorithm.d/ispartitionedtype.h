89eeafe43f418e0eafb101b388e601260488dad4
Igor Zarzycki <izarzycki@mailfence.com>
2024-09-09 21:00:19 +0200

    Make isPartitioned function acting on types uuniformly callable.

--- include/crap/algorithm.d/ispartitionedtype.h
+++ include/crap/algorithm.d/ispartitionedtype.h
@@ -4,23 +4,32 @@
 #include "noneoftype.h"
 #include "partitionpointtype.h"
 #include "../utility.d/typelist.h"
+#include "../version.d/libintegralconstantcallable.h"
 
 namespace crap
 {
  template <template <class> class, class...> struct isPartitionedType;
 
- template <template <class> class Operator> struct isPartitionedType<Operator>
+ template <template <class> class Operator>
+	 struct isPartitionedType<Operator>
  {
   constexpr const static bool value = true;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
 
- template <template <class> class Operator, class Type> struct isPartitionedType<Operator, Type>
+ template <template <class> class Operator, class Type>
+	 struct isPartitionedType<Operator, Type>
  {
   constexpr const static bool value = true;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
 
  template <template <class> class Operator, class Type1, class Type2>
@@ -29,6 +38,9 @@ namespace crap
   constexpr const static bool value = Operator <Type1> :: value;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
 
  template <template <class> class Operator, class ... Types> struct isPartitionedType
@@ -38,9 +50,13 @@ namespace crap
   template <std :: size_t Value, std :: size_t Npos> struct checkTail;
   template <std :: size_t Npos> struct checkTail<Npos, Npos>;
   public:
-  constexpr const static bool value = checkTail <partitionPoint :: value, partitionPoint :: npos> :: value;
+  constexpr const static bool value =
+	  checkTail <partitionPoint :: value, partitionPoint :: npos> :: value;
   using value_type = decltype(value);
   constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
  };
 
  template <template <class> class Operator, class ... Types>
@@ -50,11 +66,12 @@ namespace crap
   private:
   template <class ... SubTypes> using noneOf = noneOfType<Operator, SubTypes...>;
   public:
-  constexpr const static bool value = typeList <Types...> :: template since<Value + 1u, noneOf> :: value;
+  constexpr const static bool value =
+	  typeList <Types...> :: template since <Value + 1u, noneOf> :: value;
  };
 
  template <template <class> class Operator, class ... Types> template <std :: size_t Npos>
-	 struct isPartitionedType <Operator, Types...> :: template checkTail<Npos, Npos>
+	 struct isPartitionedType <Operator, Types...> :: checkTail<Npos, Npos>
  {
   constexpr const static bool value = true;
  };
@@ -66,6 +83,15 @@ template <template <class> class Operator>
 {
  return crap :: isPartitionedType <Operator> :: value;
 }
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <template <class> class Operator>
+        inline constexpr typename crap :: isPartitionedType <Operator> :: value_type
+        crap :: isPartitionedType <Operator> :: operator () () const noexcept
+{
+ return crap :: isPartitionedType <Operator> :: value;
+}
+#endif
 
 template <template <class> class Operator, class Type>
         inline constexpr crap :: isPartitionedType <Operator, Type> :: operator
@@ -73,6 +99,15 @@ template <template <class> class Operator, class Type>
 {
  return crap :: isPartitionedType <Operator, Type> :: value;
 }
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <template <class> class Operator, class Type>
+        inline constexpr typename crap :: isPartitionedType <Operator, Type> :: value_type
+        crap :: isPartitionedType <Operator, Type> :: operator () () const noexcept
+{
+ return crap :: isPartitionedType <Operator, Type> :: value;
+}
+#endif
 
 template <template <class> class Operator, class Type1, class Type2>
         inline constexpr crap :: isPartitionedType <Operator, Type1, Type2> :: operator
@@ -80,6 +115,15 @@ template <template <class> class Operator, class Type1, class Type2>
 {
  return crap :: isPartitionedType <Operator, Type1, Type2> :: value;
 }
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <template <class> class Operator, class Type1, class Type2>
+        inline constexpr typename crap :: isPartitionedType <Operator, Type1, Type2> :: value_type
+        crap :: isPartitionedType <Operator, Type1, Type2> :: operator () () const noexcept
+{
+ return crap :: isPartitionedType <Operator, Type1, Type2> :: value;
+}
+#endif
 
 template <template <class> class Operator, class ... Types>
         inline constexpr crap :: isPartitionedType <Operator, Types...> :: operator
@@ -87,5 +131,14 @@ template <template <class> class Operator, class ... Types>
 {
  return crap :: isPartitionedType <Operator, Types...> :: value;
 }
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <template <class> class Operator, class ... Types>
+        inline constexpr typename crap :: isPartitionedType <Operator, Types...> :: value_type
+        crap :: isPartitionedType <Operator, Types...> :: operator () () const noexcept
+{
+ return crap :: isPartitionedType <Operator, Types...> :: value;
+}
+#endif
 #endif
 
