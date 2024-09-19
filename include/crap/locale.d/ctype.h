197a3418ef6e6f08312b2697122baaceeb8d9317
Igor Zarzycki <izarzycki@mailfence.com>
2024-09-19 14:56:07 +0200

    Add xdigit verification to is function in ctype.

--- include/crap/locale.d/ctype.h
+++ include/crap/locale.d/ctype.h
@@ -20,17 +20,46 @@ namespace crap
   template <char_type ... Chars> using string_type =  string<char_type, Chars...>;
   template <class> struct is;
   template <char Char>
+	  struct is<std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> >;
+  template <char Char>
+	  struct is<std :: pair<ctypeBase :: blank>, std :: integral_constant<char, Char> >;
+  template <char Char>
 	  struct is<std :: pair<std :: integral_constant<char, Char>, ctypeBase :: digit> >;
   template <char Char>
 	  struct is<std :: pair<ctypeBase :: digit>, std :: integral_constant<char, Char> >;
   template <char Char>
-	  struct is<std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> >;
+	  struct is<std :: pair<std :: integral_constant<char, Char>, ctypeBase :: xdigit> >;
   template <char Char>
-	  struct is<std :: pair<ctypeBase :: blank>, std :: integral_constant<char, Char> >;
+	  struct is<std :: pair<ctypeBase :: xdigit>, std :: integral_constant<char, Char> >;
  };
 
  template <> struct ctype <wchar_t>
  { //Requires execution encoding.
+ };
+
+ template <char Char>
+ struct ctype <char> ::
+	is<std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> >
+ { //Assume ASCII, UTF-8 single octet or pure basic character set.
+  private:
+  //If certain of ASCII, ASCII superset or any encoding where digits are ordered, use binary search.
+  using result = findValue<char, Char, comparatorsForType <char> :: template
+	 EqualTo, '\t', ' '>;
+  public:
+  constexpr const static bool value = (result :: value != result :: npos);
+  using value_type = decltype(value);
+  constexpr operator value_type () const noexcept;
+#if (crap_lib_integral_constant_callable >= 201304L)
+  constexpr value_type operator () () const noexcept;
+#endif
+ };
+
+ template <char Char>
+ struct ctype <char> ::
+	is<std :: pair<ctypeBase :: blank, std :: integral_constant<char, Char> > >
+ : ctype <char> ::
+	is<std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> >
+ {
  };
 
  template <char Char>
@@ -60,12 +89,13 @@ namespace crap
 
  template <char Char>
  struct ctype <char> ::
-	is<std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> >
+	is<std :: pair<std :: integral_constant<char, Char>, ctypeBase :: xdigit> >
  { //Assume ASCII, UTF-8 single octet or pure basic character set.
   private:
   //If certain of ASCII, ASCII superset or any encoding where digits are ordered, use binary search.
   using result = findValue<char, Char, comparatorsForType <char> :: template
-	 EqualTo, '\t', ' '>;
+	 EqualTo, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+	 'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'>;
   public:
   constexpr const static bool value = (result :: value != result :: npos);
   using value_type = decltype(value);
@@ -77,12 +107,38 @@ namespace crap
 
  template <char Char>
  struct ctype <char> ::
-	is<std :: pair<ctypeBase :: blank, std :: integral_constant<char, Char> > >
+	is<std :: pair<ctypeBase :: xdigit, std :: integral_constant<char, Char> > >
  : ctype <char> ::
-	is<std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> >
+	is<std :: pair<std :: integral_constant<char, Char>, ctypeBase :: xdigit> >
  {
  };
 }
+
+template <char Char>
+inline constexpr crap :: ctype <char> ::
+        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+operator typename crap :: ctype <char> ::
+        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+value_type () const noexcept
+{
+ return crap :: ctype <char> ::
+	 is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+	 value;
+}
+#if (crap_lib_integral_constant_callable >= 201304L)
+
+template <char Char>
+inline constexpr crap :: ctype <char> ::
+        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+value_type crap :: ctype <char> ::
+        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+operator () () const noexcept
+{
+ return crap :: ctype <char> ::
+	 is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+	 value;
+}
+#endif
 
 template <char Char>
 inline constexpr crap :: ctype <char> ::
@@ -112,26 +168,26 @@ operator () () const noexcept
 
 template <char Char>
 inline constexpr crap :: ctype <char> ::
-        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: xdigit> > ::
 operator typename crap :: ctype <char> ::
-        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: xdigit> > ::
 value_type () const noexcept
 {
  return crap :: ctype <char> ::
-	 is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+	 is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: xdigit> > ::
 	 value;
 }
 #if (crap_lib_integral_constant_callable >= 201304L)
 
 template <char Char>
 inline constexpr crap :: ctype <char> ::
-        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: xdigit> > ::
 value_type crap :: ctype <char> ::
-        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+        is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: xdigit> > ::
 operator () () const noexcept
 {
  return crap :: ctype <char> ::
-	 is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: blank> > ::
+	 is <std :: pair<std :: integral_constant<char, Char>, ctypeBase :: xdigit> > ::
 	 value;
 }
 #endif
