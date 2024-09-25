f0921a2595f804d94499a19332df11355caa1dda
Igor Zarzycki <izarzycki@mailfence.com>
2024-09-25 20:59:29 +0200

    Add arabic text encoding.

--- include/crap/text_encoding.d/textencodingtype.h
+++ include/crap/text_encoding.d/textencodingtype.h
@@ -315,6 +315,9 @@ namespace crap
  using text_encoding_id_ISOLatinCyrillic_t =
 	  std :: integral_constant<textEncodingType <void> :: id, textEncodingType <void> :: id :: ISOLatinCyrillic>;
 
+ using text_encoding_id_ISOLatinArabic_t =
+	  std :: integral_constant<textEncodingType <void> :: id, textEncodingType <void> :: id :: ISOLatinArabic>;
+
  template <>
  struct textEncodingType<text_encoding_id_ASCII_t> : textEncodingType<void>
  {
@@ -405,6 +408,21 @@ namespace crap
  };
 
  template <>
+ struct textEncodingType<text_encoding_id_ISOLatinArabic_t> : textEncodingType<void>
+ {
+  using mib = text_encoding_id_ISOLatinArabic_t;
+  using name = string<char, 'I', 'S', 'O', '-', '8', '8', '5', '9', '-', '6'>; //Libstdc++ uses name ISO-8859-6:1987
+  using aliases = typeList<
+	  string<char 'i', 's', 'o', '-', 'i', 'r', '-', '1', '2', '7'>,
+	  string<char, 'I', 'S', 'O', '_', '8', '8', '5', '9', '-', '6'>,
+	  string<char, 'I', 'S', 'O', '-', '8', '8', '5', '9', '-', '6'>,
+	  string<char, 'E', 'C', 'M', 'A', '-', '1', '1', '4'>,
+	  string<char, 'A', 'S', 'M', 'O', '-', '7', '0', '8'>,
+	  string<char, 'a', 'r', 'a', 'b', 'i', 'c'>,
+	  string<char 'c', 's', 'I', 'S', 'O', 'L', 'a', 't', 'i', 'n', 'A', 'r', 'a', 'b', 'i', 'c'> >;
+ };
+
+ template <>
  struct textEncodingType<string<char 'A', 'N', 'S', 'I', '_', 'X', '3', '.', '4', '-', '1', '9', '6', '8'> >
  : textEncodingType<text_encoding_id_ASCII_t>
  {
@@ -413,6 +431,18 @@ namespace crap
  template <>
  struct textEncodingType<string<char 'A', 'N', 'S', 'I', '_', 'X', '3', '.', '4', '-', '1', '9', '8', '6'> >
  : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+
+ template <>
+ struct textEncodingType<string<char 'a', 'r', 'a', 'b', 'i', 'c'> >
+ : textEncodingType<text_encoding_id_ISOLatinArabic_t>
+ {
+ };
+
+ template <>
+ struct textEncodingType<string<char 'A', 'S', 'M', 'O', '-', '7', '0', '8'> >
+ : textEncodingType<text_encoding_id_ISOLatinArabic_t>
  {
  };
 
@@ -459,6 +489,12 @@ namespace crap
  };
 
  template <>
+ struct textEncodingType<string<char 'c', 's', 'I', 'S', 'O', 'L', 'a', 't', 'i', 'n', 'A', 'r', 'a', 'b', 'i', 'c'> >
+ : textEncodingType<text_encoding_id_ISOLatinArabic_t>
+ {
+ };
+
+ template <>
  struct textEncodingType<string<char 'c', 's', 'I', 'S', 'O', 'L', 'a', 't', 'i', 'n', 'C', 'y', 'r', 'i', 'l', 'l', 'i', 'c'> >
  : textEncodingType<text_encoding_id_ISOLatinCyrillic_t>
  {
@@ -467,6 +503,12 @@ namespace crap
  template <>
  struct textEncodingType<string<char 'c', 'y', 'r', 'i', 'l', 'l', 'i', 'c'> >
  : textEncodingType<text_encoding_id_ISOLatinCyrillic_t>
+ {
+ };
+
+ template <>
+ struct textEncodingType<string<char 'E', 'C', 'M', 'A', '-', '1', '1', '4'> >
+ : textEncodingType<text_encoding_id_ISOLatinArabic_t>
  {
  };
 
@@ -525,6 +567,12 @@ namespace crap
  };
 
  template <>
+ struct textEncodingType<string<char 'I', 'S', 'O', '_', '8', '8', '5', '9', '-', '6'> >
+ : textEncodingType<text_encoding_id_ISOLatinArabic_t>
+ {
+ };
+
+ template <>
  struct textEncodingType<string<char 'I', 'S', 'O', '-', '8', '8', '5', '9', '-', '1'> >
  : textEncodingType<text_encoding_id_ISOLatin1_t>
  {
@@ -555,6 +603,12 @@ namespace crap
  };
 
  template <>
+ struct textEncodingType<string<char 'I', 'S', 'O', '-', '8', '8', '5', '9', '-', '6'> >
+ : textEncodingType<text_encoding_id_ISOLatinArabic_t>
+ {
+ };
+
+ template <>
  struct textEncodingType<string<char 'i', 's', 'o', '-', 'i', 'r', '-', '1', '0', '0'> >
  : textEncodingType<text_encoding_id_ISOLatin1_t>
  {
@@ -575,6 +629,12 @@ namespace crap
  template <>
  struct textEncodingType<string<char 'i', 's', 'o', '-', 'i', 'r', '-', '1', '1', '0'> >
  : textEncodingType<text_encoding_id_ISOLatin4_t>
+ {
+ };
+
+ template <>
+ struct textEncodingType<string<char 'i', 's', 'o', '-', 'i', 'r', '-', '1', '2', '7'> >
+ : textEncodingType<text_encoding_id_ISOLatinArabic_t>
  {
  };
 
