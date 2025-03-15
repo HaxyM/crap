d5c7d35a590941d554a7386c039df8778104a1b7
Igor Zarzycki <izarzycki@dmcs.pl>
2025-03-15 18:58:40 +0100

    Move ASCII text encoding to dedicated location.

---
+++ include/crap/text_encoding.d/IANA_encodings.d/ASCII.h
@@ -0,0 +1,90 @@
+#include <type_traits>
+#include "../textencodingtype.h"
+
+#ifndef CRAP_TEXTENCODING_IANAENCODINGS_ASCII
+#define CRAP_TEXTENCODING_IANAENCODINGS_ASCII
+
+namespace crap
+{
+ using text_encoding_id_ASCII_t =
+    std :: integral_constant<textEncodingType <void> :: id, textEncodingType <void> :: id :: ASCII>;
+
+ template <>
+ struct textEncodingType<text_encoding_id_ASCII_t> : textEncodingType<void>
+ {
+  using mib = text_encoding_id_ASCII_t;
+  using name = string<char, 'U', 'S', '-', 'A', 'S', 'C', 'I', 'I'>;
+  using aliases = typeList<
+	  string<char 'i', 's', 'o', '-', 'i', 'r', '-', '6'>,
+	  string<char 'A', 'N', 'S', 'I', '_', 'X', '3', '.', '4', '-', '1', '9', '6', '8'>,
+	  string<char 'A', 'N', 'S', 'I', '_', 'X', '3', '.', '4', '-', '1', '9', '8', '6'>,
+	  string<char 'I', 'S', 'O', '_', '6', '4', '6', '.', 'i', 'r', 'v', ':', '1', '9', '9', '1'>,
+	  string<char 'I', 'S', 'O', '6', '4', '6', '-', 'U', 'S'>,
+	  string<char 'U', 'S', '-', 'A', 'S', 'C', 'I', 'I'>,
+	  string<char 'u', 's'>,
+	  string<char 'I', 'B', 'M', '3', '6', '7'>,
+	  string<char 'c', 'p', '3', '6', '7'>,
+	  string<char 'c', 's', 'A', 'S', 'C', 'I', 'I'> >; //Libstdc++ adds alias "ASCII"
+ };
+ 
+ template <>
+ struct textEncodingType<string<char 'A', 'N', 'S', 'I', '_', 'X', '3', '.', '4', '-', '1', '9', '6', '8'> >
+ : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+
+ template <>
+ struct textEncodingType<string<char 'A', 'N', 'S', 'I', '_', 'X', '3', '.', '4', '-', '1', '9', '8', '6'> >
+ : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+ 
+ template <>
+ struct textEncodingType<string<char 'c', 'p', '3', '6', '7'> >
+ : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+ 
+ template <>
+ struct textEncodingType<string<char 'c', 's', 'A', 'S', 'C', 'I', 'I'> >
+ : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+ 
+ template <>
+ struct textEncodingType<string<char 'I', 'B', 'M', '3', '6', '7'> >
+ : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+ 
+ template <>
+ struct textEncodingType<string<char 'I', 'S', 'O', '6', '4', '6', '-', 'U', 'S'>, >
+ : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+
+ template <>
+ struct textEncodingType<string<char 'I', 'S', 'O', '_', '6', '4', '6', '.', 'i', 'r', 'v', ':', '1', '9', '9', '1'> >
+ : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+ 
+ template <>
+ struct textEncodingType<string<char 'i', 's', 'o', '-', 'i', 'r', '-', '6'> >
+ : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+ 
+ template <>
+ struct textEncodingType<string<char 'u', 's'> >
+ : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+
+ template <>
+ struct textEncodingType<string<char 'U', 'S', '-', 'A', 'S', 'C', 'I', 'I'> >
+ : textEncodingType<text_encoding_id_ASCII_t>
+ {
+ };
+}
+#endif
--- include/crap/text_encoding.d/textencodingtype.h
+++ include/crap/text_encoding.d/textencodingtype.h
@@ -297,9 +297,6 @@ namespace crap
  };
 #endif
 
- using text_encoding_id_ASCII_t =
-	  std :: integral_constant<textEncodingType <void> :: id, textEncodingType <void> :: id :: ASCII>;
-
  using text_encoding_id_ISOLatin1_t =
 	  std :: integral_constant<textEncodingType <void> :: id, textEncodingType <void> :: id :: ISOLatin1>;
 
@@ -320,24 +317,6 @@ namespace crap
 
  using text_encoding_id_ISOLatinGreek_t =
 	  std :: integral_constant<textEncodingType <void> :: id, textEncodingType <void> :: id :: ISOLatinGreek>;
-
- template <>
- struct textEncodingType<text_encoding_id_ASCII_t> : textEncodingType<void>
- {
-  using mib = text_encoding_id_ASCII_t;
-  using name = string<char, 'U', 'S', '-', 'A', 'S', 'C', 'I', 'I'>;
-  using aliases = typeList<
-	  string<char 'i', 's', 'o', '-', 'i', 'r', '-', '6'>,
-	  string<char 'A', 'N', 'S', 'I', '_', 'X', '3', '.', '4', '-', '1', '9', '6', '8'>,
-	  string<char 'A', 'N', 'S', 'I', '_', 'X', '3', '.', '4', '-', '1', '9', '8', '6'>,
-	  string<char 'I', 'S', 'O', '_', '6', '4', '6', '.', 'i', 'r', 'v', ':', '1', '9', '9', '1'>,
-	  string<char 'I', 'S', 'O', '6', '4', '6', '-', 'U', 'S'>,
-	  string<char 'U', 'S', '-', 'A', 'S', 'C', 'I', 'I'>,
-	  string<char 'u', 's'>,
-	  string<char 'I', 'B', 'M', '3', '6', '7'>,
-	  string<char 'c', 'p', '3', '6', '7'>,
-	  string<char 'c', 's', 'A', 'S', 'C', 'I', 'I'> >; //Libstdc++ adds alias "ASCII"
- };
 
  template <>
  struct textEncodingType<text_encoding_id_ISOLatin1_t> : textEncodingType<void>
@@ -442,18 +421,6 @@ namespace crap
  };
 
  template <>
- struct textEncodingType<string<char 'A', 'N', 'S', 'I', '_', 'X', '3', '.', '4', '-', '1', '9', '6', '8'> >
- : textEncodingType<text_encoding_id_ASCII_t>
- {
- };
-
- template <>
- struct textEncodingType<string<char 'A', 'N', 'S', 'I', '_', 'X', '3', '.', '4', '-', '1', '9', '8', '6'> >
- : textEncodingType<text_encoding_id_ASCII_t>
- {
- };
-
- template <>
  struct textEncodingType<string<char 'a', 'r', 'a', 'b', 'i', 'c'> >
  : textEncodingType<text_encoding_id_ISOLatinArabic_t>
  {
@@ -466,20 +433,8 @@ namespace crap
  };
 
  template <>
- struct textEncodingType<string<char 'c', 'p', '3', '6', '7'> >
- : textEncodingType<text_encoding_id_ASCII_t>
- {
- };
-
- template <>
  struct textEncodingType<string<char 'C', 'P', '8', '1', '9'> >
  : textEncodingType<text_encoding_id_ISOLatin1_t>
- {
- };
-
- template <>
- struct textEncodingType<string<char 'c', 's', 'A', 'S', 'C', 'I', 'I'> >
- : textEncodingType<text_encoding_id_ASCII_t>
  {
  };
 
@@ -562,26 +517,8 @@ namespace crap
  };
 
  template <>
- struct textEncodingType<string<char 'I', 'B', 'M', '3', '6', '7'> >
- : textEncodingType<text_encoding_id_ASCII_t>
- {
- };
-
- template <>
  struct textEncodingType<string<char 'I', 'B', 'M', '8', '1', '9'> >
  : textEncodingType<text_encoding_id_ISOLatin1_t>
- {
- };
-
- template <>
- struct textEncodingType<string<char 'I', 'S', 'O', '6', '4', '6', '-', 'U', 'S'>, >
- : textEncodingType<text_encoding_id_ASCII_t>
- {
- };
-
- template <>
- struct textEncodingType<string<char 'I', 'S', 'O', '_', '6', '4', '6', '.', 'i', 'r', 'v', ':', '1', '9', '9', '1'> >
- : textEncodingType<text_encoding_id_ASCII_t>
  {
  };
 
@@ -754,12 +691,6 @@ namespace crap
  };
 
  template <>
- struct textEncodingType<string<char 'i', 's', 'o', '-', 'i', 'r', '-', '6'> >
- : textEncodingType<text_encoding_id_ASCII_t>
- {
- };
-
- template <>
  struct textEncodingType<string<char 'l', '1'> >
  : textEncodingType<text_encoding_id_ISOLatin1_t>
  {
@@ -806,19 +737,12 @@ namespace crap
  : textEncodingType<text_encoding_id_ISOLatin4_t>
  {
  };
+}
 
- template <>
- struct textEncodingType<string<char 'u', 's'> >
- : textEncodingType<text_encoding_id_ASCII_t>
- {
- };
+ #include "IANA_encodings.d/ASCII.h"
 
- template <>
- struct textEncodingType<string<char 'U', 'S', '-', 'A', 'S', 'C', 'I', 'I'> >
- : textEncodingType<text_encoding_id_ASCII_t>
- {
- };
-
+namespace crap
+{
  template <class ... Ignored>
  struct textEncodingType <void> :: literal<char, Ignored...>
  {
