1b5ca449d7b719201f7abe2288dc3e282b2914e1
Igor Zarzycki <izarzycki@dmcs.pl>
2025-03-18 22:04:28 +0100

    Move ISO Latin Arabic.

--- include/crap/text_encoding.d/textencodingtype.h
+++ include/crap/text_encoding.d/textencodingtype.h
@@ -297,26 +297,8 @@ namespace crap
  };
 #endif
 
- using text_encoding_id_ISOLatinArabic_t =
-	  std :: integral_constant<textEncodingType <void> :: id, textEncodingType <void> :: id :: ISOLatinArabic>;
-
  using text_encoding_id_ISOLatinGreek_t =
 	  std :: integral_constant<textEncodingType <void> :: id, textEncodingType <void> :: id :: ISOLatinGreek>;
-
- template <>
- struct textEncodingType<text_encoding_id_ISOLatinArabic_t> : textEncodingType<void>
- {
-  using mib = text_encoding_id_ISOLatinArabic_t;
-  using name = string<char, 'I', 'S', 'O', '-', '8', '8', '5', '9', '-', '6', ':', '1', '9', '8', '7'>;
-  using aliases = typeList<
-	  string<char 'i', 's', 'o', '-', 'i', 'r', '-', '1', '2', '7'>,
-	  string<char, 'I', 'S', 'O', '_', '8', '8', '5', '9', '-', '6'>,
-	  string<char, 'I', 'S', 'O', '-', '8', '8', '5', '9', '-', '6'>,
-	  string<char, 'E', 'C', 'M', 'A', '-', '1', '1', '4'>,
-	  string<char, 'A', 'S', 'M', 'O', '-', '7', '0', '8'>,
-	  string<char, 'a', 'r', 'a', 'b', 'i', 'c'>,
-	  string<char 'c', 's', 'I', 'S', 'O', 'L', 'a', 't', 'i', 'n', 'A', 'r', 'a', 'b', 'i', 'c'> >; //Libstdc++ adds alias "ISO-8859-6:1987" (name)
- };
 
  template <>
  struct textEncodingType<text_encoding_id_ISOLatinGreek_t> : textEncodingType<void>
@@ -335,32 +317,8 @@ namespace crap
  };
 
  template <>
- struct textEncodingType<string<char 'a', 'r', 'a', 'b', 'i', 'c'> >
- : textEncodingType<text_encoding_id_ISOLatinArabic_t>
- {
- };
-
- template <>
- struct textEncodingType<string<char 'A', 'S', 'M', 'O', '-', '7', '0', '8'> >
- : textEncodingType<text_encoding_id_ISOLatinArabic_t>
- {
- };
-
- template <>
- struct textEncodingType<string<char 'c', 's', 'I', 'S', 'O', 'L', 'a', 't', 'i', 'n', 'A', 'r', 'a', 'b', 'i', 'c'> >
- : textEncodingType<text_encoding_id_ISOLatinArabic_t>
- {
- };
-
- template <>
  struct textEncodingType<string<char 'c', 's', 'I', 'S', 'O', 'L', 'a', 't', 'i', 'n', 'G', 'r', 'e', 'e', 'k'> >
  : textEncodingType<text_encoding_id_ISOLatinGreek_t>
- {
- };
-
- template <>
- struct textEncodingType<string<char 'E', 'C', 'M', 'A', '-', '1', '1', '4'> >
- : textEncodingType<text_encoding_id_ISOLatinArabic_t>
  {
  };
 
@@ -389,26 +347,8 @@ namespace crap
  };
 
  template <>
- struct textEncodingType<string<char 'I', 'S', 'O', '_', '8', '8', '5', '9', '-', '6'> >
- : textEncodingType<text_encoding_id_ISOLatinArabic_t>
- {
- };
-
- template <>
  struct textEncodingType<string<char 'I', 'S', 'O', '_', '8', '8', '5', '9', '-', '7'> >
  : textEncodingType<text_encoding_id_ISOLatinGreek_t>
- {
- };
-
- template <>
- struct textEncodingType<string<char 'I', 'S', 'O', '-', '8', '8', '5', '9', '-', '6'> >
- : textEncodingType<text_encoding_id_ISOLatinArabic_t>
- {
- };
-
- template <>
- struct textEncodingType<string<char 'I', 'S', 'O', '-', '8', '8', '5', '9', '-', '6', ':', '1', '9', '8', '7'> >
- : textEncodingType<text_encoding_id_ISOLatinArabic_t>
  {
  };
 
@@ -429,12 +369,6 @@ namespace crap
  : textEncodingType<text_encoding_id_ISOLatinGreek_t>
  {
  };
-
- template <>
- struct textEncodingType<string<char 'i', 's', 'o', '-', 'i', 'r', '-', '1', '2', '7'> >
- : textEncodingType<text_encoding_id_ISOLatinArabic_t>
- {
- };
 }
 
 #include "IANA_encodings.d/ASCII_textencodingtype.h"
@@ -443,6 +377,7 @@ namespace crap
 #include "IANA_encodings.d/ISOLatin3_textencodingtype.h"
 #include "IANA_encodings.d/ISOLatin4_textencodingtype.h"
 #include "IANA_encodings.d/ISOLatinCyrillic_textencodingtype.h"
+#include "IANA_encodings.d/ISOLatinArabic_textencodingtype.h"
 
 namespace crap
 {
