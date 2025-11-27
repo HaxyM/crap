c9f3247e97d539b235c63bff001bcf314e22f3ac
Igor Zarzycki <izarzycki@dmcs.pl>
2025-11-27 19:39:34 +0100

    Add ISO 84 Portuguese2 aliases recognision.

---
+++ include/crap/text_encoding.d/aliases_declarations.d/ISO84Portuguese2_textencodingtype_aliasesdeclaration.h
@@ -0,0 +1,17 @@
+#include "../textencodingtype.h"
+
+#ifndef CRAP_TEXTENCODING_ALIASESDECLARATIONS_ISO84PORTUGUESE2TEXTENCODINGTYPEALIASESDECLARATION
+#define CRAP_TEXTENCODING_ALIASESDECLARATIONS_ISO84PORTUGUESE2TEXTENCODINGTYPEALIASESDECLARATION
+
+template <class PlaceHolder>
+struct alias<PlaceHolder, char, 'c', 's', 'i', 's', 'o', '8', '4', 'p', 'o', 'r', 't', 'u', 'g', 'u', 'e', 's', 'e', '2'>;
+
+template <class PlaceHolder>
+struct alias<PlaceHolder, char, 'i', 's', 'o', '6', '4', '6', 'p', 't', '2'>;
+
+template <class PlaceHolder>
+struct alias<PlaceHolder, char, 'i', 's', 'o', 'i', 'r', '8', '4'>;
+
+template <class PlaceHolder>
+struct alias<PlaceHolder, char, 'p', 't', '2'>;
+#endif
--- include/crap/text_encoding.d/textencodingtype.h
+++ include/crap/text_encoding.d/textencodingtype.h
@@ -765,6 +765,7 @@ namespace crap
   #include "aliases_declarations.d/ISO58GB231280_textencodingtype_aliasesdeclaration.h"
   #include "aliases_declarations.d/ISO61Norwegian2_textencodingtype_aliasesdeclaration.h"
   #include "aliases_declarations.d/ISO70VideotexSupp1_textencodingtype_aliasesdeclaration.h"
+  #include "aliases_declarations.d/ISO84Portuguese2_textencodingtype_aliasesdeclaration.h"
   //
   template <CharType ... SubLetters>
 	  using aliasMaker = alias<CharType, CharType, SubLetters...>; 
@@ -917,6 +918,7 @@ namespace crap
 #include "aliases.d/ISO58GB231280_textencodingtype_alias.h"
 #include "aliases.d/ISO61Norwegian2_textencodingtype_alias.h"
 #include "aliases.d/ISO70VideotexSupp1_textencodingtype_alias.h"
+#include "aliases.d/ISO84Portuguese2_textencodingtype_alias.h"
 
 namespace crap
 {
