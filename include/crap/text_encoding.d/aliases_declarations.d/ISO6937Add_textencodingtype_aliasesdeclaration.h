d77c37f7378083f8d3432d04aa0ab3d5a725cba9
Igor Zarzycki <izarzycki@dmcs.pl>
2025-12-11 12:48:48 +0100

    Add ISO 6937 Add eliases recognision.

---
+++ include/crap/text_encoding.d/aliases_declarations.d/ISO6937Add_textencodingtype_aliasesdeclaration.h
@@ -0,0 +1,14 @@
+#include "../textencodingtype.h"
+
+#ifndef CRAP_TEXTENCODING_ALIASESDECLARATIONS_ISO6937ADDTEXTENCODINGTYPEALIASESDECLARATION
+#define CRAP_TEXTENCODING_ALIASESDECLARATIONS_ISO6937ADDTEXTENCODINGTYPEALIASESDECLARATION
+
+template <class PlaceHolder>
+struct alias<PlaceHolder, char, 'c', 's', 'i', 's', 'o', '6', '9', '3', '7', 'a', 'd', 'd'>;
+
+template <class PlaceHolder>
+struct alias<PlaceHolder, char, 'i', 's', 'o', '6', '9', '3', '7', '2', '2', '5'>;
+
+template <class PlaceHolder>
+struct alias<PlaceHolder, char, 'i', 's', 'o', 'i', 'r', '1', '5', '2'>;
+#endif
--- include/crap/text_encoding.d/textencodingtype.h
+++ include/crap/text_encoding.d/textencodingtype.h
@@ -798,6 +798,7 @@ namespace crap
   #include "aliases_declarations.d/ISO147Macedonian_textencodingtype_aliasesdeclaration.h"
   #include "aliases_declarations.d/ISO150_textencodingtype_aliasesdeclaration.h"
   #include "aliases_declarations.d/ISO151Cuba_textencodingtype_aliasesdeclaration.h"
+  #include "aliases_declarations.d/ISO6937Add_textencodingtype_aliasesdeclaration.h"
   //
   template <CharType ... SubLetters>
 	  using aliasMaker = alias<CharType, CharType, SubLetters...>; 
@@ -983,6 +984,7 @@ namespace crap
 #include "aliases.d/ISO147Macedonian_textencodingtype_alias.h"
 #include "aliases.d/ISO150_textencodingtype_alias.h"
 #include "aliases.d/ISO151Cuba_textencodingtype_alias.h"
+#include "aliases.d/ISO6937Add_textencodingtype_alias.h"
 
 namespace crap
 {
