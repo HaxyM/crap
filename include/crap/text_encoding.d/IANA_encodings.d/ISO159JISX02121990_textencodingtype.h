1e0b3c23934e7b86e8075775580ee422931ad5d1
Igor Zarzycki <izarzycki@dmcs.pl>
2025-12-14 23:21:57 +0100

    Fix type.

--- include/crap/text_encoding.d/IANA_encodings.d/ISO159JISX02121990_textencodingtype.h
+++ include/crap/text_encoding.d/IANA_encodings.d/ISO159JISX02121990_textencodingtype.h
@@ -16,7 +16,7 @@ namespace crap
   using mib = text_encoding_id_ISO159JISX02121990_t;
   using name = string<char, 'J', 'I', 'S', '_', 'X', '0', '2', '1', '2', '-', '1', '9', '9', '0'>;
   using aliases = typeList<
-      string<char, 'J', 'I', 'S', '_', 'X', '0', '2', '1', '2', '-', '1'. '9', '9', '0'>,
+      string<char, 'J', 'I', 'S', '_', 'X', '0', '2', '1', '2', '-', '1', '9', '9', '0'>,
       string<char, 'x', '0', '2', '1', '2'>,
       string<char, 'i', 's', 'o', '-', 'i', 'r', '-', '1', '5', '9'>,
       string<char, 'c', 's', 'I', 'S', 'O', '1', '5', '9', 'J', 'I', 'S', 'X', '0', '2', '1', '2', '1', '9', '9', '0'> >;
